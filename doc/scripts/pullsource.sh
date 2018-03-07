8d7a449c55333dd25db80b383f4cd979e044acea
David B. Kinder <david.b.kinder@intel.com>
2018-03-06 17:07:19 -0800

    doc: fix source file fetching and cleaning
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

--- doc/scripts/pullsource.sh
+++ doc/scripts/pullsource.sh
@@ -3,16 +3,25 @@
 # pull fresh copies of the ACRN source and copy public API headers
 # over to the documentation tree
 
+if [ ! -d "../acrn-hypervisor" ]; then
+  echo Repo for acrn-hypervisor is missing.
+  exit -1
+fi
+if [ ! -d "../acrn-devicemodel" ]; then
+  echo Repo for acrn-devicemodel is missing.
+  exit -1
+fi
+
 cd ../acrn-hypervisor;git pull
 
-mkdir -p ../acrn_documentation/_source/hypervisor/include/common
-cp include/common/hypercall.h ../acrn_documentation/_source/hypervisor/include/common
+mkdir -p ../acrn-documentation/_source/hypervisor/include/common
+cp include/common/hypercall.h ../acrn-documentation/_source/hypervisor/include/common
 
-mkdir -p ../acrn_documentation/_source/hypervisor/include/public
-cp include/public/acrn_common.h ../acrn_documentation/_source/hypervisor/include/public
-cp include/public/acrn_hv_defs.h ../acrn_documentation/_source/hypervisor/include/public
+mkdir -p ../acrn-documentation/_source/hypervisor/include/public
+cp include/public/acrn_common.h ../acrn-documentation/_source/hypervisor/include/public
+cp include/public/acrn_hv_defs.h ../acrn-documentation/_source/hypervisor/include/public
 
 cd ../acrn-devicemodel;git pull
 
-mkdir -p ../acrn_documentation/_source/devicemodel/include
-cp include/virtio.h ../acrn_documentation/_source/devicemodel/include
+mkdir -p ../acrn-documentation/_source/devicemodel/include
+cp include/virtio.h ../acrn-documentation/_source/devicemodel/include
