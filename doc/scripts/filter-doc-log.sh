0dc93a5281f306b8df45155782a9a3e67e684cb7
David B. Kinder <david.b.kinder@intel.com>
2018-03-13 16:24:35 -0700

    doc: filter known issues
    make the doc build process quiet and add filtering of known (Sphinx)
    issues.  Scripting comes from the open source Zephyr project.
    
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

---
+++ doc/scripts/filter-doc-log.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# run the filter-known-issues.py script to remove "expected" warning
+# messages from the output of the document build process and write
+# the filtered output to stdout
+#
+# Only argument is the name of the log file saved by the build.
+
+KI_SCRIPT=scripts/filter-known-issues.py
+CONFIG_DIR=.known-issues/doc
+
+LOG_FILE=$1
+
+if [ -z "${LOG_FILE}" ]; then
+        echo "Error in $0: missing input parameter <logfile>"
+        exit 1
+fi
+
+# When running in background, detached from terminal jobs, tput will
+# fail; we usually can tell because there is no TERM env variable.
+if [ -z "${TERM:-}" -o "${TERM:-}" = dumb ]; then
+    TPUT="true"
+    red=''
+    green=''
+else
+    TPUT="tput"
+    red='\E[31m'
+    green='\e[32m'
+fi
+
+if [ -s "${LOG_FILE}" ]; then
+   $KI_SCRIPT --config-dir ${CONFIG_DIR} ${LOG_FILE} > doc.warnings 2>&1
+   if [ -s doc.warnings ]; then
+	   echo
+	   echo -e "${red}New errors/warnings found, please fix them:"
+	   echo -e "=============================================="
+	   $TPUT sgr0
+	   echo
+	   cat doc.warnings
+	   echo
+   else
+	   echo -e "${green}No new errors/warnings."
+	   $TPUT sgr0
+   fi
+
+else
+   echo "Error in $0: logfile \"${LOG_FILE}\" not found."
+   exit 1
+fi
