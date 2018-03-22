65233802f0c317bb518f8b28d7437b2ae5154e68
Li, Fei1 <fei1.li@intel.com>
2018-03-22 16:44:56 +0800

    Minor cleanup not to use uart for release on platform uefi
    Signed-off-by: Li, Fei1 <fei1.li@intel.com>

--- include/debug/serial.h
+++ include/debug/serial.h
@@ -33,10 +33,14 @@
 
 #ifdef HV_DEBUG
 int serial_init(void);
+void uart16550_set_property(int enabled, int port_mapped, uint64_t base_addr);
 #else
 static inline int serial_init(void) { return 0; }
+static inline void uart16550_set_property(__unused int enabled,
+					__unused int port_mapped,
+					__unused uint64_t base_addr)
+{
+}
 #endif
-
-void uart16550_set_property(int enabled, int port_mapped, uint64_t base_addr);
 
 #endif
