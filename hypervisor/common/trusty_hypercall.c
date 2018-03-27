1fd07ba349d9d1cd0c37ce22ad2a2dfbf1ba9e3c
Qi Yadong <yadong.qi@intel.com>
2018-03-27 17:26:38 +0800

    trusty: Simulate Secure Monitor Call(SMC) by Hypercall
    For ARM, The SMC instruction is used to generate a synchronous
    exception that is handled by Secure Monitor code running in EL3.
    In the ARM architecture, synchronous control is transferred between
    the normal Non-secure state and the Secure state through Secure
    Monitor Call exceptions. SMC exceptions are generated by the SMC
    instruction, and handled by the Secure Monitor.The operation of
    the Secure Monitor is determined by the parameters that are passed
    in through registers.
    
    For ACRN, Hypervisor will simulate SMC by hypercall to switch vCPU
    State between Normal World and Secure World.
    There are 4 registers(RDI, RSI, RDX, RBX) reserved for paramters
    passing between Normal World and Secure World.
    
    Signed-off-by: Qi Yadong <yadong.qi@intel.com>

---
+++ hypervisor/common/trusty_hypercall.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <hypervisor.h>
+#include <hv_lib.h>
+#include <acrn_common.h>
+#include <hv_arch.h>
+#include <hypercall.h>
+#include <acrn_hv_defs.h>
+#include <hv_debug.h>
+
+int64_t hcall_world_switch(struct vcpu *vcpu)
+{
+	int next_world_id = !(vcpu->arch_vcpu.cur_context);
+
+	if (!is_hypercall_from_ring0()) {
+		pr_err("%s() is only allowed from RING-0!\n", __func__);
+		return -1;
+	}
+
+	if (!vcpu->vm->sworld_control.sworld_enabled) {
+		pr_err("Secure World is not enabled!\n");
+		return -1;
+	}
+
+	if (!vcpu->vm->arch_vm.sworld_eptp) {
+		pr_err("Trusty is not launched!\n");
+		return -1;
+	}
+
+	ASSERT(next_world_id < NR_WORLD,
+		"world_id exceed max number of Worlds");
+
+	switch_world(vcpu, next_world_id);
+	return 0;
+}
