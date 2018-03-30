2bec3b3c73c111d6695ae3ce9c514b684954f25c
Zheng, Gen <gen.zheng@intel.com>
2018-03-30 12:32:38 +0800

    UEFI: cleanup functions in boot.c file
    Remove the useless function named get_path().
    Remove the useless function named print_ch().
    Remove the useless function named isspace().
    Move the function memory_map() from boot.c to malloc.c
    
    Signed-off-by: Zheng, Gen <gen.zheng@intel.com>

--- hypervisor/bsp/uefi/efi/malloc.c
+++ hypervisor/bsp/uefi/efi/malloc.c
@@ -37,6 +37,63 @@
 #include "stdlib.h"
 
 /**
+ * memory_map - Allocate and fill out an array of memory descriptors
+ * @map_buf: buffer containing the memory map
+ * @map_size: size of the buffer containing the memory map
+ * @map_key: key for the current memory map
+ * @desc_size: size of the desc
+ * @desc_version: memory descriptor version
+ *
+ * On success, @map_size contains the size of the memory map pointed
+ * to by @map_buf and @map_key, @desc_size and @desc_version are
+ * updated.
+ */
+EFI_STATUS
+memory_map(EFI_MEMORY_DESCRIPTOR **map_buf, UINTN *map_size,
+	   UINTN *map_key, UINTN *desc_size, UINT32 *desc_version)
+{
+	EFI_STATUS err;
+
+	*map_size = sizeof(**map_buf) * 31;
+get_map:
+
+	/*
+	 * Because we're about to allocate memory, we may
+	 * potentially create a new memory descriptor, thereby
+	 * increasing the size of the memory map. So increase
+	 * the buffer size by the size of one memory
+	 * descriptor, just in case.
+	 */
+	*map_size += sizeof(**map_buf);
+
+	err = allocate_pool(EfiLoaderData, *map_size,
+			    (void **)map_buf);
+	if (err != EFI_SUCCESS) {
+		Print(L"Failed to allocate pool for memory map");
+		goto failed;
+	}
+
+	err = get_memory_map(map_size, *map_buf, map_key,
+			     desc_size, desc_version);
+	if (err != EFI_SUCCESS) {
+		if (err == EFI_BUFFER_TOO_SMALL) {
+			/*
+			 * 'map_size' has been updated to reflect the
+			 * required size of a map buffer.
+			 */
+			free_pool((void *)*map_buf);
+			goto get_map;
+		}
+
+		Print(L"Failed to get memory map");
+		goto failed;
+	}
+
+failed:
+	return err;
+}
+
+/**
  * emalloc - Allocate memory with a strict alignment requirement
  * @size: size in bytes of the requested allocation
  * @align: the required alignment of the allocation
