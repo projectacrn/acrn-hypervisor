12b9299ff69fd9a743c5189ee152017a90ee42d5
Liu Shuo <shuo.a.liu@intel.com>
2018-04-02 17:40:52 +0800

    virtio-heci: Introduce TX and RX threads for data transfer
    Backend service access the native HECI driver through MEI driver
    interface in service OS. Some MEI clients need to be multiplexed,
    the backend service will engage necessary service at clients filter.
    
    TX thread is for passing data from guest to host MEI driver, then to
    native hardware. RX thread is for passing data from native hardware to
    guest.
    
    Signed-off-by: Liu Shuo <shuo.a.liu@intel.com>
    Reviewed-by: Li Hao <hao.l.li@intel.com>
    Reviewed-by: Wang Yu <yu1.wang@intel.com>
    Reviewed-by: Zhao, Yakui <yakui.zhao@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- devicemodel/include/heci.h
+++ devicemodel/include/heci.h
@@ -32,6 +32,16 @@
 #define _HECI_H_
 #include <uuid/uuid.h>
 
+#define HECI_HBM_HOST_VERSION			0x01
+#define HECI_HBM_HOST_STOP			0x02
+#define HECI_HBM_ME_STOP			0x03
+#define HECI_HBM_HOST_ENUM			0x04
+#define HECI_HBM_HOST_CLIENT_PROP		0x05
+#define HECI_HBM_CLIENT_CONNECT			0x06
+#define HECI_HBM_CLIENT_DISCONNECT		0x07
+#define HECI_HBM_FLOW_CONTROL			0x08
+#define HECI_HBM_CLIENT_CONNECTION_RESET	0x09
+
 /*
  * enum heci_hbm_status  - heci host bus messages return values
  *
@@ -86,6 +96,121 @@ struct heci_msg_hdr {
 	uint32_t internal:1;
 	uint32_t msg_complete:1;
 } __attribute__((packed));
+
+struct heci_hbm_cmd {
+	uint8_t cmd:7;
+	uint8_t is_response:1;
+};
+
+struct heci_hbm_host_ver_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t reserved;
+	uint8_t minor;
+	uint8_t major;
+};
+
+struct heci_hbm_host_ver_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t host_ver_support;
+	uint8_t minor;
+	uint8_t major;
+};
+
+struct heci_hbm_host_stop_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t reason;
+	uint8_t reserved[2];
+};
+
+struct heci_hbm_host_stop_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t reserved[3];
+};
+
+struct heci_hbm_me_stop_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t reason;
+	uint8_t reserved[2];
+};
+
+struct heci_hbm_me_stop_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t reserved[3];
+};
+
+struct heci_hbm_host_enum_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t reserved[3];
+};
+
+struct heci_hbm_host_enum_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t reserved[3];
+	uint8_t valid_addresses[32];
+};
+
+struct heci_hbm_host_client_prop_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t address;
+	uint8_t reserved[2];
+};
+
+struct heci_hbm_host_client_prop_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t address;
+	uint8_t status;
+	uint8_t reserved[1];
+	struct heci_client_properties props;
+};
+
+struct heci_hbm_client_connect_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t me_addr;
+	uint8_t host_addr;
+	uint8_t reserved;
+};
+
+struct heci_hbm_client_connect_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t me_addr;
+	uint8_t host_addr;
+	uint8_t status;
+};
+
+struct heci_hbm_client_disconnect_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t me_addr;
+	uint8_t host_addr;
+	uint8_t reserved;
+};
+
+struct heci_hbm_client_disconnect_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t me_addr;
+	uint8_t host_addr;
+	uint8_t status;
+};
+
+struct heci_hbm_flow_ctl {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t me_addr;
+	uint8_t host_addr;
+	uint8_t reserved[5];
+};
+
+struct heci_hbm_client_connection_reset_req {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t me_addr;
+	uint8_t host_addr;
+	uint8_t reserved[1];
+};
+
+struct heci_hbm_client_connection_reset_res {
+	struct heci_hbm_cmd hbm_cmd;
+	uint8_t me_addr;
+	uint8_t host_addr;
+	uint8_t status;
+};
 
 #define IOCTL_MEI_ENUMERATE_ME_CLIENTS \
 	_IOWR('H', 0x04, struct mei_enumerate_me_clients)
