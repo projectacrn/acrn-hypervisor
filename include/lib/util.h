12015d61da255f40e6608c50db426ac49f6572f8
Li, Fei1 <fei1.li@intel.com>
2018-04-19 16:16:42 +0800

    hv: lib: add max()/min() API
    Add max()/min() lib API
    
    Signed-off-by: Li, Fei1 <fei1.li@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- include/lib/util.h
+++ include/lib/util.h
@@ -56,4 +56,8 @@
 /** Roundup (x/y) to ( x/y + (x%y) ? 1 : 0) **/
 #define INT_DIV_ROUNDUP(x, y)	(((x)+(y)-1)/(y))
 
+#define min(x, y)	((x) < (y)) ? (x) : (y)
+
+#define max(x, y)	((x) < (y)) ? (y) : (x)
+
 #endif /* UTIL_H */
