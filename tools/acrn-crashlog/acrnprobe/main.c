be3d71e2a41ffc7c623b66e493bbec8f890c08fc
Liu Xinwu <xinwu.liu@intel.com>
2018-04-25 10:19:46 +0800

    tools: acrn-crashlog: framework of acrn-crashlog
    This is the first patch of acrn-crashlog.
    
    This patch initializes the framework of acrn-crashlog: acrnprobe,
    common, data, and usercrash. And it initializes the Makefile for
    each part.
    
    Signed-off-by: Liu Xinwu <xinwu.liu@intel.com>
    Signed-off-by: CHEN Gang <gang.c.chen@intel.com>
    Reviewed-by: Zhang Yanmin <yanmin.zhang@intel.com>
    Reviewed-by: Liu Chuansheng <chuansheng.liu@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>
    Reviewed-by: Geoffroy Van Cutsem <geoffroy.vancutsem@intel.com>
    Acked-by: Eddie Dong <Eddie.dong@intel.com>

---
+++ tools/acrn-crashlog/acrnprobe/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2018 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+int main(void)
+{
+	//TO BE DONE
+	//This empty function is to satisfy the dependency of Makefile.
+	//This is the entry of acrnprobe, the implementation will be filled
+	//by following patches.
+	return 0;
+}
