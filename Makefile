projectacrn/acrn-hypervisor

commit fc2b6fbe86e8e83ce39be4bc588cc77097dbd914

Makefile: keep using 'PLATFORM' variable for existing documentation
Signed-off-by: Tonny Tzeng tonny.tzeng@intel.com

Makefile
@@ -1,8 +1,8 @@
 
 # global helper variables
 T := $(CURDIR)
-PLAT := uefi
-RELEASE := 0
+PLATFORM ?= uefi
+RELEASE ?= 0
 
 ROOT_OUT := $(T)/build
 HV_OUT := $(ROOT_OUT)/hypervisor
@@ -13,8 +13,8 @@ TOOLS_OUT := $(ROOT_OUT)/tools
 all: hypervisor devicemodel tools
 
 hypervisor:
-	make -C $(T)/hypervisor HV_OBJDIR=$(HV_OUT) PLATFORM=$(PLAT) RELEASE=$(RELEASE) clean
-	make -C $(T)/hypervisor HV_OBJDIR=$(HV_OUT) PLATFORM=$(PLAT) RELEASE=$(RELEASE)
+	make -C $(T)/hypervisor HV_OBJDIR=$(HV_OUT) PLATFORM=$(PLATFORM) RELEASE=$(RELEASE) clean
+	make -C $(T)/hypervisor HV_OBJDIR=$(HV_OUT) PLATFORM=$(PLATFORM) RELEASE=$(RELEASE)
 
 devicemodel:
 	make -C $(T)/devicemodel DM_OBJDIR=$(DM_OUT) clean

commit 63cdaac6e3406bf13cdc9b4a8fa3530d24fa5933

Makefile: fix PLATFORM variable at install target
The variable PLAT was renamed to PLATFORM, but was not updated in the
install target giving a failure when installing ACRN hypervisor.

Fixes: fc2b6fbe86e8 ("Makefile: keep using 'PLATFORM' variable for existing documentation")

Signed-off-by: Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>

Makefile
@@ -34,7 +34,7 @@ clean:
 install: hypervisor-install devicemodel-install tools-install
 
 hypervisor-install:
-	make -C $(T)/hypervisor HV_OBJDIR=$(HV_OUT) PLATFORM=$(PLAT) RELEASE=$(RELEASE) install
+	make -C $(T)/hypervisor HV_OBJDIR=$(HV_OUT) PLATFORM=$(PLATFORM) RELEASE=$(RELEASE) install
 
 devicemodel-install:
 	make -C $(T)/devicemodel DM_OBJDIR=$(DM_OUT) install