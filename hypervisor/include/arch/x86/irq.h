88b8ce8362f6873c5f9c4dee6e4a4eba3a947eb3
Mingqiang Chi <mingqiang.chi@intel.com>
2018-05-23 10:25:00 +0800

    hv: move structure(intr_ctx) to irq.h
    move intr_ctx to irq.h, delete intr_ctx.h
    
    Signed-off-by: Mingqiang Chi <mingqiang.chi@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/include/arch/x86/irq.h
+++ hypervisor/include/arch/x86/irq.h
@@ -91,6 +91,36 @@ struct irq_routing_entry {
 	int intx;	/* PCI cfg offset 0x3D: 0-3 = INTA,INTB,INTC,INTD*/
 };
 
+/*
+ * Definition of the stack frame layout
+ */
+struct intr_ctx {
+	uint64_t r12;
+	uint64_t r13;
+	uint64_t r14;
+	uint64_t r15;
+	uint64_t rbx;
+	uint64_t rbp;
+
+	uint64_t rax;
+	uint64_t rcx;
+	uint64_t rdx;
+	uint64_t rsi;
+	uint64_t rdi;
+	uint64_t r8;
+	uint64_t r9;
+	uint64_t r10;
+	uint64_t r11;
+
+	uint64_t vector;
+	uint64_t error_code;
+	uint64_t rip;
+	uint64_t cs;
+	uint64_t rflags;
+	uint64_t rsp;
+	uint64_t ss;
+};
+
 int irq_mark_used(int irq);
 int irq_alloc(void);
 
