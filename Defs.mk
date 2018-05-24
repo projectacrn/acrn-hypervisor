97d5bf3781b7cb0e1d3618bd2ed0acea61e155f4
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2018-05-23 23:57:05 -0700

    Configure install locations in a single place.
    Also, fix default install destinations for some files (most notably
    binaries should be located in /usr/lib/acrn).

---
+++ Defs.mk
@@ -0,0 +1,6 @@
+# Copyright (c) 2018 Intel Corporation. All rights reserved.
+# See LICENSE for terms.
+
+INSTALL_BINDIR := /usr/bin
+INSTALL_LIBDIR := /usr/lib/acrn
+INSTALL_SHAREDIR := /usr/share/acrn
