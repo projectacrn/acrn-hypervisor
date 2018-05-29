1bf0c4f199bef63e7f6e49d634716d4c3949d3b7
Li, Fei1 <fei1.li@intel.com>
2018-05-29 11:42:04 +0800

    hv: define errno more general
    Hypercall will return errno to SOS (Now is linux kernel).
    Let's define this more general.
    
    Signed-off-by: Li, Fei1 <fei1.li@intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>

--- hypervisor/include/lib/errno.h
+++ hypervisor/include/lib/errno.h
@@ -31,17 +31,21 @@
 #ifndef ERRNO_H
 #define ERRNO_H
 
-/** Indicates that not enough memory. */
-#define ENOMEM             1
-/** Indicates that argument is not valid. */
-#define EINVAL             2
-/** Indicates that no such dev. */
-#define ENODEV             3
+/** Indicates that operation not permitted. */
+#define EPERM		1
 /** Indicates that there is IO error. */
-#define EIO                4
-/** Indicates that target is busy. */
-#define EBUSY              5
+#define EIO		5
+/** Indicates that not enough memory. */
+#define ENOMEM		12
+/** Indicates Permission denied */
+#define EACCES		13
 /** Indicates there is fault. */
-#define EFAULT             6
+#define EFAULT		14
+/** Indicates that target is busy. */
+#define EBUSY		16
+/** Indicates that no such dev. */
+#define ENODEV		19
+/** Indicates that argument is not valid. */
+#define EINVAL		22
 
 #endif /* ERRNO_H */
