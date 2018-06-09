f899b5fda2b860f1d6afc1a4f43790f47a85e492
Junjie Mao <junjie.mao@intel.com>
2018-06-09 15:39:28 +0800

    HV: make: add menuconfig
    This patch enables 'make menuconfig' leveraging the menuconfig.py in kconfiglib,
    which shows a TUI for modifying the current configurations.
    
    Menuconfig.py is python3 only, and thus the existence of 'python3' instead of
    'python' is checked and python3 is used to run all the scripts.
    
    Signed-off-by: Junjie Mao <junjie.mao@intel.com>
    Acked-by: Anthony Xu <anthony.xu@intel.com>
    Acked-by: Geoffroy VanCutsem <geoffroy.vancutsem@intel.com>

--- scripts/deps.mk
+++ scripts/deps.mk
@@ -14,17 +14,16 @@ check_exec_$(1):
 	 fi
 endef
 
-# usage: check_dep_pylib <library name> <variable>
+# usage: check_dep_py3lib <library name> <variable>
 #
-# Create a target that checks the existence of the specified python library, and
-# append that target to the given variable. The default python version (which
-# can be either 2.x or 3.x) is used.
-define check_dep_pylib =
-$(2) += check_pylib_$(1)
-check_pylib_$(1):
-	@if ! pip list 2>/dev/null | grep $(1) > /dev/null 2>&1; then   \
+# Create a target that checks the existence of the specified python 3 library, and
+# append that target to the given variable.
+define check_dep_py3lib =
+$(2) += check_py3lib_$(1)
+check_py3lib_$(1):
+	@if ! pip3 list 2>/dev/null | grep $(1) > /dev/null 2>&1; then   \
 	     echo "******** Missing prerequisite tool ********"; \
-	     echo "The python library *$(1)* is not installed"; \
+	     echo "The python3 library *$(1)* is not installed"; \
 	     echo "Please refer to the Getting Started Guide" \
 	          "for installation instructions"; \
 	     exit 1; \
