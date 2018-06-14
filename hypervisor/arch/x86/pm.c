52c17408be37685354bc71bfba53794882ce0e94
Yin Fengwei <fengwei.yin@intel.com>
2018-06-14 10:50:34 +0800

    hv: add enter_s3
    enter_s3 is main function for ACRN to enter S3 state with following
    process:
     - pause vm0
     - save the wakeup vec of vm0 and set wakeup vec of ACRN. So
       resume from S3 will jump to ACRN wakeup
     - offline APs
     - update the main entry of trampoline to resume entry. After BSP
       is resume from S3, it will jump to resume entry instead of AP
       startup routine
     - turn off vmx
     - suspend devices
     - enter S3.
    
    exit S3 with following process:
     - release trampoline_spinlock which is hold in trampoline code
     - resume devices
     - enable vmx
     - update the main entry of trampoline to AP startup routine.
     - online APs.
    
    The following operations will be resume vm0 which will be added
    in next patch.
    
    Signed-off-by: Zheng Gen <gen.zheng@intel.com>
    Signed-off-by: Yin Fengwei <fengwei.yin@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/arch/x86/pm.c
+++ hypervisor/arch/x86/pm.c
@@ -14,3 +14,119 @@ void restore_msrs(void)
 	msr_write(MSR_IA32_FS_BASE, (uint64_t)psc);
 #endif
 }
+
+static void acpi_gas_write(struct acpi_generic_address *gas, uint32_t val)
+{
+	if (gas->space_id == SPACE_SYSTEM_MEMORY)
+		mmio_write_word(val, (void *)HPA2HVA(gas->address));
+	else
+		io_write_word(val, gas->address);
+}
+
+static uint32_t acpi_gas_read(struct acpi_generic_address *gas)
+{
+	uint32_t ret = 0;
+
+	if (gas->space_id == SPACE_SYSTEM_MEMORY)
+		ret = mmio_read_word((void *)HPA2HVA(gas->address));
+	else
+		ret = io_read_word(gas->address);
+
+	return ret;
+}
+
+void do_acpi_s3(struct vm *vm, uint32_t pm1a_cnt_val,
+	uint32_t pm1b_cnt_val)
+{
+	uint32_t s1, s2;
+
+	acpi_gas_write(&vm->pm.sx_state_data->pm1a_cnt, pm1a_cnt_val);
+
+	if (vm->pm.sx_state_data->pm1b_cnt.address != 0)
+		acpi_gas_write(&vm->pm.sx_state_data->pm1b_cnt, pm1b_cnt_val);
+
+	while (1) {
+		/* polling PM1 state register to detect wether
+		 * the Sx state enter is interrupted by wakeup event.
+		 */
+		s1 = s2 = 0;
+
+		s1 = acpi_gas_read(&vm->pm.sx_state_data->pm1a_evt);
+
+		if (vm->pm.sx_state_data->pm1b_evt.address != 0) {
+			s2 = acpi_gas_read(&vm->pm.sx_state_data->pm1b_evt);
+			s1 |= s2;
+		}
+
+		/* According to ACPI spec 4.8.3.1.1 PM1 state register, the bit
+		 * WAK_STS(bit 15) is set if system will transition to working
+		 * state.
+		 */
+		if ((s1 & (1 << BIT_WAK_STS)) != 0)
+			break;
+	}
+}
+
+int enter_s3(struct vm *vm, uint32_t pm1a_cnt_val,
+	uint32_t pm1b_cnt_val)
+{
+	uint32_t pcpu_id;
+	uint64_t pmain_entry_saved;
+	uint64_t *pmain_entry;
+
+	if (vm->pm.sx_state_data == NULL) {
+		pr_err("No Sx state info avaiable. No Sx support");
+		return -1;
+	}
+
+	pause_vm(vm);	/* pause vm0 before suspend system */
+
+	pcpu_id = get_cpu_id();
+
+	/* offline all APs */
+	stop_cpus();
+
+	/* Trampoline code is relocatable now. We have to calculate
+	 * main_entry address with relocation base address
+	 */
+	pmain_entry =
+		(uint64_t *)(HPA2HVA(trampoline_start16_paddr) +
+		(uint64_t) main_entry);
+
+	/* Save default main entry and we will restore it after
+	 * back from S3. So the AP online could jmp to correct
+	 * main entry.
+	 */
+	pmain_entry_saved = *pmain_entry;
+	/* Set the main entry for resume from S3 state */
+	*pmain_entry = (uint64_t)restore_s3_context;
+
+	CPU_IRQ_DISABLE();
+	vmx_off(pcpu_id);
+
+	suspend_console();
+	suspend_ioapic();
+	suspend_iommu();
+	suspend_lapic();
+
+	__enter_s3(vm, pm1a_cnt_val, pm1b_cnt_val);
+
+	/* release the lock aquired in trampoline code */
+	spinlock_release(&trampoline_spinlock);
+
+	resume_lapic();
+	resume_iommu();
+	resume_ioapic();
+	resume_console();
+
+	exec_vmxon_instr(pcpu_id);
+	CPU_IRQ_ENABLE();
+
+	/* restore the default main entry */
+	*pmain_entry = pmain_entry_saved;
+
+	/* online all APs again */
+	start_cpus();
+
+	return 0;
+}
