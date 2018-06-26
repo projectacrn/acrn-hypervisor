197d2e6cef72e88c7fc43d1d6d7de659661fb1a4
Yonghua Huang <yonghua.huang@intel.com>
2018-06-26 16:37:46 +0800

    HV: add MTRR capability check when CPU boot
    - to avoid reading operations on MTRR registers if
    no MTRR feature support on current platform in "init_mtrr()".
    
    Signed-off-by: Yonghua Huang <yonghua.huang@intel.com>

--- hypervisor/arch/x86/cpu.c
+++ hypervisor/arch/x86/cpu.c
@@ -219,6 +219,11 @@ static int hardware_detect_support(void)
 		return -ENODEV;
 	}
 
+	if (!cpu_has_cap(X86_FEATURE_MTRR)) {
+		pr_fatal("%s, MTRR not supported\n", __func__);
+		return -ENODEV;
+	}
+
 	if (!cpu_has_cap(X86_FEATURE_VMX)) {
 		pr_fatal("%s, vmx not supported\n", __func__);
 		return -ENODEV;
