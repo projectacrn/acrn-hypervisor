ee1311027863a7a259547cbd2c1fa9ebfb3e61f6
Victor Sun <victor.sun@intel.com>
2018-07-20 18:39:10 +0800

    HV: change wake vector address to accommodate sbl
    - The previous wake vector address just work for ABL, need to change it
      to accommodate the latest SBL;
    
    - Fix wrong bit width value in the pm1a gas;
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Anthony Xu <anthony.xu@intel.com>

--- hypervisor/bsp/sbl/platform_acpi_info.c
+++ hypervisor/bsp/sbl/platform_acpi_info.c
@@ -10,13 +10,13 @@ const struct acpi_info host_acpi_info = {
 	6,				/* x86 family: 6 */
 	0x5C,				/* x86 model: 0x5C, ApolloLake */
 	{
-		{SPACE_SYSTEM_IO, 20, 0, 3, 0x400},	/* PM1a EVT */
+		{SPACE_SYSTEM_IO, 32, 0, 3, 0x400},	/* PM1a EVT */
 		{SPACE_SYSTEM_IO,  0, 0, 0,     0},	/* PM1b EVT */
-		{SPACE_SYSTEM_IO, 10, 0, 2, 0x404},	/* PM1a CNT */
+		{SPACE_SYSTEM_IO, 16, 0, 2, 0x404},	/* PM1a CNT */
 		{SPACE_SYSTEM_IO,  0, 0, 0,     0},	/* PM1b CNT */
 		{0x05,	0,	0},			/* _S3 Package */
 		{0x07,	0,	0},			/* _S5 Package */
-		(uint32_t *)0x7AEDCEFC,			/* Wake Vector 32 */
-		(uint64_t *)0x7AEDCF08			/* Wake Vector 64 */
+		(uint32_t *)0x7A86BC9C,			/* Wake Vector 32 */
+		(uint64_t *)0x7A86BCA8			/* Wake Vector 64 */
 	}
 };
