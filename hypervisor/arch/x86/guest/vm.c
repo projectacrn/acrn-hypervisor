6d1ad76eb7f1111b9ee69fda3897ae809423f746
Sainath Grandhi <sainath.grandhi@intel.com>
2018-08-10 11:02:42 -0700

    hv: Add vuart flag to VM descriptions in partition mode
    ACRN in partition mode emulates UART for the VMs directly launced
    from HV. This patch adds an option to enable/disable vUART for a
    VM.
    
    Signed-off-by: Sainath Grandhi <sainath.grandhi@intel.com>

--- hypervisor/arch/x86/guest/vm.c
+++ hypervisor/arch/x86/guest/vm.c
@@ -197,6 +197,11 @@ int create_vm(struct vm_description *vm_desc, struct vm **rtn_vm)
 	vm->vpic = vpic_init(vm);
 
 #ifdef CONFIG_PARTITION_MODE
+	/* Create virtual uart */
+	if (vm_desc->vm_vuart) {
+		vm->vuart = vuart_init(vm);
+	}
+
 	vpci_init(vm);
 #endif
 
