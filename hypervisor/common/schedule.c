8ef072165fa64ced0c233dcbcc5059c0bb293e31
Jason Chen CJ <jason.cj.chen@intel.com>
2018-08-12 20:56:30 +0800

    idle: enable IRQ in default idle
    we should let cpu handle IRQ during idle
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- hypervisor/common/schedule.c
+++ hypervisor/common/schedule.c
@@ -176,8 +176,11 @@ void default_idle(void)
 			schedule();
 		} else if (need_offline(pcpu_id) != 0) {
 			cpu_dead(pcpu_id);
-		} else
+		} else {
+			CPU_IRQ_ENABLE();
 			cpu_do_idle(pcpu_id);
+			CPU_IRQ_DISABLE();
+		}
 	}
 }
 
