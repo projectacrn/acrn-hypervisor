99e8997504f67a73d0220312bf8a42e59ea8446b
Zhao Yakui <yakui.zhao@intel.com>
2018-08-16 14:38:35 +0800

    DM: Add boot option of "i915.enable_guc=0" to disable Guc on UOS new kernel
    The guc boot option is refined on the new linux kernel. The boot option of
    "i915.enable_guc=0" should be added in order to disable Guc instead of using
    "enable_guc_loading/submission". But in order to use the same boot option on
    multi kernel, both of them are kept.
    
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>
    Reviewed-by: Binbin Wu<binbin.wu@intel.com>

--- devicemodel/samples/apl-mrb/launch_uos.sh
+++ devicemodel/samples/apl-mrb/launch_uos.sh
@@ -123,7 +123,7 @@ acrn-dm -A -m $mem_size -c $2$boot_GVT_option"$GVT_args" -s 0:0,hostbridge -s 1:
   console=ttyS0 no_timer_check ignore_loglevel log_buf_len=16M \
   consoleblank=0 tsc=reliable i915.avail_planes_per_pipe=$4 i915.enable_guc_loading=0 \
   i915.enable_hangcheck=0 i915.nuclear_pageflip=1 \
-  i915.enable_guc_submission=0" $vm_name
+  i915.enable_guc_submission=0 i915.enable_guc=0" $vm_name
 }
 
 function launch_android()
@@ -243,7 +243,7 @@ fi
 kernel_cmdline_generic="maxcpus=$2 nohpet tsc=reliable intel_iommu=off \
    androidboot.serialno=$ser \
    i915.enable_rc6=1 i915.enable_fbc=1 i915.enable_guc_loading=0 i915.avail_planes_per_pipe=$4 \
-   i915.enable_hangcheck=0 use_nuclear_flip=1 i915.enable_guc_submission=0"
+   i915.enable_hangcheck=0 use_nuclear_flip=1 i915.enable_guc_submission=0 i915.enable_guc=0"
 
 boot_dev_flag=",b"
 if [ $6 == 1 ];then
