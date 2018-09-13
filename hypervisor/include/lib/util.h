83e7995d5d16c653f299b2211f9527afa76fc5f4
Shiqing Gao <shiqing.gao@intel.com>
2018-09-13 09:25:27 +0800

    hv: clean up some header files
    - move the contents in macros.h and stdarg.h to util.h
    - remove macros.h and stdarg.h
    
    Tracked-On: #861
    Signed-off-by: Shiqing Gao <shiqing.gao@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/include/lib/util.h
+++ hypervisor/include/lib/util.h
@@ -8,6 +8,8 @@
 #define UTIL_H
 
 #define offsetof(st, m) __builtin_offsetof(st, m)
+#define va_start	__builtin_va_start
+#define va_end		__builtin_va_end
 
 /** Roundup (x/y) to ( x/y + (x%y) ? 1 : 0) **/
 #define INT_DIV_ROUNDUP(x, y)	((((x)+(y))-1)/(y))
@@ -15,5 +17,17 @@
 #define min(x, y)	((x) < (y)) ? (x) : (y)
 
 #define max(x, y)	((x) < (y)) ? (y) : (x)
+
+/** Replaces 'x' by the string "x". */
+#define STRINGIFY(x) #x
+
+/* Macro used to check if a value is aligned to the required boundary.
+ * Returns TRUE if aligned; FALSE if not aligned
+ * NOTE:  The required alignment must be a power of 2 (2, 4, 8, 16, 32, etc)
+ */
+static inline bool mem_aligned_check(uint64_t value, uint64_t req_align)
+{
+	return ((value & (req_align - 1UL)) == 0UL);
+}
 
 #endif /* UTIL_H */
