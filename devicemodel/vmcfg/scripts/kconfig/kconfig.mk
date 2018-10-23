b97cb61bc12f95ab6978d3079da8364e2d9bb881
Ming Liu <liu.ming50@gmail.com>
2018-10-23 10:51:06 +0200

    devicemodel: Makefile: clean up/refactor some code
    - Put version.h, vmcfg_config.h, .config into DM_OBJDIR like what's
      already done in hypervisor and tools, it does not have to stay in
      source or else a extra entry in .gitignore is needed.
    - Change some implicit targets to explicit targets, include:
      vmcfg_header -> $(DM_OBJDIR)/include/vmcfg_config.h
      $(PROGRAM) -> $(DM_OBJDIR)/$(PROGRAM)
    
      then $(DM_OBJDIR)/include/vmcfg_config.h would be depended by
      $(DM_OBJDIR)/%.o without the need involving in a extra implicit
      target vmcfg_header. (And it's not set in .PHONY)
    - These header targets vmcfg_config.h and version.h should be depended
      by $(DM_OBJDIR)/%.o target instead of by all target, and this is done
      since they are in HEADERS now.
    - Drop redundant clean commands and fix some bad styled coding, like
      multiple blanks in a line.
    
    Tracked-On: #1599
    Signed-off-by: Ming Liu <liu.ming50@gmail.com>

--- devicemodel/vmcfg/scripts/kconfig/kconfig.mk
+++ devicemodel/vmcfg/scripts/kconfig/kconfig.mk
@@ -1,20 +1,22 @@
 $(eval $(call check_dep_exec,menuconfig,MENUCONFIG_DEPS))
 
-export KCONFIG_CONFIG := $(BASEDIR)/vmcfg/.config
+export KCONFIG_CONFIG := $(DM_OBJDIR)/vmcfg/.config
 
 .PHONY: oldconfig
 oldconfig:
-	@python3 $(BASEDIR)/../scripts/kconfig/silentoldconfig.py Kconfig  $(KCONFIG_CONFIG)
+	mkdir -p $(dir $(KCONFIG_CONFIG))
+	@python3 $(BASEDIR)/../scripts/kconfig/silentoldconfig.py Kconfig $(KCONFIG_CONFIG)
 
 %_defconfig:
-	@python3 $(BASEDIR)/../scripts/kconfig/defconfig.py Kconfig  $(BASEDIR)/vmcfg/config/$@ $(KCONFIG_CONFIG)
+	mkdir -p $(dir $(KCONFIG_CONFIG))
+	@python3 $(BASEDIR)/../scripts/kconfig/defconfig.py Kconfig $(BASEDIR)/vmcfg/config/$@ $(KCONFIG_CONFIG)
 
 $(KCONFIG_CONFIG): oldconfig
 
-$(BASEDIR)/include/vmcfg_config.h: $(KCONFIG_CONFIG)
+$(DM_OBJDIR)/include/vmcfg_config.h: $(KCONFIG_CONFIG)
 	echo @mkdir -p $(dir $@)
 	@mkdir -p $(dir $@)
 	@python3 $(BASEDIR)/../scripts/kconfig/generate_header.py Kconfig $< $@
 
-menuconfig: $(MENUCONFIG_DEPS) $(HV_OBJDIR)/$(HV_CONFIG)
-	@python3 $(shell which menuconfig) Kconfig
+menuconfig: $(MENUCONFIG_DEPS) $(HV_OBJDIR)/$(HV_CONFIG) $(KCONFIG_CONFIG)
+	@python3 $(shell which menuconfig) Kconfig $(KCONFIG_CONFIG)
