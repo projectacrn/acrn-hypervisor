projectacrn/acrn-hypervisor

commit e1dab512c25765f57a41da311f1b4209bcb859c7

dm: add string convert API
As function sscanf is banned, to get value from parameter buffer,strto*
is recommended. To reduce the inspection code when using strto*, it's
better to use a string convert API.

Usage:
    For virtio-blk, it has parameters:
        range=<start lba>/<subfile size>
    sscanf:
        if (sscanf(cp, "range=%ld/%ld", &sub_file_start_lba,
                &sub_file_size) == 2)
            sub_file_assign = 1;
    string API:
        if (strsep(&cp, "=") &&
                !dm_strtol(cp, &cp, 10, &sub_file_start_lba) &&
                *cp == '/' &&
                !dm_strtol(cp + 1, &cp, 10, &sub_file_size))
            sub_file_assign = 1;

Tracked-on: #1496
Signed-off-by: Conghui Chen <conghui.chen@intel.com>
Reviewed-by: Shuo Liu <shuo.a.liu@intel.com>
Acked-by: Yin Fengwei <fengwei.yin@intel.com>

devicemodel/include/dm_string.h
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef _DM_STRING_H_
+#define _DM_STRING_H_
+
+/**
+ * @brief Convert string to a long integer.
+ *
+ * @param s Pointer to original string.
+ * @param end Pointer to end string.
+ * @param base Base: 8, 10, 16...
+ * @param val Long integer convert from string.
+ *
+ * @return -1 error.
+ * @return 0  no error.
+ */
+
+int dm_strtol(char *s, char **end, unsigned int base, long *val);
+
+/**
+ * @brief Convert string to an integer.
+ *
+ * @param s Pointer to original string.
+ * @param end Pointer to end string.
+ * @param base Base: 8, 10, 16...
+ * @param val Integer convert from string.
+ *
+ * @return -1 error.
+ * @return 0  no error.
+ */
+
+int dm_strtoi(char *s, char **end, unsigned int base, int *val);
+
+/**
+ * @brief Convert string to an unsigned long integer.
+ *
+ * @param s Pointer to original string.
+ * @param end Pointer to end string.
+ * @param base Base: 8, 10, 16...
+ * @param val Unsigned long integer convert from string.
+ *
+ * @return -1 error.
+ * @return 0  no error.
+ */
+
+int dm_strtoul(char *s, char **end, unsigned int base, unsigned long *val);
+
+/**
+ * @brief Convert string to an unsigned integer.
+ *
+ * @param s Pointer to original string.
+ * @param end Pointer to end string after strtol.
+ * @param base Base: 8, 10, 16...
+ * @param val Unsigned integer convert from string.
+ *
+ * @return -1 error.
+ * @return 0  no error.
+ */
+
+int dm_strtoui(char *s, char **end, unsigned int base, unsigned int *val);
+
+#endif

devicemodel/lib/dm_string.c
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "dm_string.h"
+
+int
+dm_strtol(char *s, char **end, unsigned int base, long *val)
+{
+	if (!s)
+		goto err;
+
+	*val = strtol(s, end, base);
+	if (*end == s) {
+		printf("ERROR! nothing covert for: %s!\n", s);
+		goto err;
+	}
+	return 0;
+
+err:
+	return -1;
+}
+
+int
+dm_strtoi(char *s, char **end, unsigned int base, int *val)
+{
+	long l_val;
+	int ret;
+
+	l_val = 0;
+	ret = dm_strtol(s, end, base, &l_val);
+	*val = (int)l_val;
+	return ret;
+}
+
+int
+dm_strtoul(char *s, char **end, unsigned int base, unsigned long *val)
+{
+	if (!s)
+		goto err;
+
+	*val = strtoul(s, end, base);
+	if (*end == s) {
+		printf("ERROR! nothing covert for: %s!\n", s);
+		goto err;
+	}
+	return 0;
+
+err:
+	return -1;
+}
+
+int
+dm_strtoui(char *s, char **end, unsigned int base, unsigned int *val)
+{
+	unsigned long l_val;
+	int ret;
+
+	l_val = 0;
+	ret = dm_strtoul(s, end, base, &l_val);
+	*val = (unsigned int)l_val;
+	return ret;
+}

devicemodel/Makefile
@@ -56,6 +56,9 @@ LIBS += -luuid
 LIBS += -lusb-1.0
 LIBS += -lacrn-mngr
 
+# lib
+SRCS += lib/dm_string.c
+
 # hw
 SRCS += hw/block_if.c
 SRCS += hw/usb_core.c

commit 17aeee9245a4a1dd99df8b145859093895de4db6

dm: add const declaration for dm_strto* APIs
These APIs are wrap of standard strto* APIs. The first parameters in
these APIs should be declared as "const" since they have never been
changed so that they can also be used when passing a variable declared
as const char *

Fixes: e1dab512c257 ("dm: add string convert API")
Tracked-on: #1496

Signed-off-by: Jie Deng <jie.deng@intel.com>
Reviewed-by: Conghui Chen <conghui.chen@intel.com>
Acked-by: Yu Wang <yu1.wang@intel.com>

devicemodel/include/dm_string.h
@@ -20,7 +20,7 @@
  * @return 0  no error.
  */
 
-int dm_strtol(char *s, char **end, unsigned int base, long *val);
+int dm_strtol(const char *s, char **end, unsigned int base, long *val);
 
 /**
  * @brief Convert string to an integer.
@@ -34,7 +34,7 @@ int dm_strtol(char *s, char **end, unsigned int base, long *val);
  * @return 0  no error.
  */
 
-int dm_strtoi(char *s, char **end, unsigned int base, int *val);
+int dm_strtoi(const char *s, char **end, unsigned int base, int *val);
 
 /**
  * @brief Convert string to an unsigned long integer.
@@ -48,7 +48,7 @@ int dm_strtoi(char *s, char **end, unsigned int base, int *val);
  * @return 0  no error.
  */
 
-int dm_strtoul(char *s, char **end, unsigned int base, unsigned long *val);
+int dm_strtoul(const char *s, char **end, unsigned int base, unsigned long *val);
 
 /**
  * @brief Convert string to an unsigned integer.
@@ -62,6 +62,6 @@ int dm_strtoul(char *s, char **end, unsigned int base, unsigned long *val);
  * @return 0  no error.
  */
 
-int dm_strtoui(char *s, char **end, unsigned int base, unsigned int *val);
+int dm_strtoui(const char *s, char **end, unsigned int base, unsigned int *val);
 
 #endif

devicemodel/lib/dm_string.c
@@ -11,7 +11,7 @@
 #include "dm_string.h"
 
 int
-dm_strtol(char *s, char **end, unsigned int base, long *val)
+dm_strtol(const char *s, char **end, unsigned int base, long *val)
 {
 	if (!s)
 		goto err;
@@ -28,7 +28,7 @@ err:
 }
 
 int
-dm_strtoi(char *s, char **end, unsigned int base, int *val)
+dm_strtoi(const char *s, char **end, unsigned int base, int *val)
 {
 	long l_val;
 	int ret;
@@ -40,7 +40,7 @@ dm_strtoi(char *s, char **end, unsigned int base, int *val)
 }
 
 int
-dm_strtoul(char *s, char **end, unsigned int base, unsigned long *val)
+dm_strtoul(const char *s, char **end, unsigned int base, unsigned long *val)
 {
 	if (!s)
 		goto err;
@@ -57,7 +57,7 @@ err:
 }
 
 int
-dm_strtoui(char *s, char **end, unsigned int base, unsigned int *val)
+dm_strtoui(const char *s, char **end, unsigned int base, unsigned int *val)
 {
 	unsigned long l_val;
 	int ret;