6d803e2cb10a298619c7bc23edf0143781bfcca3
Shiqing Gao <shiqing.gao@intel.com>
2018-11-27 14:54:30 +0800

    hv: clean up function definitions in console.h
    seperate the function definitions into debug/release directories
    to better distinguish debug/release libraries
    
    Tracked-On: #861
    Signed-off-by: Shiqing Gao <shiqing.gao@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/include/debug/console.h
+++ hypervisor/include/debug/console.h
@@ -10,13 +10,9 @@
 /* Switching key combinations for shell and uart console */
 #define GUEST_CONSOLE_TO_HV_SWITCH_KEY      0       /* CTRL + SPACE */
 
-#ifdef HV_DEBUG
-extern struct hv_timer console_timer;
-
 /** Initializes the console module.
  *
  */
-
 void console_init(void);
 
 /** Writes a given number of characters to the console.
@@ -27,7 +23,6 @@ void console_init(void);
  *  @return The number of characters written or -1 if an error occurred
  *          and no character was written.
  */
-
 size_t console_write(const char *s, size_t len);
 
 /** Writes a single character to the console.
@@ -37,46 +32,16 @@ size_t console_write(const char *s, size_t len);
  *  @preturn The number of characters written or -1 if an error
  *           occurred before any character was written.
  */
-
 void console_putc(const char *ch);
 char console_getc(void);
 
 void console_setup_timer(void);
-
-static inline void suspend_console(void)
-{
-	del_timer(&console_timer);
-}
-
-static inline void resume_console(void)
-{
-	console_setup_timer();
-}
 void uart16550_set_property(bool enabled, bool port_mapped, uint64_t base_addr);
 
 void shell_init(void);
 void shell_kick(void);
 
-#else
-static inline void console_init(void)
-{
-}
-
-static inline size_t console_write(__unused const char *str,
-			__unused size_t len)
-{
-	return 0;
-}
-static inline void console_putc(__unused const char *ch) { }
-static inline char console_getc(void) { return '\0'; }
-static inline void console_setup_timer(void) {}
-static inline void suspend_console(void) {}
-static inline void resume_console(void) {}
-static inline void uart16550_set_property(__unused bool enabled,
-		__unused bool port_mapped, __unused uint64_t base_addr) {}
-
-static inline void shell_init(void) {}
-static inline void shell_kick(void) {}
-#endif
+void suspend_console(void);
+void resume_console(void);
 
 #endif /* CONSOLE_H */
---
+++ hypervisor/release/console.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <hypervisor.h>
+
+size_t console_write(__unused const char *str, __unused size_t len)
+{
+	return 0;
+}
+
+char console_getc(void)
+{
+	return '\0';
+}
+
+void console_putc(__unused const char *ch) {}
+
+void console_init(void) {}
+void console_setup_timer(void) {}
+
+void suspend_console(void) {}
+void resume_console(void) {}
+
+void uart16550_set_property(__unused bool enabled, __unused bool port_mapped, __unused uint64_t base_addr) {}
+
+void shell_init(void) {}
+void shell_kick(void) {}
