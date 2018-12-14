ed5e210d91881e8f5b8cf66e41c865c3b3c43159
Xie, nanlin <nanlin.xie@intel.com>
2018-12-15 00:07:15 +0800

    Doc: Update GSG for v0.4 version and launch and acrn.conf sample script
    Signed-off-by: Xie, nanlin <nanlin.xie@intel.com>

--- devicemodel/samples/nuc/launch_uos.sh
+++ devicemodel/samples/nuc/launch_uos.sh
@@ -21,7 +21,7 @@ acrn-dm -A -m $mem_size -c $2 -s 0:0,hostbridge -s 1:0,lpc -l com1,stdio \
   -s 2,pci-gvt -G "$3" \
   -s 5,virtio-console,@pty:pty_port \
   -s 6,virtio-hyper_dmabuf \
-  -s 3,virtio-blk,/home/clear/uos/clear-26200-kvm.img \
+  -s 3,virtio-blk,/home/clear/uos/clear-26770-kvm.img \
   -s 4,virtio-net,tap0 \
   --mac_seed $mac_seed \
   -k /usr/lib/kernel/default-iot-lts2018 \
