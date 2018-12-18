ea672c5b0775651516e74ed4140e3180a52b8c8e
Yonghua Huang <yonghua.huang@intel.com>
2018-12-18 18:55:46 +0800

    hv: update coding style for tampoline.c
      - add API get trampoline_start16_paddr() instead
        of exporting this global variable.
      - other minor update.
    
    Tracked-On: #2095
    Signed-off-by: Yonghua Huang <yonghua.huang@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/include/arch/x86/trampoline.h
+++ hypervisor/include/arch/x86/trampoline.h
@@ -10,6 +10,7 @@ extern uint64_t read_trampoline_sym(const void *sym);
 extern void write_trampoline_sym(const void *sym, uint64_t val);
 extern void write_trampoline_stack_sym(uint16_t pcpu_id);
 extern uint64_t prepare_trampoline(void);
+extern uint64_t get_trampoline_start16_paddr(void);
 
 /* external symbols that are helpful for relocation */
 extern const uint8_t	ld_trampoline_load;
@@ -23,7 +24,4 @@ extern uint8_t		cpu_boot_page_tables_ptr;
 extern uint8_t		trampoline_pdpt_addr;
 extern uint8_t		trampoline_gdt_ptr;
 extern uint8_t		trampoline_start64_fixup;
-
-extern uint64_t		trampoline_start16_paddr;
-
 #endif /* TRAMPOLINE_H */
