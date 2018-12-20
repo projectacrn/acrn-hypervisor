a53dad931267cd29cfe68386ccc6132dd5344104
Tw <wei.tan@intel.com>
2018-12-20 09:48:57 +0800

    hv:vtd: fix MISRA-C violations on pointer not checked for null before use
    This patch fix MISRA-C violations in arch/x86/vtd.c
    on Pointer not checked for null before use.
    
    Tracked-On: #861
    Signed-off-by: Tw <wei.tan@intel.com>
    Acked-by: Anthony Xu <anthony.xu@intel.com>

--- hypervisor/arch/x86/vtd.c
+++ hypervisor/arch/x86/vtd.c
@@ -992,12 +992,16 @@ static void do_action_for_iommus(void (*action)(struct dmar_drhd_rt *))
 	struct dmar_drhd_rt *dmar_unit;
 	uint32_t i;
 
-	for (i = 0U; i < info->drhd_count; i++) {
-		dmar_unit = &dmar_drhd_units[i];
-		if (!dmar_unit->drhd->ignore) {
-			action(dmar_unit);
-		} else {
-			dev_dbg(ACRN_DBG_IOMMU, "ignore dmar_unit @0x%x", dmar_unit->drhd->reg_base_addr);
+	if (info == NULL) {
+		pr_fatal("%s: can't find dmar info\n", __func__);
+	} else {
+		for (i = 0U; i < info->drhd_count; i++) {
+			dmar_unit = &dmar_drhd_units[i];
+			if (!dmar_unit->drhd->ignore) {
+				action(dmar_unit);
+			} else {
+				dev_dbg(ACRN_DBG_IOMMU, "ignore dmar_unit @0x%x", dmar_unit->drhd->reg_base_addr);
+			}
 		}
 	}
 }
