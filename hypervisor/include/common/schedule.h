3e9f4b958d98f8da97eab5b0a0d2871a8e3c760d
Shuo A Liu <shuo.a.liu@intel.com>
2018-12-25 14:48:17 +0800

    hv: clear NEED_RESCHEDULE flag in schedule
    Now, need_reschedule will test_and_clear the bit NEED_RESCHEDULE in
    schedule context, then call schedule. It is not a exact match with the
    name.
    This patch move the flag clearing into scheudle, and need_reschedule
    just check and return.
    
    Tracked-On: #1821
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>
    Reviewed-by: Yin Fengwei <fengwei.yin@intel.com>

--- hypervisor/include/common/schedule.h
+++ hypervisor/include/common/schedule.h
@@ -32,7 +32,7 @@ void remove_vcpu_from_runqueue(struct acrn_vcpu *vcpu);
 void default_idle(void);
 
 void make_reschedule_request(const struct acrn_vcpu *vcpu);
-int32_t need_reschedule(uint16_t pcpu_id);
+bool need_reschedule(uint16_t pcpu_id);
 void make_pcpu_offline(uint16_t pcpu_id);
 int32_t need_offline(uint16_t pcpu_id);
 
