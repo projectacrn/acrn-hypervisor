d2b03e8c33dae1dc4ada85ca7d7aad8076995a0e
Tw <wei.tan@intel.com>
2019-01-04 16:14:43 +0800

    dm:script: remove ioc related stuff from uos on UP2
    UP2 don't have ioc, there is no need to simulate it.
    
    Tracked-On: #1995
    Signed-off-by: Tw <wei.tan@intel.com>
    Reviewed-by: Binbin Wu <binbin.wu@intel.com>

--- devicemodel/samples/up2/launch_uos.sh
+++ devicemodel/samples/up2/launch_uos.sh
@@ -154,8 +154,6 @@ acrn-dm -A -m $mem_size -c $2$boot_GVT_option"$GVT_args" -s 0:0,hostbridge -s 1:
   $boot_cse_option \
   $intr_storm_monitor \
   $boot_ipu_option      \
-  -i /run/acrn/ioc_$vm_name,0x20 \
-  -l com2,/run/acrn/ioc_$vm_name \
   --mac_seed $mac_seed \
   -B "root=/dev/vda2 rw rootwait maxcpus=$2 nohpet console=hvc0 \
   console=ttyS0 no_timer_check ignore_loglevel log_buf_len=16M \
@@ -344,8 +342,6 @@ fi
    $intr_storm_monitor \
    $boot_ipu_option      \
    --mac_seed $mac_seed \
-   -i /run/acrn/ioc_$vm_name,0x20 \
-   -l com2,/run/acrn/ioc_$vm_name \
    $boot_image_option \
    --enable_trusty \
    -B "$kernel_cmdline" $vm_name
