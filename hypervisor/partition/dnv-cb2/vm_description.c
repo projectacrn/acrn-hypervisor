0fcbc70eeb60d4070171541b9a55be6884cccff3
Victor Sun <victor.sun@intel.com>
2019-01-22 09:37:04 +0800

    HV: move Kconfig IOREQ_POLLING to acrn vm config
    Previously I/O emulation completion mode mode was configured in Kconfig:
    
    	config IOREQ_NOTIFICATION
    	       bool "Notification mode"
    	       help
    	         When I/O request is completed, SOS will mark the completion status and
    	         notify hypervisor via hypercall. Hypervisor will finish the post work
    	         when notification is received.
    
    	config IOREQ_POLLING
    	       bool "Polling mode"
    	       help
    	         When I/O request is completed, SOS will only mark completion status
    	         without notifying hypervisor. Hypervisor will poll the completion
    	         status and finish the post work.
    
    Now move this configuration to guest_flags of acrn_vm_config struct.
    
    	if ((vm_config->guest_flags & IO_COMPLETION_POLLING) != 0U) {
    		I/O with polling;
    	} else {
    		I/O with notification;
    	}
    
    Tracked-On: #2291
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/partition/dnv-cb2/vm_description.c
+++ hypervisor/partition/dnv-cb2/vm_description.c
@@ -177,7 +177,7 @@ struct vm_config_arraies vm_config_partition = {
 			{
 				.type = PRE_LAUNCHED_VM,
 				.pcpu_bitmap = (PLUG_CPU(0) | PLUG_CPU(2) | PLUG_CPU(4) | PLUG_CPU(6)),
-				.guest_flags = LAPIC_PASSTHROUGH,
+				.guest_flags = LAPIC_PASSTHROUGH | IO_COMPLETION_POLLING,
 				.memory.start_hpa = 0x100000000UL,
 				.memory.size = 0x80000000UL, /* uses contiguous memory from host */
 				.vm_vuart = true,
@@ -190,7 +190,7 @@ struct vm_config_arraies vm_config_partition = {
 			{
 				.type = PRE_LAUNCHED_VM,
 				.pcpu_bitmap = (PLUG_CPU(1) | PLUG_CPU(3) | PLUG_CPU(5) | PLUG_CPU(7)),
-				.guest_flags = LAPIC_PASSTHROUGH,
+				.guest_flags = LAPIC_PASSTHROUGH | IO_COMPLETION_POLLING,
 				.memory.start_hpa = 0x180000000UL,
 				.memory.size = 0x80000000UL, /* uses contiguous memory from host */
 				.vm_vuart = true,
