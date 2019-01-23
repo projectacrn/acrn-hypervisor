c20d095a6332f5d8b024f21cde2f5789bad45a33
Victor Sun <victor.sun@intel.com>
2019-01-23 15:52:37 +0800

    HV: refine sos_vm config header
    The sos_vm config under arch/x86/configs/$(CONFIG_BOARD) folder should be
    customer specific configuration, leave it back to customer.
    
    When hypervisor start building, it will check whether BOARD specific sos_vm.h
    exist in configs/$(CONFIG_BOARD) folder. If exist, then include this header;
    if not, then include default sos_vm config header under configs/default folder.
    
    Tracked-On: #2291
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/arch/x86/configs/sharing_config.c
+++ hypervisor/arch/x86/configs/sharing_config.c
@@ -14,7 +14,7 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] __aligned(PAGE_SIZE) = {
 		.guest_flags = SOS_VM_CONFIG_GUEST_FLAGS,
 		.memory = {
 			.start_hpa = 0x0UL,
-			.size = SOS_VM_CONFIG_MEM_SIZE,
+			.size = CONFIG_SOS_RAM_SIZE,
 		},
 		.os_config = {
 			.name = SOS_VM_CONFIG_OS_NAME,
