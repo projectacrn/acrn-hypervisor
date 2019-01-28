135ed80f51b0b5244f0bbdfed5691caceddb0f55
Yin Fengwei <fengwei.yin@intel.com>
2019-01-28 16:09:01 +0800

    watchdog: map the watchdog reset to warm reset
    Per debugging requirement, map the watchdog reset to warm reset.
    So the ramconsole could be used to capture the kernel log of UOS
    before watchdog is hit.
    
    Tracked-On: #2471
    Signed-off-by: Yin Fengwei <fengwei.yin@intel.com>

--- devicemodel/hw/pci/wdt_i6300esb.c
+++ devicemodel/hw/pci/wdt_i6300esb.c
@@ -136,7 +136,8 @@ wdt_expired_handler(void *arg)
 			wdt_timeout = 1;
 
 			/* watchdog timer out, set the uos to reboot */
-			vm_set_suspend_mode(VM_SUSPEND_FULL_RESET);
+			vm_set_suspend_mode(VM_SUSPEND_SYSTEM_RESET);
+			notify_vmloop_thread();
 			mevent_notify();
 		} else {
 			/* if not need reboot, just loop timer */
