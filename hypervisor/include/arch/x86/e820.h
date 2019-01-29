615c2bf88bc14353254b960baaf3f27df3df4349
Mingqiang Chi <mingqiang.chi@intel.com>
2019-01-29 17:04:39 +0800

    hv:move e820 related macro and structure to e820.h
    move 'struct e820_entry' 'E820_TYPE_XXX' from mmu.h
    to e820.h
    
    Tracked-On: #1842
    Signed-off-by: Mingqiang Chi <mingqiang.chi@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/include/arch/x86/e820.h
+++ hypervisor/include/arch/x86/e820.h
@@ -7,6 +7,24 @@
 #ifndef E820_H
 #define E820_H
 
+/* E820 memory types */
+#define E820_TYPE_RAM		1U	/* EFI 1, 2, 3, 4, 5, 6, 7 */
+#define E820_TYPE_RESERVED	2U
+/* EFI 0, 11, 12, 13 (everything not used elsewhere) */
+#define E820_TYPE_ACPI_RECLAIM	3U	/* EFI 9 */
+#define E820_TYPE_ACPI_NVS	4U	/* EFI 10 */
+#define E820_TYPE_UNUSABLE	5U	/* EFI 8 */
+
+/** Defines a single entry in an E820 memory map. */
+struct e820_entry {
+   /** The base address of the memory range. */
+	uint64_t baseaddr;
+   /** The length of the memory range. */
+	uint64_t length;
+   /** The type of memory region. */
+	uint32_t type;
+} __packed;
+
 struct e820_mem_params {
 	uint64_t mem_bottom;
 	uint64_t mem_top;
