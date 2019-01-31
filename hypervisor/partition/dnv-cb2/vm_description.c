11bfe3d43ed9c6ceeeced1af7ff5dab8cf9c69d6
Victor Sun <victor.sun@intel.com>
2019-01-31 13:35:51 +0800

    HV: move e820 entry out of vm description
    move e820_default_entries[] from vm_description.c to ve820.c and rename
    to ve820_entry[] as a temparary solution for partiton mode e820 management;
    
    Tracked-On: #2291
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/partition/dnv-cb2/vm_description.c
+++ hypervisor/partition/dnv-cb2/vm_description.c
@@ -236,35 +236,3 @@ const struct pcpu_vm_config_mapping pcpu_vm_config_map[] = {
 		.is_bsp = true,
 	},
 };
-
-const struct e820_entry e820_default_entries[NUM_E820_ENTRIES] = {
-	{	/* 0 to mptable */
-		.baseaddr =  0x0U,
-		.length   =  0xEFFFFU,
-		.type     =  E820_TYPE_RAM
-	},
-
-	{	/* mptable 65536U */
-		.baseaddr =  0xF0000U,
-		.length   =  0x10000U,
-		.type     =  E820_TYPE_RESERVED
-	},
-
-	{	/* mptable to lowmem */
-		.baseaddr =  0x100000U,
-		.length   =  0x7FF00000U,
-		.type     =  E820_TYPE_RAM
-	},
-
-	{	/* lowmem to PCI hole */
-		.baseaddr =  0x80000000U,
-		.length   =  0x40000000U,
-		.type     =  E820_TYPE_RESERVED
-	},
-
-	{	/* PCI hole to 4G */
-		.baseaddr =  0xe0000000U,
-		.length   =  0x20000000U,
-		.type     =  E820_TYPE_RESERVED
-	},
-};
