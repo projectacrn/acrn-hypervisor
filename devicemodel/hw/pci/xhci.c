76a5131b37cfdf7bdf069227002609a65e5a7cf9
Xiaoguang Wu <xiaoguang.wu@intel.com>
2019-02-17 13:14:11 +0800

    DM USB: xHCI: fix PORTSC error in 'pci_xhci_cmd_disable_slot'
    The disable slot command is used for forcing a device slot to enter into
    disconnect state, a typical usage would be to free a Device Slot when an
    USB device is disconnected. But it should not impact the portsc value.
    
    Tracked-On: #2576
    Signed-off-by: Conghui Chen <conghui.chen@intel.com>
    Signed-off-by: Xiaoguang Wu <xiaoguang.wu@intel.com>
    Acked-by: Yu Wang <yu1.wang@intel.com>

--- devicemodel/hw/pci/xhci.c
+++ devicemodel/hw/pci/xhci.c
@@ -1820,9 +1820,6 @@ pci_xhci_cmd_disable_slot(struct pci_xhci_vdev *xdev, uint32_t slot)
 			break;
 
 	if (i <= XHCI_MAX_DEVS && XHCI_PORTREG_PTR(xdev, i)) {
-		XHCI_PORTREG_PTR(xdev, i)->portsc &= ~(XHCI_PS_CSC |
-				XHCI_PS_CCS | XHCI_PS_PED | XHCI_PS_PP);
-
 		udev = dev->dev_instance;
 		assert(udev);
 
