e38ff18be30a23e757a2f2de0a0d5dc61906f538
Mingqiang Chi <mingqiang.chi@intel.com>
2019-02-28 10:58:32 +0800

    hv:cleanup header files for release folder
    cleanup release folder, only include some necessary
    header files,doesn't include hypervisor.h
    
    Tracked-On: #1842
    Signed-off-by: Mingqiang Chi <mingqiang.chi@intel.com>
    
    	modified:   release/console.c
    	modified:   release/dump.c
    	modified:   release/hypercall.c
    	modified:   release/logmsg.c
    	modified:   release/npk_log.c
    	modified:   release/profiling.c
    	modified:   release/trace.c
    	modified:   release/vuart.c

--- hypervisor/release/dump.c
+++ hypervisor/release/dump.c
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <hypervisor.h>
+#include <types.h>
+#include <irq.h>
 
 void dump_intr_excp_frame(__unused const struct intr_excp_ctx *ctx) {}
 void dump_exception(__unused struct intr_excp_ctx *ctx, __unused uint16_t pcpu_id) {}
