cb20ad3a571c8014b81402e9ea99cce0ae104579
Victor Sun <victor.sun@intel.com>
2019-03-05 11:21:54 +0800

    HV: refine location of platform_acpi_info header
    platform_acpi_info.h should be BOARD specific so move the header file
    to configs/$(CONFIG_BOARD)/ folder.
    
    So the target acpi info file detect process is:
    
    Compiler search for $(CONFIG_BOARD)_acpi_info.h under
    hypervisor/arch/x86/$(CONFIG_BOARD)/ first (the file is supposed to be
    generated by offline tool so it is not included in original source), if
    found, then the file is the target acpi info file. If not and at the
    same time the ENFORCE_VALIDATED_ACPI_INFO Kconfig was set, the compiler
    will stop and hint user to add target acpi info file by offline tool.
    If ENFORCE_VALIDATED_ACPI_INFO was unset, then compiler will continue to
    search for platform_acpi_info.h as target acpi info file under
    hypervisor/arch/x86/$(CONFIG_BOARD) folder (with this file we could bypass
    ENFORCE_VALIDATED_ACPI_INFO check and benifit for development). If this
    file is not exist, then hypervisor/arch/x86/configs/platform_acpi_info.h
    will be included as final target acpi info.
    
    The process should work for UEFI path also, with a valid platform acpi
    info file, S3/S5 should work on UEFI platform.
    
    Tracked-On: #2291
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

---
+++ hypervisor/arch/x86/configs/platform_acpi_info.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/* This is a template header file for platform ACPI info definition,
+ * we should use a user space tool running on target to generate
+ * board specific acpi info file named as $(CONFIG_BOARD)_acpi_info.h
+ * and put it in hypervisor/arch/x86/configs/$(CONFIG_BOARD)/.
+ */
+#ifndef PLATFORM_ACPI_INFO_H
+#define PLATFORM_ACPI_INFO_H
+
+/* pm sstate data */
+#define PM1A_EVT_ACCESS_SIZE	0U
+#define PM1A_EVT_ADDRESS	0UL
+#define PM1A_CNT_ADDRESS	0UL
+
+#define WAKE_VECTOR_32		0UL
+#define WAKE_VECTOR_64		0UL
+
+/* DRHD of DMAR */
+#define DRHD_COUNT		0U
+
+#define DRHD0_DEV_CNT		0U
+#define DRHD0_SEGMENT		0U
+#define DRHD0_FLAGS		0U
+#define DRHD0_REG_BASE		0UL
+#define DRHD0_IGNORE		false
+#define DRHD0_DEVSCOPE0_BUS	0U
+#define DRHD0_DEVSCOPE0_PATH	0U
+#define DRHD0_DEVSCOPE1_BUS	0U
+#define DRHD0_DEVSCOPE1_PATH	0U
+#define DRHD0_DEVSCOPE2_BUS	0U
+#define DRHD0_DEVSCOPE2_PATH	0U
+#define DRHD0_DEVSCOPE3_BUS	0U
+#define DRHD0_DEVSCOPE3_PATH	0U
+
+#define DRHD1_DEV_CNT		0U
+#define DRHD1_SEGMENT		0U
+#define DRHD1_FLAGS		0U
+#define DRHD1_REG_BASE		0UL
+#define DRHD1_IGNORE		false
+#define DRHD1_DEVSCOPE0_BUS	0U
+#define DRHD1_DEVSCOPE0_PATH	0U
+#define DRHD1_DEVSCOPE1_BUS	0U
+#define DRHD1_DEVSCOPE1_PATH	0U
+#define DRHD1_DEVSCOPE2_BUS	0U
+#define DRHD1_DEVSCOPE2_PATH	0U
+#define DRHD1_DEVSCOPE3_BUS	0U
+#define DRHD1_DEVSCOPE3_PATH	0U
+#define DRHD1_IOAPIC_ID		0U
+
+#define DRHD2_DEV_CNT		0U
+#define DRHD2_SEGMENT		0U
+#define DRHD2_FLAGS		0U
+#define DRHD2_REG_BASE		0U
+#define DRHD2_IGNORE		false
+#define DRHD2_DEVSCOPE0_BUS	0U
+#define DRHD2_DEVSCOPE0_PATH	0U
+#define DRHD2_DEVSCOPE1_BUS	0U
+#define DRHD2_DEVSCOPE1_PATH	0U
+#define DRHD2_DEVSCOPE2_BUS	0U
+#define DRHD2_DEVSCOPE2_PATH	0U
+#define DRHD2_DEVSCOPE3_BUS	0U
+#define DRHD2_DEVSCOPE3_PATH	0U
+
+#define DRHD3_DEV_CNT		0U
+#define DRHD3_SEGMENT		0U
+#define DRHD3_FLAGS		0U
+#define DRHD3_REG_BASE		0U
+#define DRHD3_IGNORE		false
+#define DRHD3_DEVSCOPE0_BUS	0U
+#define DRHD3_DEVSCOPE0_PATH	0U
+#define DRHD3_DEVSCOPE1_BUS	0U
+#define DRHD3_DEVSCOPE1_PATH	0U
+#define DRHD3_DEVSCOPE2_BUS	0U
+#define DRHD3_DEVSCOPE2_PATH	0U
+#define DRHD3_DEVSCOPE3_BUS	0U
+#define DRHD3_DEVSCOPE3_PATH	0U
+
+#endif	/* PLATFORM_ACPI_INFO_H */
