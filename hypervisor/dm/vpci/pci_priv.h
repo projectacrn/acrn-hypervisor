8afb6c99c6594b8951d42040599088aa68d89ff7
dongshen <dongsheng.x.zhang@intel.com>
2019-03-05 16:07:45 -0800

    HV: declare and export sharing mode's vdev functions as global instead of static local
    Export the following functions as global instead of static so that they can be
    called/referred outside of the files where they are declared:
    vmsi_init
    vmsi_cfgread
    vmsi_cfgwrite
    vmsi_deinit
    vmsix_init
    vmsix_cfgread
    vmsix_cfgwrite
    vmsix_deinit
    
    This is in preparation for removal of the sharing modes vdev ops, and call them
    directly instead.
    
    Tracked-On: #2534
    Signed-off-by: dongshen <dongsheng.x.zhang@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/dm/vpci/pci_priv.h
+++ hypervisor/dm/vpci/pci_priv.h
@@ -73,6 +73,16 @@ extern const struct vpci_ops partition_mode_vpci_ops;
 extern const struct vpci_ops sharing_mode_vpci_ops;
 extern const struct pci_vdev_ops pci_ops_vdev_msi;
 extern const struct pci_vdev_ops pci_ops_vdev_msix;
+
+int32_t vmsi_init(struct pci_vdev *vdev);
+int32_t vmsi_cfgread(const struct pci_vdev *vdev, uint32_t offset, uint32_t bytes, uint32_t *val);
+int32_t vmsi_cfgwrite(struct pci_vdev *vdev, uint32_t offset, uint32_t bytes, uint32_t val);
+int32_t vmsi_deinit(struct pci_vdev *vdev);
+
+int32_t vmsix_init(struct pci_vdev *vdev);
+int32_t vmsix_cfgread(const struct pci_vdev *vdev, uint32_t offset, uint32_t bytes, uint32_t *val);
+int32_t vmsix_cfgwrite(struct pci_vdev *vdev, uint32_t offset, uint32_t bytes, uint32_t val);
+int32_t vmsix_deinit(struct pci_vdev *vdev);
 #endif
 
 uint32_t pci_vdev_read_cfg(const struct pci_vdev *vdev, uint32_t offset, uint32_t bytes);
