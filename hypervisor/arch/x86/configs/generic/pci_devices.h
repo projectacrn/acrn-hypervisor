6378b74db4a1608ca532ee90e8b843bcc34046b4
Victor Sun <victor.sun@intel.com>
2019-04-29 01:36:14 +0800

    HV: add generic board config
    hypervisor/arch/x86/configs/($CONFIG_BOARD) will store Board specific configs,
    add a generic folder to store configs for generic boards that they can share
    one build binary with minimal and common features in some scenarios like SDC.
    
    Please be aware that BDF of pci devices are different on each board, so one
    binary for LOGICAL_PARTITION scenario is not supported.
    
    Tracked-On: #2291
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

---
+++ hypervisor/arch/x86/configs/generic/pci_devices.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2019 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PCI_DEVICES_H_
+#define PCI_DEVICES_H_
+
+#error "This is a dummy pci_devices.h, please generate PCI BDF info for your board."
+
+#endif /* PCI_DEVICES_H_ */
