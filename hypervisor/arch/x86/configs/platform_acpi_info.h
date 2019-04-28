00ebe8630fffa3f9404abd50cccec687065425ed
Victor Sun <victor.sun@intel.com>
2019-04-29 01:37:41 +0800

    HV: remove dummy DRHD in template platform acpi info
    The DRHD MACROs are needed only when CONFIG_DMAR_PARSE_ENABLED set to n,
    whereas the template platform_acpi_info.h is prepared for generic boards
    that usually CONFIG_DMAR_PARSE_ENABLED is set to y, so remove these dummy
    DRHD info MACROs.
    
    Tracked-On: #2291
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/arch/x86/configs/platform_acpi_info.h
+++ hypervisor/arch/x86/configs/platform_acpi_info.h
@@ -20,64 +20,10 @@
 #define WAKE_VECTOR_32		0UL
 #define WAKE_VECTOR_64		0UL
 
-/* DRHD of DMAR */
-#define DRHD_COUNT		0U
-
-#define DRHD0_DEV_CNT		0U
-#define DRHD0_SEGMENT		0U
-#define DRHD0_FLAGS		0U
-#define DRHD0_REG_BASE		0UL
-#define DRHD0_IGNORE		false
-#define DRHD0_DEVSCOPE0_BUS	0U
-#define DRHD0_DEVSCOPE0_PATH	0U
-#define DRHD0_DEVSCOPE1_BUS	0U
-#define DRHD0_DEVSCOPE1_PATH	0U
-#define DRHD0_DEVSCOPE2_BUS	0U
-#define DRHD0_DEVSCOPE2_PATH	0U
-#define DRHD0_DEVSCOPE3_BUS	0U
-#define DRHD0_DEVSCOPE3_PATH	0U
-
-#define DRHD1_DEV_CNT		0U
-#define DRHD1_SEGMENT		0U
-#define DRHD1_FLAGS		0U
-#define DRHD1_REG_BASE		0UL
-#define DRHD1_IGNORE		false
-#define DRHD1_DEVSCOPE0_BUS	0U
-#define DRHD1_DEVSCOPE0_PATH	0U
-#define DRHD1_DEVSCOPE1_BUS	0U
-#define DRHD1_DEVSCOPE1_PATH	0U
-#define DRHD1_DEVSCOPE2_BUS	0U
-#define DRHD1_DEVSCOPE2_PATH	0U
-#define DRHD1_DEVSCOPE3_BUS	0U
-#define DRHD1_DEVSCOPE3_PATH	0U
-#define DRHD1_IOAPIC_ID		0U
-
-#define DRHD2_DEV_CNT		0U
-#define DRHD2_SEGMENT		0U
-#define DRHD2_FLAGS		0U
-#define DRHD2_REG_BASE		0U
-#define DRHD2_IGNORE		false
-#define DRHD2_DEVSCOPE0_BUS	0U
-#define DRHD2_DEVSCOPE0_PATH	0U
-#define DRHD2_DEVSCOPE1_BUS	0U
-#define DRHD2_DEVSCOPE1_PATH	0U
-#define DRHD2_DEVSCOPE2_BUS	0U
-#define DRHD2_DEVSCOPE2_PATH	0U
-#define DRHD2_DEVSCOPE3_BUS	0U
-#define DRHD2_DEVSCOPE3_PATH	0U
-
-#define DRHD3_DEV_CNT		0U
-#define DRHD3_SEGMENT		0U
-#define DRHD3_FLAGS		0U
-#define DRHD3_REG_BASE		0U
-#define DRHD3_IGNORE		false
-#define DRHD3_DEVSCOPE0_BUS	0U
-#define DRHD3_DEVSCOPE0_PATH	0U
-#define DRHD3_DEVSCOPE1_BUS	0U
-#define DRHD3_DEVSCOPE1_PATH	0U
-#define DRHD3_DEVSCOPE2_BUS	0U
-#define DRHD3_DEVSCOPE2_PATH	0U
-#define DRHD3_DEVSCOPE3_BUS	0U
-#define DRHD3_DEVSCOPE3_PATH	0U
+#ifndef CONFIG_DMAR_PARSE_ENABLED
+#error "The template platform_acpi_info.h would not include DRHD info MACROs, if your Kconfig disabled \
+ACPI DMA Remapping tables parsing, please include DRHD info MACROs in your board specific platform_acpi_info.h \
+and put the file under hypervisor/arch/x86/configs/($CONFIG_BOARD)/."
+#endif
 
 #endif	/* PLATFORM_ACPI_INFO_H */
