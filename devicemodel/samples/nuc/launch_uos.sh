d9717967d768e88229b8f9786cd82f6e69fd99f1
lijianpi <wilson.li@intel.com>
2019-05-08 10:03:48 +0800

    dm:add grep -w option for uos launch sh
    for vm_name  with vxworks_vm1  or zephyr_vm1 or vm1
    grep "vm1" will match three of them, add -w option fix this issue
    Tracked-On:#3081
    Signed-off-by: zhangyun <yunx.zhang@intel.com>

--- devicemodel/samples/nuc/launch_uos.sh
+++ devicemodel/samples/nuc/launch_uos.sh
@@ -15,7 +15,7 @@ mac_seed=${mac:9:8}-${vm_name}
 
 #check if the vm is running or not
 vm_ps=$(pgrep -a -f acrn-dm)
-result=$(echo $vm_ps | grep "${vm_name}")
+result=$(echo $vm_ps | grep -w "${vm_name}")
 if [[ "$result" != "" ]]; then
   echo "$vm_name is running, can't create twice!"
   exit
