089aeddf995a1692edd4e15ed058877171a6ed7f
Zide Chen <zide.chen@intel.com>
2019-05-08 22:39:05 -0700

    hv: shutdown guest VM upon triple fault exceptions
    This patch implements triple fault vmexit handler and base on VM types:
    
    - post-launched VMs: shutdown_target_vm() injects S5 PIO write to request
      DM to shut down the target VM.
    - pre-launched VMs: shut down the guest.
    - SOS: similarly, but shut down all the non real-time post-launched VMs that
      depend to SOS before shutting down SOS.
    
    Tracked-On: #2700
    Signed-off-by: Zide Chen <zide.chen@intel.com>
    Signed-off-by: Sainath Grandhi <sainath.grandhi@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/include/arch/x86/guest/vm_reset.h
+++ hypervisor/include/arch/x86/guest/vm_reset.h
@@ -9,5 +9,6 @@
 
 void register_reset_port_handler(struct acrn_vm *vm);
 void shutdown_vm_from_idle(uint16_t pcpu_id);
+void triple_fault_shutdown_vm(struct acrn_vm *vm);
 
 #endif /* VM_RESET_H_ */
