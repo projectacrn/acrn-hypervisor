3221385d2b01a830deef2cdeb14b451fab92ccfe
Victor Sun <victor.sun@intel.com>
2019-05-27 15:44:50 +0800

    HV: add board specific config header
    Use a misc_cfg.h in each board configs folder so that VM configurations
    could include board specific MACROs;
    
    Tracked-On: #3214
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>

---
+++ hypervisor/arch/x86/configs/apl-mrb/misc_cfg.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2019 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MISC_CFG_H
+#define MISC_CFG_H
+
+#define ROOTFS_0		"root=/dev/sda3 "
+#define ROOTFS_1		"root=/dev/mmcblk1p1 "
+
+#define SOS_CONSOLE		"console=ttyS2 "
+
+#endif /* MISC_CFG_H */
