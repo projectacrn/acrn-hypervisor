a3f3bbce1b66241e689dbab0cc72e3ce4d66cae4
Zhao Yakui <yakui.zhao@intel.com>
2019-06-04 09:29:39 +0800

    ACRN/HV: Add one new board configuration for ACRN-hypervisor
    The memory size and IOMMU number are refined to meet with ICL board requirement.
    Otherwise the ACRN hypervisor can't be booted on the new ICL board.
    ICL(the abbreviation of Ice Lake) is the next generation platform based on 10nm.
    CPU is based on Sunny Cove microarchitecture and GPU is based on gen11.
    The new board is named as icl-rvp.
    
    Tracked-On: #3216
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>
    Reviewed-by: Binbin Wu <binbin.wu@intel.com>

---
+++ hypervisor/arch/x86/configs/icl-rvp/board.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2019 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <board.h>
+
+struct platform_clos_info platform_clos_array[0];
+uint16_t platform_clos_num = 0;
