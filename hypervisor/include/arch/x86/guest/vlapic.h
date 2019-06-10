7cb71a317e08234aaeb143dede9036bf4c453dd1
Sainath Grandhi <sainath.grandhi@intel.com>
2019-06-10 15:33:35 -0700

    hv: Make is_x2apic_enabled API visible across source code
    Remove static and inline attributes to the API is_x2apic_enabled
    and declare a prototype in vlapic.h. Also fix the check performed on guest
    APICBASE_MSR value to query vLAPIC mode.
    
    Tracked-On: #3253
    Signed-off-by: Sainath Grandhi <sainath.grandhi@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/include/arch/x86/guest/vlapic.h
+++ hypervisor/include/arch/x86/guest/vlapic.h
@@ -211,6 +211,7 @@ void vlapic_calc_dest(struct acrn_vm *vm, uint64_t *dmask, bool is_broadcast,
 void vlapic_calc_dest_lapic_pt(struct acrn_vm *vm, uint64_t *dmask, bool is_broadcast,
 		uint32_t dest, bool phys);
 bool is_lapic_pt_enabled(struct acrn_vm *vm);
+bool is_x2apic_enabled(const struct acrn_vlapic *vlapic);
 /**
  * @}
  */
