d5c3523d304ee6ae02fb04972590457f00cc63ea
Kaige Fu <kaige.fu@intel.com>
2019-10-24 16:38:18 +0000

    hv: Update industry scenarios configuration
    This patch makes the following changes:
      - Remove the 4th VM
      - Make the default vcpu num of RTVM as 2
    
    ---
      v1 -> v2: Modify CONFIG_MAX_VM_NUM to 3U + KATA
    
    Tracked-On: #3925
    Signed-off-by: Yan, Like <like.yan@intel.com>
    Signed-off-by: Kaige Fu <kaige.fu@intel.com>

--- hypervisor/scenarios/industry/vm_configurations.c
+++ hypervisor/scenarios/industry/vm_configurations.c
@@ -68,7 +68,7 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 
 		/* The hard RTVM must be launched as VM2 */
 		.guest_flags = GUEST_FLAG_HIGHEST_SEVERITY,
-		.vcpu_num = 1U,
+		.vcpu_num = 2U,
 		.vcpu_affinity = VM2_CONFIG_VCPU_AFFINITY,
 		.vuart[0] = {
 			.type = VUART_LEGACY_PIO,
@@ -83,22 +83,4 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 			.t_vuart.vuart_id = 1U,
 		},
 	},
-	{
-		.load_order = POST_LAUNCHED_VM,
-		.uuid = {0x38U, 0x15U, 0x88U, 0x21U, 0x52U, 0x08U, 0x40U, 0x05U,	\
-			 0xb7U, 0x2aU, 0x8aU, 0x60U, 0x9eU, 0x41U, 0x90U, 0xd0U},
-			/* 38158821-5208-4005-b72a-8a609e4190d0 */
-		.vcpu_num = 1U,
-		.vcpu_affinity = VM3_CONFIG_VCPU_AFFINITY,
-		.vuart[0] = {
-			.type = VUART_LEGACY_PIO,
-			.addr.port_base = COM1_BASE,
-			.irq = COM1_IRQ,
-		},
-		.vuart[1] = {
-			.type = VUART_LEGACY_PIO,
-			.addr.port_base = INVALID_COM_BASE,
-		}
-
-	}
 };
