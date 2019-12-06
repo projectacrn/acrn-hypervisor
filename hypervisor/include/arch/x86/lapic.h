0f1bff34f41860eb55d9c813bb6f19240206539b
Kaige Fu <kaige.fu@intel.com>
2019-12-06 09:52:11 +0000

    HV: Remove INIT signal notification related code
    We don't use INIT signal notification method now. This patch
    removes them.
    
    Tracked-On: #3886
    Acked-by: Eddie Dong <eddie.dong@intel.com>
    Signed-off-by: Kaige Fu <kaige.fu@intel.com>

--- hypervisor/include/arch/x86/lapic.h
+++ hypervisor/include/arch/x86/lapic.h
@@ -175,15 +175,6 @@ void send_single_ipi(uint16_t pcpu_id, uint32_t vector);
 /* End of ipi_ext_apis */
 
 /**
- * @brief Send an INIT signal to a single pCPU
- *
- * @param[in] pcpu_id The id of destination physical cpu
- *
- * @return None
- */
-void send_single_init(uint16_t pcpu_id);
-
-/**
  * @brief Send an NMI signal to a single pCPU
  *
  * @param[in] pcpu_id The id of destination physical cpu
