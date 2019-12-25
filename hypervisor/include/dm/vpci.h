5674475c21118fd0126276328d945c5e3d87638a
Li Fei1 <fei1.li@intel.com>
2019-12-25 19:41:22 +0800

    hv: vpci: an assign PT device should support FLR or PM reset
    Before we assign a PT device to post-launched VM, we should reset the PCI device
    first. However, ACRN hypervisor doesn't plan to support PCIe hot-plug and doesn't
    support PCIe bridge Secondary Bus Reset. So the PT device must support FLR or PM
    reset. This patch do this check when assigning a PT device to post-launched VM.
    
    Tracked-On: #3465
    Signed-off-by: Li Fei1 <fei1.li@intel.com>

--- hypervisor/include/dm/vpci.h
+++ hypervisor/include/dm/vpci.h
@@ -130,6 +130,7 @@ struct acrn_vpci {
 extern const struct pci_vdev_ops vhostbridge_ops;
 void vpci_init(struct acrn_vm *vm);
 void vpci_cleanup(struct acrn_vm *vm);
+struct pci_vdev *pci_find_vdev(struct acrn_vpci *vpci, union pci_bdf vbdf);
 void vpci_set_ptdev_intr_info(struct acrn_vm *target_vm, uint16_t vbdf, uint16_t pbdf);
 void vpci_reset_ptdev_intr_info(struct acrn_vm *target_vm, uint16_t vbdf, uint16_t pbdf);
 
