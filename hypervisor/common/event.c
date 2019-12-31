6c221e958d4c907b8e2a903e271b63929ddaf423
Shuo A Liu <shuo.a.liu@intel.com>
2019-12-31 12:55:01 +0800

    hv: sched: simple event implemention
    This simple event implemention can only support exclusive waiting
    at same time. It mainly used by thread who want to wait for special event
    happens.
      Thread A who want to wait for some events calls
    	wait_event(struct sched_event *);
    
      Thread B who can give the event signal calls
    	signal_event(struct sched_event *);
    
    Tracked-On: #4329
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

---
+++ hypervisor/common/event.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <cpu.h>
+#include <schedule.h>
+#include <event.h>
+#include <logmsg.h>
+
+void init_event(struct sched_event *event)
+{
+	spinlock_init(&event->lock);
+	event->set = false;
+	event->waiting_thread = NULL;
+}
+
+void reset_event(struct sched_event *event)
+{
+	uint64_t rflag;
+
+	spinlock_irqsave_obtain(&event->lock, &rflag);
+	event->set = false;
+	event->waiting_thread = NULL;
+	spinlock_irqrestore_release(&event->lock, rflag);
+}
+
+/* support exclusive waiting only */
+void wait_event(struct sched_event *event)
+{
+	uint64_t rflag;
+
+	spinlock_irqsave_obtain(&event->lock, &rflag);
+	ASSERT((event->waiting_thread == NULL), "only support exclusive waiting");
+	while (!event->set) {
+		event->waiting_thread = sched_get_current(get_pcpu_id());
+		sleep_thread(event->waiting_thread);
+		spinlock_irqrestore_release(&event->lock, rflag);
+		schedule();
+		spinlock_irqsave_obtain(&event->lock, &rflag);
+	}
+	event->set = false;
+	event->waiting_thread = NULL;
+	spinlock_irqrestore_release(&event->lock, rflag);
+}
+
+void signal_event(struct sched_event *event)
+{
+	uint64_t rflag;
+
+	spinlock_irqsave_obtain(&event->lock, &rflag);
+	event->set = true;
+	if (event->waiting_thread != NULL) {
+		wake_thread(event->waiting_thread);
+	}
+	spinlock_irqrestore_release(&event->lock, rflag);
+}
