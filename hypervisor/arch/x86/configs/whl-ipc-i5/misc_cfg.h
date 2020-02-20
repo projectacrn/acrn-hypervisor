37ca59370e7b1ca264bdc6d477fd17d5eb434197
Vijay Dhanraj <vijay.dhanraj@intel.com>
2020-02-19 23:46:54 -0800

    HV: Add WhiskeyLake board configuration files
    This patch adds offline tool generated WhiskeyLake
    board configurations files.
    
    Tracked-On: #3715
    Signed-off-by: Vijay Dhanraj <vijay.dhanraj@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

---
+++ hypervisor/arch/x86/configs/whl-ipc-i5/misc_cfg.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2019 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+
+#ifndef MISC_CFG_H
+#define MISC_CFG_H
+
+#define MAX_PCPU_NUM	4U
+#define MAX_PLATFORM_CLOS_NUM	0U
+#define ROOTFS_0		"root=/dev/nvme0n1p3 "
+#define ROOTFS_1		"root=/dev/sda3 "
+
+
+
+#define MAX_HIDDEN_PDEVS_NUM	0U
+
+#endif /* MISC_CFG_H */
