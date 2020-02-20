2d42c4f414135764be031475c2370c44f9cec2b5
Junming Liu <junming.liu@intel.com>
2020-02-20 10:15:39 +0000

    dm:add an extra lpc bridge when enabling gvt-d
    Intel Graphics Device(IGD) passthrough on Windows guest
    has the restriction that it need a lpc bridge device
    located in 00:1f.0 PCI slot.
    
    This patch add a 'gpu' option for 'passthrou' type device,
    which will create the dedicated "igd-lpc" for IGD passthrough.
    
    Tracked-On: #4405
    
    v2 -> v3:
            * refine the commit message and comments.
    
    Signed-off-by: Junming Liu <junming.liu@intel.com>
    Reviewed-by: Shuo A Liu <shuo.a.liu@intel.com>
    Reviewed-by: Wu Binbin <binbin.wu@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

--- devicemodel/hw/pci/passthrough.c
+++ devicemodel/hw/pci/passthrough.c
@@ -808,7 +808,11 @@ passthru_init(struct vmctx *ctx, struct pci_vdev *dev, char *opts)
 			keep_gsi = true;
 		else if (!strncmp(opt, "no_reset", 8))
 			need_reset = false;
-		else
+		else if (!strncmp(opt, "gpu", 3)) {
+			/* Create the dedicated "igd-lpc" on 00:1f.0 for IGD passthrough */
+			if (pci_parse_slot("31,igd-lpc") != 0)
+				warnx("faild to create igd-lpc");
+		} else
 			warnx("Invalid passthru options:%s", opt);
 	}
 
