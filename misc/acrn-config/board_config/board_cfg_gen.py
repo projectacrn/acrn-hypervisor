ca7b530329909816aca9c8d775bd58af34e5e7cb
Wei Liu <weix.w.liu@intel.com>
2020-03-26 17:07:49 +0800

    acrn-config: remove the same parameters and functions from board_cfg_lib
    Board config should relay on library/common.py and board_cfg_lib, then
    remove the duplicate parameters and functions.
    
    Tracked-On: #3854
    Signed-off-by: Wei Liu <weix.w.liu@intel.com>
    Acked-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Terry Zou <terry.zou@intel.com>

--- misc/acrn-config/board_config/board_cfg_gen.py
+++ misc/acrn-config/board_config/board_cfg_gen.py
@@ -15,8 +15,8 @@ import misc_cfg_h
 import new_board_kconfig
 import common
 
-ACRN_PATH = board_cfg_lib.SOURCE_ROOT_DIR
-ACRN_CONFIG_TARGET = ACRN_PATH + "hypervisor/arch/x86/configs/"
+ACRN_PATH = common.SOURCE_ROOT_DIR
+ACRN_CONFIG_DEF = ACRN_PATH + "hypervisor/arch/x86/configs/"
 
 ACRN_DEFAULT_ACPI = ACRN_PATH + "hypervisor/include/arch/x86/default_acpi_info.h"
 GEN_FILE = ["pci_devices.h", "board.c", "_acpi_info.h", "misc_cfg.h", ".config"]
@@ -27,48 +27,50 @@ def main(args):
     This is main function to start generate source code related with board
     :param args: it is a command line args for the script
     """
-    global ACRN_CONFIG_TARGET
     err_dic = {}
 
-    (err_dic, board_info_file, scenario_info_file, output_folder) = board_cfg_lib.get_param(args)
+    (err_dic, board_info_file, scenario_info_file, output_folder) = common.get_param(args)
     if err_dic:
         return err_dic
 
     if output_folder:
-        ACRN_CONFIG_TARGET = os.path.abspath(output_folder) + '/'
+        common.ACRN_CONFIG_TARGET = os.path.abspath(output_folder) + '/'
 
     # check env
-    err_dic = board_cfg_lib.prepare()
+    err_dic = common.prepare()
     if err_dic:
         return err_dic
 
     common.BOARD_INFO_FILE = board_info_file
     common.SCENARIO_INFO_FILE = scenario_info_file
     common.get_vm_num(scenario_info_file)
-    board_cfg_lib.BOARD_INFO_FILE = board_info_file
-    board_cfg_lib.SCENARIO_INFO_FILE = scenario_info_file
-    board_cfg_lib.get_vm_num(scenario_info_file)
 
     # get board name
-    (err_dic, board) = board_cfg_lib.get_board_name()
+    (err_dic, board) = common.get_board_name()
     if err_dic:
         return err_dic
     board_cfg_lib.BOARD_NAME = board
 
     # check if this is the scenario config which matched board info
-    (err_dic, status) = board_cfg_lib.is_config_file_match()
+    (err_dic, status) = common.is_config_file_match()
     if not status:
         err_dic['board config: Not match'] = "The board xml and scenario xml should be matched"
         return err_dic
 
-    board_dir = ACRN_CONFIG_TARGET + board + '/'
-    board_cfg_lib.mkdir(board_dir)
+    if common.ACRN_CONFIG_TARGET:
+        board_dir = common.ACRN_CONFIG_TARGET + board + '/'
+    else:
+        board_dir = ACRN_CONFIG_DEF + board + '/'
+    common.mkdir(board_dir)
 
     config_pci = board_dir + GEN_FILE[0]
     config_board = board_dir + GEN_FILE[1]
     config_acpi = board_dir + board + GEN_FILE[2]
     config_misc_cfg = board_dir + GEN_FILE[3]
-    config_board_kconfig = ACRN_CONFIG_TARGET + board + GEN_FILE[4]
+    if common.ACRN_CONFIG_TARGET:
+        config_board_kconfig = common.ACRN_CONFIG_TARGET + board + GEN_FILE[4]
+    else:
+        config_board_kconfig = ACRN_CONFIG_DEF + board + GEN_FILE[4]
 
     # generate board.c
     with open(config_board, 'w+') as config:
@@ -108,7 +110,7 @@ def ui_entry_api(board_info, scenario_info):
 
     arg_list = ['board_cfg_gen.py', '--board', board_info, '--scenario', scenario_info]
 
-    err_dic = board_cfg_lib.prepare()
+    err_dic = common.prepare()
     if err_dic:
         return err_dic
 
@@ -123,4 +125,4 @@ if __name__ == '__main__':
     err_dic = main(ARGS)
     if err_dic:
         for err_k, err_v in err_dic.items():
-            board_cfg_lib.print_red("{}: {}".format(err_k, err_v), err=True)
+            common.print_red("{}: {}".format(err_k, err_v), err=True)
