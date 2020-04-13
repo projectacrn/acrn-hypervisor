1b7cd4cc77449821d3e065718cbda44b070deb7a
Victor Sun <victor.sun@intel.com>
2020-04-13 09:05:16 +0800

    HV: merge sos_pci_dev config to sos macro
    The pci_dev config settings of SOS are same so move the config interface
    from vm_configurations.c to CONFIG_SOS_VM macro;
    
    Tracked-On: #4616
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/scenarios/industry/vm_configurations.c
+++ hypervisor/scenarios/industry/vm_configurations.c
@@ -36,8 +36,6 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 			.t_vuart.vm_id = 2U,
 			.t_vuart.vuart_id = 1U,
 		},
-		.pci_dev_num = SOS_EMULATED_PCI_DEV_NUM,
-		.pci_devs = sos_pci_devs,
 	},
 	{	/* VM1 */
 		CONFIG_POST_STD_VM(1),
