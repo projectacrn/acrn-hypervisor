45f3fe8f4419dd6dba1e814a85839fe86d9d15c1
Wei Liu <weix.w.liu@intel.com>
2020-04-14 14:09:01 +0800

    acrn-config: support passthroug GVT for WaaG by default
    Modify launch config tool to support passthroug GVT for WaaG by default.
    
    Tracked-On: #4625
    Signed-off-by: Wei Liu <weix.w.liu@intel.com>
    Acked-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Terry Zou <terry.zou@intel.com>

--- misc/acrn-config/library/launch_cfg_lib.py
+++ misc/acrn-config/library/launch_cfg_lib.py
@@ -54,6 +54,7 @@ PM_CHANNEL_DIC = {
 }
 
 MOUNT_FLAG_DIC = {}
+GPU_BDF = "00:02.0"
 
 
 def usage(file_name):
@@ -548,3 +549,13 @@ def bdf_duplicate_check(bdf_dic):
                 return
             else:
                 bdf_used.append(dev_bdf)
+
+
+def get_gpu_vpid():
+
+    vpid = ''
+    vpid_lines = board_cfg_lib.get_info(common.BOARD_INFO_FILE, "<PCI_VID_PID>", "</PCI_VID_PID>")
+    for vpid_line in vpid_lines:
+        if GPU_BDF in vpid_line:
+            vpid = vpid_line.split()[2]
+    return vpid
