b99de16ff5259bca18549503d99e6f81a0fa9bf7
Sainath Grandhi <sainath.grandhi@intel.com>
2020-04-18 20:36:43 -0700

    hv: Wake up vCPU for interrupts from vPIC
    Wake up vCPUs that are blocked upon interrupts from vPIC.
    
    Tracked-On: #4664
    Signed-off-by: Sainath Grandhi <sainath.grandhi@intel.com>

--- hypervisor/arch/x86/guest/virq.c
+++ hypervisor/arch/x86/guest/virq.c
@@ -289,6 +289,7 @@ static bool vcpu_inject_lo_exception(struct acrn_vcpu *vcpu)
 void vcpu_inject_extint(struct acrn_vcpu *vcpu)
 {
 	vcpu_make_request(vcpu, ACRN_REQUEST_EXTINT);
+	signal_event(&vcpu->events[VCPU_EVENT_VIRTUAL_INTERRUPT]);
 }
 
 /* Inject NMI to guest */
