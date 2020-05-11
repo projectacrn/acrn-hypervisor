523ed38d92bf6e7665d4cba5a5fa0fcbbc30cb6c
David B. Kinder <david.b.kinder@intel.com>
2020-05-11 08:33:37 -0700

    doc: add 1.6.1 docs version menu choice
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

--- doc/conf.py
+++ doc/conf.py
@@ -189,6 +189,8 @@ else:
 html_context = {
    'current_version': current_version,
    'versions': ( ("latest", "/latest/"),
+                 ("1.6.1", "/1.6.1/"),
+                 ("1.6", "/1.6/"),
                  ("1.6", "/1.6/"),
                  ("1.5", "/1.5/"),
                  ("1.4", "/1.4/"),
