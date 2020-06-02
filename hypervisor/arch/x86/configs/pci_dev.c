b8f151a55fd882375d6ba1ad069b0b0550eb734b
Li Fei1 <fei1.li@intel.com>
2020-06-02 09:27:40 +0800

    hv: pci: check whether a PCI device is host bridge or not by class
    According PCI Code and ID Assignment Specification Revision 1.11, a PCI device
    whose Base Class is 06h and Sub-Class is 00h is a Host bridge.
    
    Tracked-On: #4550
    Signed-off-by: Li Fei1 <fei1.li@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/arch/x86/configs/pci_dev.c
+++ hypervisor/arch/x86/configs/pci_dev.c
@@ -9,13 +9,7 @@
 #include <pci_dev.h>
 #include <vpci.h>
 
-struct acrn_vm_pci_dev_config sos_pci_devs[CONFIG_MAX_PCI_DEV_NUM] = {
-	{
-		.emu_type = PCI_DEV_TYPE_HVEMUL,
-		.vbdf.bits = {.b = 0x00U, .d = 0x00U, .f = 0x00U},
-		.vdev_ops = &vhostbridge_ops,
-	},
-};
+struct acrn_vm_pci_dev_config sos_pci_devs[CONFIG_MAX_PCI_DEV_NUM];
 
 /*
  * @pre pdev != NULL;
@@ -28,24 +22,18 @@ static bool is_allocated_to_prelaunched_vm(struct pci_pdev *pdev)
 	struct acrn_vm_config *vm_config;
 	struct acrn_vm_pci_dev_config *dev_config;
 
-	for (vmid = 0U; vmid < CONFIG_MAX_VM_NUM; vmid++) {
+	for (vmid = 0U; (vmid < CONFIG_MAX_VM_NUM) && !found; vmid++) {
 		vm_config = get_vm_config(vmid);
-		if (vm_config->load_order != PRE_LAUNCHED_VM) {
-			continue;
-		}
-
-		for (pci_idx = 0U; pci_idx < vm_config->pci_dev_num; pci_idx++) {
-			dev_config = &vm_config->pci_devs[pci_idx];
-			if ((dev_config->emu_type == PCI_DEV_TYPE_PTDEV) &&
-					bdf_is_equal(dev_config->pbdf, pdev->bdf)) {
-				dev_config->pdev = pdev;
-				found = true;
-				break;
+		if (vm_config->load_order == PRE_LAUNCHED_VM) {
+			for (pci_idx = 0U; pci_idx < vm_config->pci_dev_num; pci_idx++) {
+				dev_config = &vm_config->pci_devs[pci_idx];
+				if ((dev_config->emu_type == PCI_DEV_TYPE_PTDEV) &&
+						bdf_is_equal(dev_config->pbdf, pdev->bdf)) {
+					dev_config->pdev = pdev;
+					found = true;
+					break;
+				}
 			}
-		}
-
-		if (found) {
-			break;
 		}
 	}
 
