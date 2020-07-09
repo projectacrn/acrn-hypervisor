288efd31b483f7ab8fe8db581245c378f2f4802f
zhanqi <sherry.qi.zhang@intel.com>
2020-07-09 19:02:32 +0800

    create acrn hypervisor/kernel deb packaging
    Tracked-On: #5022
    
    Signed-off-by: zhanqi <sherry.qi.zhang@intel.com>

---
+++ misc/packaging/compile_iasl.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#* Copyright (c) 2020 Intel Corporation
+import os,sys,copy,json
+import subprocess
+import datetime
+import time
+
+os.system('sudo -E apt-get install iasl')
+
+if os.path.exists('iasl_build'):
+	os.system('rm -rf iasl_build')
+os.system('mkdir -p iasl_build')
+
+cmd = "cd iasl_build" + "&&" +"wget https://acpica.org/sites/acpica/files/acpica-unix-20191018.tar.gz"
+os.system(cmd)
+
+cmd = "cd iasl_build" + "&&" +"tar zxvf acpica-unix-20191018.tar.gz"
+os.system(cmd)
+
+cmd = "cd iasl_build/acpica-unix-20191018" + "&&" +"make clean"
+os.system(cmd)
+
+cmd = "cd iasl_build/acpica-unix-20191018" + "&&" +"make iasl"
+os.system(cmd)
+
+cmd = "cd iasl_build/acpica-unix-20191018" + "&&" +"cp ./generate/unix/bin/iasl /usr/sbin/"
