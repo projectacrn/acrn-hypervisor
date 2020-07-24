03d75fe35bcc556663aefca4e73fc0ce240e4417
Victor Sun <victor.sun@intel.com>
2020-07-24 09:45:39 +0800

    TEST: DO not merge
    Signed-off-by: Victor Sun <victor.sun@intel.com>

--- hypervisor/scenarios/platform_acpi_info.h
+++ hypervisor/scenarios/platform_acpi_info.h
@@ -4,13 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-/* This is a template header file for generic platform ACPI info definition,
- * we should use acrn-config tool to generate board specific acpi info file
- * which named as $(CONFIG_BOARD)_acpi_info.h and put it under
- * hypervisor/arch/x86/configs/$(CONFIG_BOARD)/.
- */
 #ifndef PLATFORM_ACPI_INFO_H
 #define PLATFORM_ACPI_INFO_H
+
+
 
 /* pm sstate data */
 #define PM1A_EVT_ACCESS_SIZE	0U
@@ -23,6 +20,8 @@
 #define RESET_REGISTER_ADDRESS	0UL
 #define RESET_REGISTER_VALUE	0UL
 #define RESET_REGISTER_SPACE_ID 0UL
+
+/* add test here */
 
 /* PCI mmcfg base of MCFG, pre-assumption is platform only has one PCI segment group */
 #define DEFAULT_PCI_MMCFG_BASE	0xE0000000UL
