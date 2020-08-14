088cd62d8bed6c18763de6a4c18e4a353fc8610e
Wei Liu <weix.w.liu@intel.com>
2020-08-14 20:25:44 +0800

    HV: sync hv reference code that generated by config tool
    Sync hv reference code that generated by acrn-config tool.
    
    Tracked-On: #5092
    Signed-off-by: Wei Liu <weix.w.liu@intel.com>

--- misc/vm_configs/boards/whl-ipc-i5/board.c
+++ misc/vm_configs/boards/whl-ipc-i5/board.c
@@ -74,7 +74,7 @@ struct dmar_info plat_dmar_info = {
 #ifdef CONFIG_RDT_ENABLED
 struct platform_clos_info platform_l2_clos_array[MAX_PLATFORM_CLOS_NUM];
 struct platform_clos_info platform_l3_clos_array[MAX_PLATFORM_CLOS_NUM];
-struct platform_clos_info platform_mba_clos_array[MAX_MBA_CLOS_NUM_ENTRIES];
+struct platform_clos_info platform_mba_clos_array[MAX_PLATFORM_CLOS_NUM];
 #endif
 
 static const struct cpu_cx_data board_cpu_cx[3] = {
