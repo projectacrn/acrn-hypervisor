22ad8c5e92abd7834258dc5a80cd627ca5050cd3
Nishioka, Toshiki <toshiki.nishioka@intel.com>
2020-08-28 01:14:25 +0900

    hv: add vgpio device model support
    When HV pass through the P2SB MMIO device to pre-launched VM, vgpio
    device model traps MMIO access to the GPIO registers within P2SB so
    that it can expose virtual IOAPIC pins to the VM in accordance with
    the programmed mappings between gsi and vgsi.
    
    Tracked-On: #5246
    
    Signed-off-by: Toshiki Nishioka <toshiki.nishioka@intel.com>
    Reviewed-by: Junjie Mao <junjie.mao@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

---
+++ hypervisor/include/dm/vgpio.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2020 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef VGPIO_H
+#define VGPIO_H
+
+void register_vgpio_handler(struct acrn_vm *vm, const struct acrn_mmiodev *mmiodev);
+
+#endif /* MMIO_DEV_H */
