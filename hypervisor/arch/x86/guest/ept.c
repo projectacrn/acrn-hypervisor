f94ad6aef7f20d76ecb5bb6b8b6148e03b233803
Li Fei1 <fei1.li@intel.com>
2020-09-14 23:02:43 +0800

    hv: ptcm: enable ptcm by default
    Enable PTCM by default if the platform support it. We could check it by whether
    the PTCM ACPI Table is exist. Besides to parse the PTCM ACPI Table and call
    PTCM command interface to init pSRAM on all CPU cores, we need to ignore
    WBINVD to flush the cache of th pSRAM.
    
    Tracked-On: #5330
    
    Signed-off-by: Qian Wang <qian1.wang@intel.com>
    Signed-off-by: Li Fei1 <fei1.li@intel.com>
    Reviewed-by: Wang, Yu1 <yu1.wang@intel.com>

--- hypervisor/arch/x86/guest/ept.c
+++ hypervisor/arch/x86/guest/ept.c
@@ -15,6 +15,7 @@
 #include <vtd.h>
 #include <logmsg.h>
 #include <trace.h>
+#include <ptct.h>
 
 #define DBG_LEVEL_EPT	6U
 
@@ -170,14 +171,39 @@ void ept_del_mr(struct acrn_vm *vm, uint64_t *pml4_page, uint64_t gpa, uint64_t 
  */
 void ept_flush_leaf_page(uint64_t *pge, uint64_t size)
 {
-	uint64_t hpa = INVALID_HPA;
+	uint64_t hpa = INVALID_HPA, hpa_end;
 	void *hva = NULL;
+	uint64_t flush_size = size;
 
 	if ((*pge & EPT_MT_MASK) != EPT_UNCACHED) {
 		hpa = (*pge & (~(size - 1UL)));
+		hpa_end = hpa + size;
+
+		if (hpa < psram_area_bottom) {
+			if (hpa_end > psram_area_top) {
+				flush_size = psram_area_bottom - hpa;
+				hva = hpa2hva(hpa);
+				stac();
+				flush_address_space(hva, flush_size);
+				clac();
+
+				flush_size = hpa_end - psram_area_top;
+				hpa = psram_area_top;
+			} else if (hpa_end > psram_area_bottom) {
+				flush_size = psram_area_bottom - hpa;
+			}
+		} else if (hpa < psram_area_top) {
+			if (hpa_end <= psram_area_top) {
+				flush_size = 0UL;
+			} else {
+				hpa = psram_area_top;
+				flush_size = hpa_end - psram_area_top;
+			}
+		}
+
 		hva = hpa2hva(hpa);
 		stac();
-		flush_address_space(hva, size);
+		flush_address_space(hva, flush_size);
 		clac();
 	}
 }
