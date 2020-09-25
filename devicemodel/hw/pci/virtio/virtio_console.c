b58a6de4fd7768c0cae5dfa2710773c37c37c055
Liu Long <long.liu@intel.com>
2020-09-25 13:41:25 +0800

    dm: fix fault Injection into VirtIO console backend
     Add Null pointer check in init vq ring and add vq ring descriptor
     check in case cause Nullpointer exception.
    
    Tracked-On: #5355
    Signed-off-by: Liu Long <long.liu@intel.com>
    Reviewed-by: Wang, Yu1 <yu1.wang@intel.com>

--- devicemodel/hw/pci/virtio/virtio_console.c
+++ devicemodel/hw/pci/virtio/virtio_console.c
@@ -412,7 +412,9 @@ virtio_console_notify_rx(void *vdev, struct virtio_vq_info *vq)
 
 	if (!port->rx_ready) {
 		port->rx_ready = 1;
-		vq->used->flags |= VRING_USED_F_NO_NOTIFY;
+		if (vq_has_descs(vq)) {
+			vq->used->flags |= VRING_USED_F_NO_NOTIFY;
+		}
 	}
 }
 
