aaabd8c0879f052588a52ea75531a70857fcd672
Liang Yi <yi.liang@intel.com>
2021-01-18 05:53:42 +0800

    hv: modularization: hide "struct multiboot_info"
    Move "struct multboot_info" from multiboot.h into multboot.c.
    
    Tracked-On: #6846
    Signed-off-by: Vijay Dhanraj <vijay.dhanraj@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- hypervisor/boot/include/multiboot.h
+++ hypervisor/boot/include/multiboot.h
@@ -41,57 +41,6 @@
 #include <zeropage.h>
 #include <vm_configurations.h>
 
-struct multiboot_info {
-	uint32_t               mi_flags;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_MEMORY. */
-	uint32_t               mi_mem_lower;
-	uint32_t               mi_mem_upper;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_BOOT_DEVICE. */
-	uint8_t                 mi_boot_device_part3;
-	uint8_t                 mi_boot_device_part2;
-	uint8_t                 mi_boot_device_part1;
-	uint8_t                 mi_boot_device_drive;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_CMDLINE. */
-	uint32_t                mi_cmdline;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_MODS. */
-	uint32_t               mi_mods_count;
-	uint32_t               mi_mods_addr;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_{AOUT,ELF}_SYMS. */
-	uint32_t               mi_elfshdr_num;
-	uint32_t               mi_elfshdr_size;
-	uint32_t               mi_elfshdr_addr;
-	uint32_t               mi_elfshdr_shndx;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_MMAP. */
-	uint32_t               mi_mmap_length;
-	uint32_t               mi_mmap_addr;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_DRIVES. */
-	uint32_t               mi_drives_length;
-	uint32_t               mi_drives_addr;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_CONFIG_TABLE. */
-	uint32_t               unused_mi_config_table;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_LOADER_NAME. */
-	uint32_t               mi_loader_name;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_APM. */
-	uint32_t               unused_mi_apm_table;
-
-	/* Valid if mi_flags sets MULTIBOOT_INFO_HAS_VBE. */
-	uint32_t               unused_mi_vbe_control_info;
-	uint32_t               unused_mi_vbe_mode_info;
-	uint32_t               unused_mi_vbe_interface_seg;
-	uint32_t               unused_mi_vbe_interface_off;
-	uint32_t               unused_mi_vbe_interface_len;
-};
-
 struct multiboot_mmap {
 	uint32_t size;
 	uint64_t baseaddr;
