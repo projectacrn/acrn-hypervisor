projectacrn/acrn-hypervisor

commit 97c9b240303602141fa116d5f315c62b6958b5fa

acrn-config: Reorg config tool folder
Remove vm_configs folder and move all the XML files and generic code example into config_tools/data

Tracked-On: #5644
Signed-off-by: Xie, nanlin <nanlin.xie@intel.com>

hypervisor/scripts/makefile/config.mk
@@ -50,13 +50,13 @@ endef
 #
 #   * The <symbol>_FILE (i.e. either BOARD_FILE or SCENARIO_FILE) will always hold the path to an existing XML file that
 #     defines the effective board/scenario. If only a BOARD/SCENARIO name is given, a predefined configuration under
-#     misc/acrn-config/xmls will be used.
+#     misc/config_tools/data/$BOARD will be used.
 #
 define determine_config =
 ifneq ($($(1)),)
   ifneq ($(realpath $($(1))),)
     override $(1)_FILE := $($(1))
-    override $(1) := $$(shell xmllint --xpath 'string(/acrn-config/@$(shell echo $(1) | tr A-Z a-z))' $$($(1)_FILE))
+    override $(1) := $$(shell xmllint --xpath 'string(/config_tools/@$(shell echo $(1) | tr A-Z a-z))' $$($(1)_FILE))
   else
     override $(1)_FILE := $(HV_PREDEFINED_$(1)_DIR)/$$($(1)).xml
     ifeq ($$(realpath $$($(1)_FILE)),)
@@ -113,9 +113,9 @@ endef
 HV_BOARD_XML := $(HV_OBJDIR)/.board.xml
 HV_SCENARIO_XML := $(HV_OBJDIR)/.scenario.xml
 HV_UNIFIED_XML_IN := $(BASEDIR)/scripts/makefile/unified.xml.in
-HV_PREDEFINED_BOARD_DIR := $(realpath $(BASEDIR)/../misc/acrn-config/xmls/board-xmls)
-HV_PREDEFINED_SCENARIO_DIR = $(realpath $(BASEDIR)/../misc/acrn-config/xmls/config-xmls)/$(BOARD)
-HV_CONFIG_TOOL_DIR := $(realpath $(BASEDIR)/../misc/acrn-config)
+HV_PREDEFINED_BOARD_DIR := $(realpath $(BASEDIR)/../misc/config_tools/data)/$(BOARD)
+HV_PREDEFINED_SCENARIO_DIR = $(realpath $(BASEDIR)/../misc/config_tools/data)/$(BOARD)
+HV_CONFIG_TOOL_DIR := $(realpath $(BASEDIR)/../misc/config_tools)
 HV_CONFIG_XFORM_DIR := $(HV_CONFIG_TOOL_DIR)/xforms
 
 # Paths to the outputs:

commit 89e66c930a20a0bbd2105c5f254c3f11cb339d8b

Makefile: fix build issues due to reorg of misc/
This patch fixes the following issues that break the build system:

 1. The tag of the root nodes of board/scenario XML files are still acrn-config,
    not config_tools. This patch reverts the XPATH that refers to these nodes.

 2. HV_PREDEFINED_BOARD_DIR now also relies on BOARD which may not be
    available at the time the variable is defined. As both board and
    scenario XML files are placed under the same directory, this patch
    refines the path calculation logic to get rid of mixing variables of
    the different flavors.

Tracked-On: #5644
Fixes: 97c9b2403036 ("acrn-config: Reorg config tool folder")
Signed-off-by: Junjie Mao <junjie.mao@intel.com>

hypervisor/scripts/makefile/config.mk
@@ -56,9 +56,9 @@ define determine_config =
 ifneq ($($(1)),)
   ifneq ($(realpath $($(1))),)
     override $(1)_FILE := $($(1))
-    override $(1) := $$(shell xmllint --xpath 'string(/config_tools/@$(shell echo $(1) | tr A-Z a-z))' $$($(1)_FILE))
+    override $(1) := $$(shell xmllint --xpath 'string(/acrn-config/@$(shell echo $(1) | tr A-Z a-z))' $$($(1)_FILE))
   else
-    override $(1)_FILE := $(HV_PREDEFINED_$(1)_DIR)/$$($(1)).xml
+    override $(1)_FILE := $(HV_PREDEFINED_DATA_DIR)/$$(BOARD)/$$($(1)).xml
     ifeq ($$(realpath $$($(1)_FILE)),)
       $$(error $(1) = '$($(1))' is neither path to a file nor name of a predefined board)
     endif
@@ -76,7 +76,7 @@ else
   else
     override $(1) := $(2)
   endif
-  override $(1)_FILE := $(HV_PREDEFINED_$(1)_DIR)/$$($(1)).xml
+  override $(1)_FILE := $(HV_PREDEFINED_DATA_DIR)/$$(BOARD)/$$($(1)).xml
 endif
 endef
 
@@ -107,14 +107,10 @@ endif
 endef
 
 # Paths to the inputs
-#
-# Note: BOARD may not be defined at this point. Thus, HV_PREDEFINED_SCENARIO_DIR is defined using '=' rather than ':='
-# so that the variable will only be expanded when used.
 HV_BOARD_XML := $(HV_OBJDIR)/.board.xml
 HV_SCENARIO_XML := $(HV_OBJDIR)/.scenario.xml
 HV_UNIFIED_XML_IN := $(BASEDIR)/scripts/makefile/unified.xml.in
-HV_PREDEFINED_BOARD_DIR := $(realpath $(BASEDIR)/../misc/config_tools/data)/$(BOARD)
-HV_PREDEFINED_SCENARIO_DIR = $(realpath $(BASEDIR)/../misc/config_tools/data)/$(BOARD)
+HV_PREDEFINED_DATA_DIR := $(realpath $(BASEDIR)/../misc/config_tools/data)
 HV_CONFIG_TOOL_DIR := $(realpath $(BASEDIR)/../misc/config_tools)
 HV_CONFIG_XFORM_DIR := $(HV_CONFIG_TOOL_DIR)/xforms
 