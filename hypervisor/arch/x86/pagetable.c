ea701c63c7530e433bdbbece1996d9077e85100a
Li Fei1 <fei1.li@intel.com>
2021-03-09 11:02:30 +0800

    hv: pgtable: add pgtable_create_trusty_root
    Add pgtable_create_trusty_root to allocate a page for trusty PML4 page table page.
    This function also copy PDPT entries from Normal world to Secure world.
    
    Tracked-On: #5830
    Signed-off-by: Li Fei1 <fei1.li@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- hypervisor/arch/x86/pagetable.c
+++ hypervisor/arch/x86/pagetable.c
@@ -435,6 +435,53 @@ void *pgtable_create_root(const struct pgtable *table)
 	return (uint64_t *)alloc_page(table->pool);
 }
 
+void *pgtable_create_trusty_root(const struct pgtable *table,
+	void *nworld_pml4_page, uint64_t prot_table_present, uint64_t prot_clr)
+{
+	uint16_t i;
+	uint64_t pdpte, *dest_pdpte_p, *src_pdpte_p;
+	uint64_t nworld_pml4e, sworld_pml4e;
+	void *sub_table_addr, *pml4_base;
+
+	/* Copy PDPT entries from Normal world to Secure world
+	 * Secure world can access Normal World's memory,
+	 * but Normal World can not access Secure World's memory.
+	 * The PML4/PDPT for Secure world are separated from
+	 * Normal World.PD/PT are shared in both Secure world's EPT
+	 * and Normal World's EPT
+	 */
+	pml4_base = alloc_page(table->pool);
+	sanitize_pte((uint64_t *)pml4_base, table);
+
+	/* The trusty memory is remapped to guest physical address
+	 * of gpa_rebased to gpa_rebased + size
+	 */
+	sub_table_addr = alloc_page(table->pool);
+	sworld_pml4e = hva2hpa(sub_table_addr) | prot_table_present;
+	set_pgentry((uint64_t *)pml4_base, sworld_pml4e, table);
+
+	nworld_pml4e = get_pgentry((uint64_t *)nworld_pml4_page);
+
+	/*
+	 * copy PTPDEs from normal world EPT to secure world EPT,
+	 * and remove execute access attribute in these entries
+	 */
+	dest_pdpte_p = pml4e_page_vaddr(sworld_pml4e);
+	src_pdpte_p = pml4e_page_vaddr(nworld_pml4e);
+	for (i = 0U; i < (uint16_t)(PTRS_PER_PDPTE - 1UL); i++) {
+		pdpte = get_pgentry(src_pdpte_p);
+		if ((pdpte & prot_table_present) != 0UL) {
+			pdpte &= ~prot_clr;
+			set_pgentry(dest_pdpte_p, pdpte, table);
+		}
+		src_pdpte_p++;
+		dest_pdpte_p++;
+	}
+
+	return pml4_base;
+}
+
+
 /**
  * @pre (pml4_page != NULL) && (pg_size != NULL)
  */
