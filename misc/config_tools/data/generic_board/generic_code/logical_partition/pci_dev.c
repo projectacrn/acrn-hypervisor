c56ace1d39d6ea5c19f337d3b98022a719d661e0
Yang,Yu-chu <yu-chu.yang@intel.com>
2021-04-14 16:16:40 -0700

    config-tools: update the generic_code with xform output
    Replace folllowing python generated files with xslt transform outputs
    which are formatted using clang-format:
    
    misc/config_tools/data/generic_board/generic_code/
    ├── hybrid
    │   ├── ivshmem_cfg.h
    │   ├── misc_cfg.h
    │   ├── pt_intx.c
    │   ├── vm_configurations.c
    │   └── vm_configurations.h
    ├── hybrid_rt
    │   ├── ivshmem_cfg.h
    │   ├── misc_cfg.h
    │   ├── pt_intx.c
    │   ├── vm_configurations.c
    │   └── vm_configurations.h
    ├── industry
    │   ├── ivshmem_cfg.h
    │   ├── misc_cfg.h
    │   ├── pt_intx.c
    │   ├── vm_configurations.c
    │   └── vm_configurations.h
    └── logical_partition
        ├── ivshmem_cfg.h
        ├── misc_cfg.h
        ├── pt_intx.c
        ├── vm_configurations.c
        └── vm_configurations.h
    
    Tracked-On: #5980
    Signed-off-by: Yang,Yu-chu <yu-chu.yang@intel.com>

--- misc/config_tools/data/generic_board/generic_code/logical_partition/pci_dev.c
+++ misc/config_tools/data/generic_board/generic_code/logical_partition/pci_dev.c
@@ -4,9 +4,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <vm_config.h>
+#include <x86/vm_config.h>
 #include <pci_devices.h>
 #include <vpci.h>
 #include <vbar_base.h>
-#include <mmu.h>
-#include <page.h>
+#include <x86/mmu.h>
+#include <x86/page.h>
