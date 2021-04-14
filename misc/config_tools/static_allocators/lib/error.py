8e1d8121c0a624c32198ebcacc84dcee80191a91
Yang,Yu-chu <yu-chu.yang@intel.com>
2021-04-13 22:03:10 -0700

    config-tools: add user-defined errors
    Create a file which contains user-defined errors for config-tools.
    
    Tracked-On: #5980
    Signed-off-by: Yang,Yu-chu <yu-chu.yang@intel.com>
    Reviewed-by: Junjie Mao <junjie.mao@intel.com>

---
+++ misc/config_tools/static_allocators/lib/error.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 Intel Corporation. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+class ResourceError(Exception):
+    """Raise this error when it is out of resource"""
