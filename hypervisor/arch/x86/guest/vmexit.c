2924e7ae74b8edd85e2aa4c98404c50045a1f692
Zide Chen <zide.chen@intel.com>
2021-04-29 13:27:50 -0700

    hv: nested: support for VMCLEAR emulation
    This patch is to emulate VMCLEAR instruction.
    
    L1 hypervisor issues VMCLEAR on a VMCS12 whose state could be any of
    these: active and current, active but not current, not yet VMPTRLDed.
    
    To emulate the VMCLEAR instruction, ACRN sets the VMCS12 launch state to
    "clear", and if L0 already cached this VMCS12, need to sync it back to
    guest memory:
    
    - sync shadow fields from shadow VMCS VMCS to cache VMCS12
    - copy cache VMCS12 to L1 guest memory
    
    Tracked-On: #5923
    Signed-off-by: Sainath Grandhi <sainath.grandhi@intel.com>
    Signed-off-by: Zide Chen <zide.chen@intel.com>

--- hypervisor/arch/x86/guest/vmexit.c
+++ hypervisor/arch/x86/guest/vmexit.c
@@ -78,8 +78,6 @@ static const struct vm_exit_dispatch dispatch_table[NR_VMX_EXIT_REASONS] = {
 		.handler = unhandled_vmexit_handler},
 	[VMX_EXIT_REASON_VMCALL] = {
 		.handler = vmcall_vmexit_handler},
-	[VMX_EXIT_REASON_VMCLEAR] = {
-		.handler = undefined_vmexit_handler},
 	[VMX_EXIT_REASON_VMLAUNCH] = {
 		.handler = undefined_vmexit_handler},
 	[VMX_EXIT_REASON_VMPTRST] = {
@@ -91,6 +89,8 @@ static const struct vm_exit_dispatch dispatch_table[NR_VMX_EXIT_REASONS] = {
 	[VMX_EXIT_REASON_VMWRITE] = {
 		.handler = undefined_vmexit_handler},
 #ifndef CONFIG_NVMX_ENABLED
+	[VMX_EXIT_REASON_VMCLEAR] = {
+		.handler = undefined_vmexit_handler},
 	[VMX_EXIT_REASON_VMPTRLD] = {
 		.handler = undefined_vmexit_handler},
 	[VMX_EXIT_REASON_VMXOFF] = {
@@ -98,6 +98,9 @@ static const struct vm_exit_dispatch dispatch_table[NR_VMX_EXIT_REASONS] = {
 	[VMX_EXIT_REASON_VMXON] = {
 		.handler = undefined_vmexit_handler},
 #else
+	[VMX_EXIT_REASON_VMCLEAR] = {
+		.handler = vmclear_vmexit_handler,
+		.need_exit_qualification = 1},
 	[VMX_EXIT_REASON_VMPTRLD] = {
 		.handler = vmptrld_vmexit_handler,
 		.need_exit_qualification = 1},
