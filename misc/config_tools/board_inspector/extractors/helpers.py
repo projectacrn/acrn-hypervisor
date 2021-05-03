b1950dfb30b1d9cdd954f0a1cb32bc3f261fd938
Junjie Mao <junjie.mao@intel.com>
2021-05-03 10:35:30 +0800

    board_inspector/extractors: framework to support extractors
    This patch makes the `run.py` enumerate and invoke all extractors (whose
    name should be `##-<name>.py` where `##` is a decimal number for ordering)
    under the extractors/ directory. Only some helper subroutines are added in
    this patch; the actual extractors will be added in the subsequent patches
    in this series.
    
    v1 -> v2:
     - Allow an extractor to be classified as advanced by defining the variable
       `advanced` to True. Advanced extractors are not enabled by default and
       can be invoked by passing `--advanced` to the board inspector.
    
    Tracked-On: #5922
    Signed-off-by: Junjie Mao <junjie.mao@intel.com>

---
+++ misc/config_tools/board_inspector/extractors/helpers.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2021 Intel Corporation. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+import lxml
+
+def add_child(element, tag, text=None, **kwargs):
+    child = lxml.etree.Element(tag)
+    child.text = text
+    for k,v in kwargs.items():
+        child.set(k, v)
+    element.append(child)
+    return child
+
+def get_node(etree, xpath):
+    result = etree.xpath(xpath)
+    assert len(result) <= 1, "Internal error: cannot get texts from multiple nodes at a time"
+    return result[0] if len(result) == 1 else None
