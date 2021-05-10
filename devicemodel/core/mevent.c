d947fcd5712ba0101ffa7bc47b9a1319af74b1a5
Yin Fengwei <fengwei.yin@intel.com>
2021-05-10 15:17:16 +0800

    build: fix dm and acrn_crashlog build error with gcc-11
    Unify two functions definitions/declarifications:
      Update the parameters from char array to char pointer.
    to fix the build issue:
      probeutils.c:61:29: error: argument 1 of type 'char *' declared
      as a pointer [-Werror=array-parameter=]
    
    Initialize local variable "c" to fix build issue:
      core/mevent.c:122:21: error: 'c' may be used uninitialized
      [-Werror=maybe-uninitialized]
    
    Tracked-On: #5993
    Signed-off-by: Yin Fengwei <fengwei.yin@intel.com>

--- devicemodel/core/mevent.c
+++ devicemodel/core/mevent.c
@@ -112,7 +112,7 @@ mevent_pipe_read(int fd, enum ev_type type, void *param)
 int
 mevent_notify(void)
 {
-	char c;
+	char c = 0;
 
 	/*
 	 * If calling from outside the i/o thread, write a byte on the
