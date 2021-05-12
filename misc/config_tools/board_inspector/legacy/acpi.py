c681726a219a92f64a780874e93d3dedd1a52f62
Junjie Mao <junjie.mao@intel.com>
2021-05-12 09:14:34 +0800

    board_inspector/legacy: fix a copy destination error
    Tracked-On: #5922
    Signed-off-by: Junjie Mao <junjie.mao@intel.com>

--- misc/config_tools/board_inspector/legacy/acpi.py
+++ misc/config_tools/board_inspector/legacy/acpi.py
@@ -657,5 +657,5 @@ def generate_info(board_file):
     # get the PTCT table from native environment
     out_dir = os.path.dirname(board_file)
     if os.path.isfile(SYS_PATH[1] + 'PTCT'):
-        shutil.copy(SYS_PATH[1] + 'PTCT', out_dir)
+        shutil.copy(SYS_PATH[1] + 'PTCT', out_dir if out_dir != "" else "./")
         print("PTCT table has been saved to {} successfully!".format(os.path.join(out_dir, 'PTCT')))
