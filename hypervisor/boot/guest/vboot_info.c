projectacrn/acrn-hypervisor

commit 80262f060293d2ba613fa70673ebe794a511ce9c

HV: rename append_seed_arg to fill_seed_arg
Previously append_seed_arg() just do fill in seed arg to dest cmd buffer,
so rename the api name to fill_seed_arg().

Since fill_seed_arg() will be called in SOS VM path only, the param of
bool vm_is_sos is not needed and will be replaced by dest buffer size.

The seed_args[] which used by fill_seed_arg() is pre-defined as all-zero,
so memset() is not needed in fill_seed_arg(), buffer pointer check
and strncpy_s() are not needed also.

Tracked-On: #4885

Signed-off-by: Victor Sun <victor.sun@intel.com>
Reviewed-by: Yin Fengwei <fengwei.yin@intel.com>
Acked-by: Eddie Dong <eddie.dong@intel.com>

hypervisor/boot/guest/vboot_info.c
@@ -113,15 +113,15 @@ static void init_vm_bootargs_info(struct acrn_vm *vm, const struct acrn_multiboo
 
 	if (vm_config->load_order == SOS_VM) {
 		if (strncat_s((char *)vm->sw.bootargs_info.src_addr, MAX_BOOTARGS_SIZE, " ", 1U) == 0) {
-			char seed_args[MAX_SEED_ARG_SIZE];
+			char seed_args[MAX_SEED_ARG_SIZE] = "";
 
-			append_seed_arg(seed_args, true);
-			/* Append seed argument for SOS
+			fill_seed_arg(seed_args, true);
+			/* Fill seed argument for SOS
 			 * seed_args string ends with a white space and '\0', so no aditional delimiter is needed
 			 */
 			if (strncat_s((char *)vm->sw.bootargs_info.src_addr, MAX_BOOTARGS_SIZE,
 					seed_args, (MAX_BOOTARGS_SIZE - 1U)) != 0) {
-				pr_err("failed to apend seed arg to SOS bootargs!");
+				pr_err("failed to fill seed arg to SOS bootargs!");
 			}
 
 			/* If there is cmdline from mbi->mi_cmdline, merge it with configured SOS bootargs. */

hypervisor/arch/x86/seed/seed.c
@@ -76,46 +76,42 @@ static uint32_t parse_seed_arg(void)
 }
 
 /*
- * append_seed_arg
+ * fill_seed_arg
  *
  * description:
- *     append seed argument to Guest's cmdline
+ *     fill seed argument to cmdline buffer which has MAX size of MAX_SEED_ARG_SIZE
  *
  * input:
- *    vm        pointer to target VM
+ *    cmd_dst   pointer to cmdline buffer
+ *    cmd_sz    size of cmd_dst buffer
  *
  * output:
- *    cmd_dst   pointer to cmdline for Guest
+ *    cmd_dst   pointer to cmdline buffer
  *
  * return value:
  *    none
+ *
+ * @pre cmd_dst != NULL
  */
-void append_seed_arg(char *cmd_dst, bool vm_is_sos)
+void fill_seed_arg(char *cmd_dst, size_t cmd_sz)
 {
 	uint32_t i;
-	char buf[MEM_1K];
 
-	if ((cmd_dst != NULL) && vm_is_sos) {
-		for (i = 0U; seed_arg[i].str != NULL; i++) {
-			if (seed_arg[i].addr != 0UL) {
-				(void)memset(buf, 0U, sizeof(buf));
+	for (i = 0U; seed_arg[i].str != NULL; i++) {
+		if (seed_arg[i].addr != 0UL) {
 
-				snprintf(buf, sizeof(buf), "%s0x%X ", seed_arg[i].str,
-						sos_vm_hpa2gpa(seed_arg[i].addr));
+			snprintf(cmd_dst, cmd_sz, "%s0x%X ", seed_arg[i].str, sos_vm_hpa2gpa(seed_arg[i].addr));
 
-				if (seed_arg[i].bootloader_id == BOOTLOADER_SBL) {
-					struct image_boot_params *boot_params =
-						(struct image_boot_params *)hpa2hva(seed_arg[i].addr);
+			if (seed_arg[i].bootloader_id == BOOTLOADER_SBL) {
+				struct image_boot_params *boot_params =
+					(struct image_boot_params *)hpa2hva(seed_arg[i].addr);
 
-					boot_params->p_seed_list = sos_vm_hpa2gpa(boot_params->p_seed_list);
+				boot_params->p_seed_list = sos_vm_hpa2gpa(boot_params->p_seed_list);
 
-					boot_params->p_platform_info = sos_vm_hpa2gpa(boot_params->p_platform_info);
-				}
-
-				(void)strncpy_s(cmd_dst, MAX_BOOTARGS_SIZE, buf, strnlen_s(buf, MEM_1K));
-
-				break;
+				boot_params->p_platform_info = sos_vm_hpa2gpa(boot_params->p_platform_info);
 			}
+
+			break;
 		}
 	}
 }

hypervisor/include/arch/x86/seed.h
@@ -28,7 +28,7 @@ struct physical_seed {
 
 void init_seed(void);
 
-void append_seed_arg(char *cmd_dst, bool vm_is_sos);
+void fill_seed_arg(char *cmd_dst, size_t cmd_sz);
 
 bool derive_virtual_seed(struct seed_info *seed_list, uint32_t *num_seeds,
 			 const uint8_t *salt, size_t salt_len, const uint8_t *info, size_t info_len);

commit 5519faf2bee370128bf219f11d5534110d993c3b

HV: refine init_vm_bootargs_info()
changes:
	1. The VM load order type condition is not needed, since the function
	   is called only when create SOS VM or pre-launched VM;
	2. Fixed wrong parameter of fill_seed_arg() which introduced by commit
	   80262f060293d2ba613fa70673ebe794a511ce9c.
	3. More comments on why multiboot string could override the pre-
	   configured VM bootargs and why append multiboot cmdline to SOS VM
	   bootargs;

Tracked-On: #5815
Signed-off-by: Victor Sun <victor.sun@intel.com>

hypervisor/boot/guest/vboot_info.c
@@ -114,33 +114,32 @@ static char mod_cmdline[PRE_VM_NUM + SOS_VM_NUM][MAX_BOOTARGS_SIZE] = { '\0' };
 static void init_vm_bootargs_info(struct acrn_vm *vm, const struct acrn_multiboot_info *mbi)
 {
 	struct acrn_vm_config *vm_config = get_vm_config(vm->vm_id);
-	char *bootargs = vm_config->os_config.bootargs;
 
-	if ((vm_config->load_order == PRE_LAUNCHED_VM) || (vm_config->load_order == SOS_VM)) {
-		if (mod_cmdline[vm->vm_id][0] == '\0') {
-			vm->sw.bootargs_info.src_addr = bootargs;
-		} else {
-			/* override build-in bootargs with multiboot module string which is configurable
-			 * at bootloader boot time. e.g. GRUB menu
-			 */
-			vm->sw.bootargs_info.src_addr = &mod_cmdline[vm->vm_id][0];
-		}
+	vm->sw.bootargs_info.src_addr = vm_config->os_config.bootargs;
+	/* If multiboot string of the kernel module exists, it would OVERRIDE the pre-configured build-in VM bootargs,
+	 * which means we give user a chance to re-configure VM bootargs at bootloader runtime. e.g. GRUB menu
+	 */
+	if (mod_cmdline[vm->vm_id][0] != '\0') {
+		vm->sw.bootargs_info.src_addr = &mod_cmdline[vm->vm_id][0];
 	}
 
 	if (vm_config->load_order == SOS_VM) {
 		if (strncat_s((char *)vm->sw.bootargs_info.src_addr, MAX_BOOTARGS_SIZE, " ", 1U) == 0) {
 			char seed_args[MAX_SEED_ARG_SIZE] = "";
 
-			fill_seed_arg(seed_args, true);
+			fill_seed_arg(seed_args, MAX_SEED_ARG_SIZE);
 			/* Fill seed argument for SOS
-			 * seed_args string ends with a white space and '\0', so no aditional delimiter is needed
+			 * seed_args string ends with a white space and '\0', so no additional delimiter is needed
 			 */
 			if (strncat_s((char *)vm->sw.bootargs_info.src_addr, MAX_BOOTARGS_SIZE,
 					seed_args, (MAX_BOOTARGS_SIZE - 1U)) != 0) {
 				pr_err("failed to fill seed arg to SOS bootargs!");
 			}
 
-			/* If there is cmdline from mbi->mi_cmdline, merge it with configured SOS bootargs. */
+			/* If there is cmdline from mbi->mi_cmdline, merge it with configured SOS bootargs.
+			 * This is very helpful when one of configured bootargs need to be revised at GRUB runtime
+			 * (e.g. "root="), since the later one would override the previous one if multiple bootargs exist.
+			 */
 			if (((mbi->mi_flags & MULTIBOOT_INFO_HAS_CMDLINE) != 0U) && (*(mbi->mi_cmdline) != '\0')) {
 				if (strncat_s((char *)vm->sw.bootargs_info.src_addr, MAX_BOOTARGS_SIZE,
 						mbi->mi_cmdline, (MAX_BOOTARGS_SIZE - 1U)) != 0) {