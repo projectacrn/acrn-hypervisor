0f208e177c1c597c555041b8b093c7031f0c48bc
Yonghua Huang <yonghua.huang@intel.com>
2021-05-27 09:14:33 +0800

    dm: add RTCT v2 support for guest
     The latest version of RTCT specification is version 2.
    
     This patch is to add RTCT v2 support for virtual RTCT
     of guest.
    
    Tracked-On: #6020
    Signed-off-by: Yonghua Huang <yonghua.huang@intel.com>
    Acked-by: Yu Wang <yu1.wang@intel.com>

--- devicemodel/include/rtct.h
+++ devicemodel/include/rtct.h
@@ -17,11 +17,30 @@
 #define RTCT_ENTRY_TYPE_RT_IOMMU		8U
 #define RTCT_ENTRY_TYPE_MEM_HIERARCHY_LATENCY	9U
 
+/*Entry IDs for RTCT version 2*/
+#define	RTCT_V2_COMPATIBILITY	0U
+#define	RTCT_V2_RTCD_LIMIT	1U
+#define	RTCT_V2_CRL_BINARY	2U
+#define	RTCT_V2_IA_WAYMASK	3U
+#define	RTCT_V2_WRC_WAYMASK	4U
+#define	RTCT_V2_GT_WAYMASK	5U
+#define	RTCT_V2_SSRAM_WAYMASK	6U
+#define	RTCT_V2_SSRAM	7U
+#define	RTCT_V2_MEMORY_HIERARCHY_LATENCY	8U
+#define	RTCT_V2_ERROR_LOG_ADDRESS	9U
+
 struct rtct_entry {
 	uint16_t size;
 	uint16_t format_version;
 	uint32_t type;
 	uint32_t data[64];
+} __packed;
+
+struct rtct_entry_data_compatibility {
+	uint32_t RTCT_Ver_Major;
+	uint32_t RTCT_Ver_Minor;
+	uint32_t RTCD_Ver_Major;
+	uint32_t RTCD_Ver_Minor;
 } __packed;
 
 struct rtct_entry_data_ssram {
@@ -30,6 +49,14 @@ struct rtct_entry_data_ssram {
 	uint32_t ways;
 	uint32_t size;
 	uint32_t apic_id_tbl[64];
+} __packed;
+
+struct rtct_entry_data_ssram_v2 {
+	uint32_t cache_level;
+	uint32_t cache_id;
+	uint64_t base;
+	uint32_t size;
+	uint32_t shared;
 } __packed;
 
 struct rtct_entry_data_mem_hi_latency {
