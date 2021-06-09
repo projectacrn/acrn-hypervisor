9ac8e292fdaf091046eb0c038a4d83faaf9168e2
Victor Sun <victor.sun@intel.com>
2021-06-09 10:07:09 +0800

    HV: add efi memory map parsing function
    When hypervisor boot from efi environment, the efi memory layout should be
    considered as main memory map reference for hypervisor use. This patch add
    function that parses the efi memory descriptor entries info from efi memory
    map pointer and stores the info into a static hv_memdesc[] array.
    
    Tracked-On: #5626
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

---
+++ hypervisor/include/common/efi_mmap.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2021 Intel Corporation.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef EFI_MMAP_H
+#define EFI_MMAP_H
+#include <types.h>
+
+#define MAX_EFI_MMAP_ENTRIES		256U
+
+void init_efi_mmap_entries(struct efi_info *uefi_info);
+
+uint32_t get_efi_mmap_entries_count(void);
+const struct efi_memory_desc *get_efi_mmap_entry(void);
+
+#endif
