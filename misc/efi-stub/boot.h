52790b5c0ec464ebe99493b1d39e56104e41669a
Yifan Liu <yifan1.liu@intel.com>
2021-06-22 16:26:14 +0800

    misc: efi-stub: Barebone efi-stub for secure boot
    ACRN EFI application based on the legacy efi-stub code provides booting method
    of HV on UEFI-BIOS without using the GRUB Bootloader. It is supposed to be used
    for secure booting on certain platform. By means of that users can boot HV,
    Service VM kernel, pre-launched VM kernel and its ACPI table binary packed in
    the Slim Bootloader container boot image file format. ACRN EFI application has
    additional dependencies to compile which are not listed in the existing ACRN GSG
    doc. Since this is an optional feature but not all users need, it does not get
    compiled by default to avoid causing any confusion for existing users. README
    for how to use the feature will come later in a separated commit.
    
    This patch adds barebone implementation of this efi-stub. The following
    files are reused from a previous version of efi-stub without changes:
        efilinux.h, pe.c, stdlib.h
    
    Other files contains stubbed functions, whose implementation will come
    in coming patches.
    
    Tracked-On: #6241
    Signed-off-by: Toshiki Nishioka <toshiki.nishioka@intel.com>
    Signed-off-by: Yifan Liu <yifan1.liu@intel.com>
    Co-developed-by: Yifan Liu <yifan1.liu@intel.com>

---
+++ misc/efi-stub/boot.h
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2011 - 2021, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *    * Neither the name of Intel Corporation nor the names of its
+ *      contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ACRNBOOT_H__
+#define __ACRNBOOT_H__
+
+#include "multiboot.h"
+
+#define E820_RAM		1
+#define E820_RESERVED		2
+#define E820_ACPI		3
+#define E820_NVS		4
+#define E820_UNUSABLE		5
+
+#define ERROR_STRING_LENGTH	32
+#define EFI_LOADER_SIGNATURE    "EL64"
+
+#define ACPI_XSDT_ENTRY_SIZE     (sizeof(UINT64))
+#define ACPI_NAME_SIZE                  4
+#define ACPI_OEM_ID_SIZE                6
+#define ACPI_OEM_TABLE_ID_SIZE          8
+
+#define MSR_IA32_PAT                        0x00000277  /* PAT */
+#define MSR_IA32_EFER                       0xC0000080
+#define MSR_IA32_FS_BASE                    0xC0000100U
+#define MSR_IA32_GS_BASE                    0xC0000101
+#define MSR_IA32_SYSENTER_ESP               0x00000175  /* ESP for sysenter */
+#define MSR_IA32_SYSENTER_EIP               0x00000176  /* EIP for sysenter */
+
+#define UEFI_BOOT_LOADER_NAME "ACRN UEFI loader"
+
+#define ALIGN_UP(addr, align) \
+	(((addr) + (typeof (addr)) (align) - 1) & ~((typeof (addr)) (align) - 1))
+
+/* Read MSR */
+#define CPU_MSR_READ(reg, msr_val_ptr)                      \
+{                                                           \
+	uint32_t msrl, msrh;                                 \
+	asm volatile ("rdmsr" : "=a"(msrl),                 \
+		"=d"(msrh) : "c" (reg));            \
+	*msr_val_ptr = ((uint64_t)msrh << 32U) | msrl;           \
+}
+
+EFI_STATUS get_pe_section(CHAR8 *base, char *section_name, UINTN section_name_len, UINTN *vaddr, UINTN *size);
+typedef void(*hv_func)(int32_t, struct multiboot_info*);
+
+/*
+ * We allocate memory for the following struct together with hyperivosr itself
+ * memory allocation during boot.
+ */
+#define MBOOT_MMAP_NUMS        256
+#define MBOOT_MMAP_SIZE (sizeof(struct multiboot_mmap) * MBOOT_MMAP_NUMS)
+#define MBOOT_INFO_SIZE (sizeof(struct multiboot_info))
+#define MBOOT_MODS_NUMS        4
+#define MBOOT_MODS_SIZE (sizeof(struct multiboot_module) * MBOOT_MODS_NUMS)
+#define BOOT_LOADER_NAME_SIZE 17U
+#define EFI_BOOT_MEM_SIZE \
+	(MBOOT_MMAP_SIZE + MBOOT_INFO_SIZE + MBOOT_MODS_SIZE + BOOT_LOADER_NAME_SIZE)
+#define MBOOT_MMAP_PTR(addr) \
+	((struct multiboot_mmap *)((VOID *)(addr)))
+#define MBOOT_INFO_PTR(addr)  \
+	((struct multiboot_info *)((VOID *)(addr) + MBOOT_MMAP_SIZE))
+#define MBOOT_MODS_PTR(addr)  \
+	((struct multiboot_module *)((VOID *)(addr) + MBOOT_MMAP_SIZE + MBOOT_INFO_SIZE))
+#define BOOT_LOADER_NAME_PTR(addr)	\
+	((char *)((VOID *)(addr) + MBOOT_MMAP_SIZE + MBOOT_INFO_SIZE + MBOOT_MODS_SIZE))
+
+struct efi_memmap_info {
+	UINTN map_size;
+	UINTN map_key;
+	UINT32 desc_version;
+	UINTN desc_size;
+	EFI_MEMORY_DESCRIPTOR *mmap;
+};
+
+typedef struct mb_module_info {
+	UINTN mod_start;
+	UINTN mod_end;
+	const char *cmd;
+	UINTN cmdsize;
+} MB_MODULE_INFO;
+
+struct efi_info {
+	UINT32 efi_loader_signature;
+	UINT32 efi_systab;
+	UINT32 efi_memdesc_size;
+	UINT32 efi_memdesc_version;
+	UINT32 efi_memmap;
+	UINT32 efi_memmap_size;
+	UINT32 efi_systab_hi;
+	UINT32 efi_memmap_hi;
+};
+
+struct e820_entry {
+	UINT64 addr;		/* start of memory segment */
+	UINT64 size;		/* size of memory segment */
+	UINT32 type;		/* type of memory segment */
+} __attribute__((packed));
+
+struct acpi_table_rsdp {
+	/* ACPI signature, contains "RSD PTR " */
+	char signature[8];
+	/* ACPI 1.0 checksum */
+	UINT8 checksum;
+	/* OEM identification */
+	char oem_id[ACPI_OEM_ID_SIZE];
+	/* Must be (0) for ACPI 1.0 or (2) for ACPI 2.0+ */
+	UINT8 revision;
+	/* 32-bit physical address of the RSDT */
+	UINT32 rsdt_physical_address;
+	/* Table length in bytes, including header (ACPI 2.0+) */
+	UINT32 length;
+	/* 64-bit physical address of the XSDT (ACPI 2.0+) */
+	UINT64 xsdt_physical_address;
+	/* Checksum of entire table (ACPI 2.0+) */
+	UINT8 extended_checksum;
+	/* Reserved, must be zero */
+	UINT8 reserved[3];
+};
+
+struct acpi_table_header {
+	 /* ASCII table signature */
+	char signature[ACPI_NAME_SIZE];
+	/* Length of table in bytes, including this header */
+	UINT32 length;
+	/* ACPI Specification minor version number */
+	UINT8 revision;
+	/* To make sum of entire table == 0 */
+	UINT8 checksum;
+	/* ASCII OEM identification */
+	char oem_id[ACPI_OEM_ID_SIZE];
+	/* ASCII OEM table identification */
+	char oem_table_id[ACPI_OEM_TABLE_ID_SIZE];
+	/* OEM revision number */
+	UINT32 oem_revision;
+	/* ASCII ASL compiler vendor ID */
+	char asl_compiler_id[ACPI_NAME_SIZE];
+	/* ASL compiler version */
+	UINT32 asl_compiler_revision;
+};
+
+/* hypervisor loader operation table */
+typedef struct hv_loader *HV_LOADER;
+struct hv_loader {
+	/* Load ACRN hypervisor image into memory */
+	EFI_STATUS (*load_boot_image)(IN HV_LOADER hvld);
+	/* Load VM Kernels and ACPI Tables into memory */
+	EFI_STATUS (*load_modules)(IN HV_LOADER hvld);
+
+	const char *(*get_boot_cmd)(IN HV_LOADER hvld);
+	/* Get hypervisor boot command length */
+	UINTN (*get_boot_cmdsize)(IN HV_LOADER hvld);
+
+	MB_MODULE_INFO *(*get_mods_info)(IN HV_LOADER hvld, UINTN index);
+	/* Get the number of multiboot2 modules */
+	UINTN (*get_mod_count)(IN HV_LOADER hvld);
+	/* Get the total memory size allocated to load module files */
+	UINTN (*get_total_modsize)(IN HV_LOADER hvld);
+	/* Get the total lengths of the module commands */
+	UINTN (*get_total_modcmdsize)(IN HV_LOADER hvld);
+
+	/* Get the total memory size of hv image */
+	UINTN (*get_hv_ram_size)(IN HV_LOADER hvld);
+
+	/* Get the start address of the memory region stored ACRN hypervisor image */
+	EFI_PHYSICAL_ADDRESS (*get_hv_hpa)(IN HV_LOADER hvld);
+	/* Get the start address of the memory region stored module files */
+	EFI_PHYSICAL_ADDRESS (*get_mod_hpa)(IN HV_LOADER hvld);
+	/* Get the entry point of ACRN hypervisor */
+	EFI_PHYSICAL_ADDRESS (*get_hv_entry)(IN HV_LOADER hvld);
+
+	/* Get the supported multiboot version of ACRN hypervisor image */
+	int (*get_multiboot_version)(IN HV_LOADER hvld);
+
+	/* free up memory allocated by hypervisor loader */
+	void (*deinit)(IN HV_LOADER hvld);
+};
+
+EFI_STATUS get_efi_memmap(struct efi_memmap_info *mmap_info, int size_only);
+
+static inline uint64_t
+msr_read(uint32_t reg_num)
+{
+	uint64_t msr_val;
+
+	CPU_MSR_READ(reg_num, &msr_val);
+	return msr_val;
+}
+
+#endif
---
+++ misc/efi-stub/stdlib.h
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2011 - 2021, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *    * Neither the name of Intel Corporation nor the names of its
+ *      contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This file contains some wrappers around the gnu-efi functions. As
+ * we're not going through uefi_call_wrapper() directly, this allows
+ * us to get some type-safety for function call arguments and for the
+ * compiler to check that the number of function call arguments is
+ * correct.
+ *
+ * It's also a good place to document the EFI interface.
+ */
+
+
+
+#ifndef __STDLIB_H__
+#define __STDLIB_H__
+
+#include <efi.h>
+#include <efilib.h>
+
+static inline void memset(void *dstv, char ch, UINTN size)
+{
+	char *dst = dstv;
+	int32_t i;
+
+	for (i = 0; i < size; i++)
+		dst[i] = ch;
+}
+
+static inline void memcpy(char *dst, const char *src, UINTN size)
+{
+	int32_t i;
+
+	for (i = 0; i < size; i++)
+		*dst++ = *src++;
+}
+
+static inline int32_t strlen(const char *str)
+{
+	int32_t len;
+
+	len = 0;
+	while (*str++)
+		len++;
+
+	return len;
+}
+
+static inline CHAR16 *strstr_16(CHAR16 *haystack, CHAR16 *needle, UINTN len)
+{
+	CHAR16 *p;
+	CHAR16 *word = NULL;
+
+	if (!len)
+		return NULL;
+
+	p = haystack;
+	while (*p) {
+		if (!StrnCmp(p, needle, len)) {
+			word = p;
+			break;
+		}
+		p++;
+	}
+
+	return (CHAR16*)word;
+}
+
+static inline char *ch16_2_ch8(CHAR16 *str16, UINTN len)
+{
+	UINTN i;
+	char *str8;
+
+	str8 = AllocatePool((len + 1) * sizeof(char));
+
+	for (i = 0; i < len; i++)
+		str8[i] = str16[i];
+
+	str8[len] = 0;
+
+	return str8;
+}
+
+static inline CHAR16 *ch8_2_ch16(char *str8, UINTN len)
+{
+	UINTN i;
+	CHAR16 *str16;
+
+	str16 = AllocatePool((len + 1) * sizeof(CHAR16));
+
+	for (i = 0; i < len; i++)
+		str16[i] = str8[i];
+
+	str16[len] = 0;
+
+	return str16;
+}
+
+#endif /* __STDLIB_H__ */
