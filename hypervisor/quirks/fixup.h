ab82f67cb975088ffd2a6580cba1f06590814fbe
Tao Yuhong <yuhong.tao@intel.com>
2021-06-24 06:44:43 -0400

    HV: TPM: check tpm2 start method and event log
    If TPM device is passthrough to pre-launched VM, need check its
    start method and event log settings in native TPM2 ACPI table.
    Because when connect different TPM devices, TPM start method can
    change in native ACPI table. And event log address change in native
    ACPI table is expected by BIOS updating. Need fixup pre-launched
    VM's ACPI table and vm_config if they are misaligned with native
    platform. We add acrn_vm_fixup() in prepare_vm(), where the acrn_vm
    structure is not created. This is suitable for checking between
    vm_config and HW real configurations, and try fixup for native ACPI
    updating.
    
    Tracked-On: #6320
    Signed-off-by: Tao Yuhong <yuhong.tao@intel.com>

---
+++ hypervisor/quirks/fixup.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _FIXUP_H_
+#define _FIXUP_H_
+
+void acrn_vm_fixup(uint16_t vm_id);
+
+#endif /* _FIXUP_H_ */
