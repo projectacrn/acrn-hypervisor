9b632c0e4b3ecd56b9f0306c51fddac636f5155c
Victor Sun <victor.sun@intel.com>
2021-08-09 16:36:24 +0800

    HV: vm_load: split vm_load.c to support diff kernel format
    The patch splits the vm_load.c to three parts, the loader function of bzImage
    kernel is moved to bzimage_loader.c, the loader function of raw image kernel
    is moved to rawimage_loader.c, the stub is still stayed in vm_load.c to load
    the corresponding kernel loader function. Each loader function could be
    isolated by CONFIG_GUEST_KERNEL_XXX macro which generated by config tool.
    
    Tracked-On: #6323
    
    Signed-off-by: Victor Sun <victor.sun@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/boot/include/guest/vboot_info.h
+++ hypervisor/boot/include/guest/vboot_info.h
@@ -9,5 +9,14 @@
 #define VBOOT_INFO_H
 
 int32_t init_vm_boot_info(struct acrn_vm *vm);
+void load_sw_module(struct acrn_vm *vm, struct sw_module_info *sw_module);
+
+#ifdef CONFIG_GUEST_KERNEL_BZIMAGE
+int32_t vm_bzimage_loader(struct acrn_vm *vm);
+#endif
+#ifdef CONFIG_GUEST_KERNEL_RAWIMAGE
+int32_t vm_rawimage_loader(struct acrn_vm *vm);
+#endif
+
 
 #endif /* end of include guard: VBOOT_INFO_H */
