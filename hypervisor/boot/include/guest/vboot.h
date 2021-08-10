7c170e6a4e69b35a02caa015b9d15a6f3d773223
Zhou, Wu <wu.zhou@intel.com>
2021-08-10 15:58:41 +0800

    HV: Remove 'vm_' of vm_elf_loader and etc.
    In order to make better sense, vm_elf_loader, vm_bzimage_loader and
    vm_rawimage_loader are changed to elf_loaer, bzimage_loaer and
    rawimage_loader.
    
    Tracked-On: #6323
    
    Signed-off-by: Zhou, Wu <wu.zhou@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/boot/include/guest/vboot.h
+++ hypervisor/boot/include/guest/vboot.h
@@ -12,10 +12,10 @@ int32_t init_vm_boot_info(struct acrn_vm *vm);
 void load_sw_module(struct acrn_vm *vm, struct sw_module_info *sw_module);
 
 #ifdef CONFIG_GUEST_KERNEL_BZIMAGE
-int32_t vm_bzimage_loader(struct acrn_vm *vm);
+int32_t bzimage_loader(struct acrn_vm *vm);
 #endif
 #ifdef CONFIG_GUEST_KERNEL_RAWIMAGE
-int32_t vm_rawimage_loader(struct acrn_vm *vm);
+int32_t rawimage_loader(struct acrn_vm *vm);
 #endif
 
 
