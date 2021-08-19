487dc0ffe616fb101f5f4f85b8347c4c656788be
dongshen <dongsheng.x.zhang@intel.com>
2021-08-19 15:05:18 -0700

    hv: vCAT: implementing the vCAT MSRs write handler
    Implement the write_vcbm() function to handle the
    MSR_IA32_type_MASK_n vCBM MSRs write request
    
    Call write_vclosid() to handle MSR_IA32_PQR_ASSOC MSR write request
    
    Several vCAT P2V (physical to virtual) and V2P (virtual to physical)
    mappings exist:
    
       struct acrn_vm_config *vm_config = get_vm_config(vm_id)
    
       max_pcbm = vm_config->max_type_pcbm (type: l2 or l3)
       mask_shift = ffs64(max_pcbm)
    
       vclosid = vmsr - MSR_IA32_type_MASK_0
       pclosid = vm_config->pclosids[vclosid]
    
       pmsr = MSR_IA32_type_MASK_0 + pclosid
       pcbm = vcbm << mask_shift
       vcbm = pcbm >> mask_shift
    
       Where
       MSR_IA32_type_MASK_n: L2 or L3 mask msr address for CLOSIDn, from
       0C90H through 0D8FH (inclusive).
    
       max_pcbm: a bitmask that selects all the physical cache ways assigned to the VM
    
       vclosid: virtual CLOSID, always starts from 0
    
       pclosid: corresponding physical CLOSID for a given vclosid
    
       vmsr: virtual msr address, passed to vCAT handlers by the
       caller functions rdmsr_vmexit_handler()/wrmsr_vmexit_handler()
    
       pmsr: physical msr address
    
       vcbm: virtual CBM, passed to vCAT handlers by the
       caller functions rdmsr_vmexit_handler()/wrmsr_vmexit_handler()
    
       pcbm: physical CBM
    
    Tracked-On: #5917
    Signed-off-by: dongshen <dongsheng.x.zhang@intel.com>
    Acked-by: Eddie Dong <eddie.dong@Intel.com>

--- hypervisor/include/arch/x86/asm/guest/vcat.h
+++ hypervisor/include/arch/x86/asm/guest/vcat.h
@@ -16,7 +16,9 @@ void init_vcat_msrs(struct acrn_vcpu *vcpu);
 uint16_t vcat_get_num_vclosids(const struct acrn_vm *vm);
 uint64_t vcat_pcbm_to_vcbm(const struct acrn_vm *vm, uint64_t pcbm, int res);
 int32_t read_vcbm(const struct acrn_vcpu *vcpu, uint32_t vmsr, uint64_t *rval);
+int32_t write_vcbm(struct acrn_vcpu *vcpu, uint32_t vmsr, uint64_t val);
 int32_t read_vclosid(const struct acrn_vcpu *vcpu, uint64_t *rval);
+int32_t write_vclosid(struct acrn_vcpu *vcpu, uint64_t val);
 
 #endif /* VCAT_H_ */
 
