be855d2352f52ffdfe1229b0153eabae4308cf15
dongshen <dongsheng.x.zhang@intel.com>
2021-08-19 13:28:44 -0700

    hv: vCAT: expose CAT capabilities to vCAT-enabled VM
    Expose CAT feature to vCAT VM by reporting the number of
    cache ways/CLOSIDs via the 04H/10H cpuid instructions, so that the
    VM can take advantage of CAT to prioritize and partition cache
    resource for its own tasks.
    
    Add the vcat_pcbm_to_vcbm() function to map pcbm to vcbm
    
    Tracked-On: #5917
    Signed-off-by: dongshen <dongsheng.x.zhang@intel.com>
    Acked-by: Eddie Dong <eddie.dong@Intel.com>

--- hypervisor/include/arch/x86/asm/guest/vcat.h
+++ hypervisor/include/arch/x86/asm/guest/vcat.h
@@ -9,7 +9,12 @@
 
 #include <asm/guest/vm.h>
 
+bool is_l2_vcat_configured(const struct acrn_vm *vm);
+bool is_l3_vcat_configured(const struct acrn_vm *vm);
+uint16_t vcat_get_vcbm_len(const struct acrn_vm *vm, int res);
 void init_vcat_msrs(struct acrn_vcpu *vcpu);
+uint16_t vcat_get_num_vclosids(const struct acrn_vm *vm);
+uint64_t vcat_pcbm_to_vcbm(const struct acrn_vm *vm, uint64_t pcbm, int res);
 
 #endif /* VCAT_H_ */
 
