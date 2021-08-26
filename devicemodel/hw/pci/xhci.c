projectacrn/acrn-hypervisor

commit dea2574819c6d28eb9b857b91aa4ef794ba535fe

ACRN:DM: Add pointer check before use the erst
The event ring segment table  pointer may be NULL when get the address
from guest, add pointer check before use it.

Tracked-On: #6476
Signed-off-by: Liu Long <long.liu@intel.com>
Acked-by: Wang, Yu1 <yu1.wang@intel.com>

devicemodel/hw/pci/xhci.c
@@ -1817,6 +1817,11 @@ pci_xhci_insert_event(struct pci_xhci_vdev *xdev,
 	int erdp_idx;
 
 	rts = &xdev->rtsregs;
+	if (&rts->erstba_p == NULL) {
+		UPRINTF(LFTL, "Invalid Event Ring Segment Table base address!\r\n");
+		return -EINVAL;
+	}
+
 	erdp = rts->intrreg.erdp & ~0xF;
 	erst = &rts->erstba_p[rts->er_enq_seg];
 	erdp_idx = (erdp - erst->qwRingSegBase) / sizeof(struct xhci_trb);

commit 595b829dc3df1f3a09bb7b3f43d7deac75936264

ACRN:DM: Fix the bug introduced by dea2574819c.
Fix the bug introduced by dea2574819c. There had a typo that added
the "&" by mistake.

Tracked-On: #6476
Signed-off-by: Liu Long <long.liu@intel.com>
Acked-by: Wang, Yu1 <yu1.wang@intel.com>

devicemodel/hw/pci/xhci.c
@@ -1817,7 +1817,7 @@ pci_xhci_insert_event(struct pci_xhci_vdev *xdev,
 	int erdp_idx;
 
 	rts = &xdev->rtsregs;
-	if (&rts->erstba_p == NULL) {
+	if (rts->erstba_p == NULL) {
 		UPRINTF(LFTL, "Invalid Event Ring Segment Table base address!\r\n");
 		return -EINVAL;
 	}