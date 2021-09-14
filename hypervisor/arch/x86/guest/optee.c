f3792a74a34780dca2ddc168c006ff65a4b85177
Jie Deng <jie.deng@intel.com>
2021-09-14 15:16:35 +0800

    hv: tee: add TEE VM memmap support
    TEE is a secure VM which has its own partitioned resources while
    REE is a normal VM which owns the rest of platform resources.
    The TEE, as a secure world, it can see the memory of the REE
    VM, also known as normal world, but not the other way around.
    But please note, TEE and REE can only see their own devices.
    
    So this patch does the following things:
    
    1. go through physical e820 table, to ept add all system memory entries.
    2. remove hv owned memory.
    
    Tracked-On: #6571
    Signed-off-by: Jie Deng <jie.deng@intel.com>
    Reviewed-by: Wang, Yu1 <yu1.wang@intel.com>
    Acked-by: Eddie Dong <eddie.dong@Intel.com>

---
+++ hypervisor/arch/x86/guest/optee.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2021 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <asm/guest/vm.h>
+#include <asm/guest/ept.h>
+#include <asm/vm_config.h>
+#include <asm/mmu.h>
+#include <asm/guest/optee.h>
+#include <asm/trampoline.h>
+#include <reloc.h>
+
+void prepare_tee_vm_memmap(struct acrn_vm *vm, const struct acrn_vm_config *vm_config)
+{
+	uint64_t hv_hpa;
+
+	/*
+	 * Only need to map following things, let init_vpci to map the secure devices
+	 * if any.
+	 *
+	 * 1. go through physical e820 table, to ept add all system memory entries.
+	 * 2. remove hv owned memory.
+	 */
+	if ((vm_config->guest_flags & GUEST_FLAG_TEE) != 0U) {
+		vm->e820_entry_num = get_e820_entries_count();
+		vm->e820_entries = (struct e820_entry *)get_e820_entry();
+
+		prepare_vm_identical_memmap(vm, E820_TYPE_RAM, EPT_WB | EPT_RWX);
+
+		hv_hpa = hva2hpa((void *)(get_hv_image_base()));
+		ept_del_mr(vm, (uint64_t *)vm->arch_vm.nworld_eptp, hv_hpa, get_hv_ram_size());
+	}
+}
