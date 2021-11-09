97e8c9457de8b4ea5cf60fbfa0f30c3f12d98502
Chenli Wei <chenli.wei@intel.com>
2021-11-09 10:50:40 +0800

    hv: remove MAX_KATA_VM_NUM and CONFIG_KATA_VM
    Since the UUID is not a *must* set parameter for the standard post-launched
    VM which doesn't depend on any static VM configuration. We can remove
    the KATA related code from hypervisor as it belongs to such VM type.
    
    v2-->v3:
        separate the struce acrn_platform_info change of devicemodel
    
    v1-->v2:
        update the subject and commit msg
    
    Tracked-On:#6685
    Signed-off-by: Chenli Wei <chenli.wei@intel.com>
    Reviewed-by: Wang, Yu1 <yu1.wang@intel.com>

--- hypervisor/include/public/acrn_hv_defs.h
+++ hypervisor/include/public/acrn_hv_defs.h
@@ -337,10 +337,8 @@ struct acrn_platform_info {
 		 */
 		uint64_t vm_configs_addr;
 
-		/** Maximum Kata container number in Service VM */
-		uint64_t max_kata_containers;
 		/** Align the size of Configuration info to 128Bytes. */
-		uint8_t  reserved[104];
+		uint8_t  reserved[112];
 	} sw;
 } __aligned(8);
 
