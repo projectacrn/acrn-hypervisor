26fdfc6a30acf97cbd76d187de2cf80c9ee44cb5
Xiangyang Wu <xiangyang.wu@intel.com>
2022-01-19 13:31:21 +0800

    DM: add socket module for command monitor
    The socket module is to provide interfaces below, the unix domain
    socket can be created by command monitor for the communication
    between DM instance and libvirt daemon or kata container:
    init_socket: allocate a new socket instance according to socket path
    deinit_socket: free a socket instance
    open_socket: open one unix domain socket server, initialize a
    socket, create one thread to listen to client, another thread to
    poll message from client.
    close_socket: close one unix domain socket server
    find_socket_client: find socket client instance according to fd
    write_socket_char: send message through unix domain socket server
    
    v1--v2:
    	Update some log message format and copyright format.
    v2-->v3:
    	Update SOCKET_MAX_CLIENT to 1H since the socket instance of
    	command monitor only have one client (libvirt or kata container).
    
    Tracked-On: #5921
    
    Signed-off-by: Xiangyang Wu <xiangyang.wu@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

---
+++ devicemodel/core/cmd_monitor/socket.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2022 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _SOCKET_H_
+#define _SOCKET_H_
+#include <sys/queue.h>
+#include <pthread.h>
+#include <sys/un.h>
+
+#define BUFFER_SIZE 16U
+#define UNIX_SOCKET_PATH_MAX 1024U
+#define CLIENT_BUF_LEN 4096U
+#define SOCKET_MAX_CLIENT		1
+
+typedef void data_handler_f(char *cmd_name, int fd);
+
+struct socket_client {
+	struct sockaddr_un addr;
+	int fd;
+	socklen_t addr_len;
+	char buf[CLIENT_BUF_LEN];
+	int len; /* buf len */
+
+	LIST_ENTRY(socket_client) list;
+};
+
+struct socket_dev {
+	char unix_sock_path[UNIX_SOCKET_PATH_MAX];
+	int sock_fd;
+	int logfd;
+
+	data_handler_f *data_handler;
+
+	bool listening;
+	bool polling;
+	pthread_t listen_thread;
+	pthread_t connect_thread;
+
+	LIST_HEAD(client_list, socket_client) client_head;        /* clients for this server */
+	pthread_mutex_t client_mtx;
+	int num_client;
+};
+
+/**
+ * @brief Send message through unix domain socket server
+ */
+int write_socket_char(struct socket_client *client);
+/**
+ * @brief Find socket client instance according to fd
+ */
+struct socket_client *find_socket_client(struct socket_dev *sock, int fd);
+/**
+ * @brief Open one unix domain socket server, initialize a socket,
+ * create one thread to listen to client, another thread to poll message from client.
+ */
+int open_socket(struct socket_dev *sock, data_handler_f *fn);
+/**
+ * @brief Close one unix domain socket server
+ */
+void close_socket(struct socket_dev *sock);
+/**
+ * @brief Initialize a socket
+ *
+ * @param path the socket path
+ * @return struct socket_dev* the socket instance
+ */
+struct socket_dev *init_socket(char *path);
+/**
+ * @brief Deinit a socket
+ *
+ * @param sock The pointer of socket instance
+ */
+void deinit_socket(struct socket_dev *sock);
+
+#endif
