9c1e06e73e0b5e1b322e3cbdb5fd89b311b46e6f
Xiangyang Wu <xiangyang.wu@intel.com>
2022-01-20 09:51:17 +0800

    DM: add new monitor module
    This monitor module is to initialize socket intance, register
    handlers to handle command from socket message, close socket,
    free socket instance:
    init_cmd_monitor: initialize socket intance and register handlers
    to handle command.
    deinit_cmd_monitor: close socket and free socket instance.
    
    In this patch DM makefile is updated to build command monitor.
    
    v1--v2:
    	Update socket path and update log message format.
    	Parse JSON format command message using libcjson lib APIs.
    v2-->v3:
    	Use socket path length MACRO.
    	Update JSON format command message to {"command": "xxx"}.
    
    Tracked-On: #5921
    
    Signed-off-by: Xiangyang Wu <xiangyang.wu@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

---
+++ devicemodel/include/cmd_monitor.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2022 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _CMD_MONITOR_H_
+#define _CMD_MONITOR_H_
+
+int init_cmd_monitor(struct vmctx *ctx);
+void deinit_cmd_monitor(void);
+#endif
