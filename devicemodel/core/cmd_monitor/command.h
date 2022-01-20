65a7bf4803f88f9ce787da71ca5f574ac4891d4a
Xiangyang Wu <xiangyang.wu@intel.com>
2022-01-20 08:12:04 +0800

    DM: add command module for command monitor
    The command module provides interfaces to find a command
    intance, register handler for specified command, dispatch
    command and invoke related handler.
    find_command: find a command instance by name.
    register_command_handler: register the handler for one
    command instance.
    dispatch_command_handlers: dispatch the command and invoke
    registered handler.
    
    v1-->v2:
    	Only support single handler for one command instance.
    v2-->v3:
    	Remove command id.
    	Add error check to avoid regiter handler to command
    	instance which has handler.
    	Update the second parameter type of register_command_handler
    	to avoid unnecessary type conversion.
    
    Tracked-On: #5921
    
    Signed-off-by: Xiangyang Wu <xiangyang.wu@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

---
+++ devicemodel/core/cmd_monitor/command.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2022 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _COMMAND_H_
+#define _COMMAND_H_
+#include <pthread.h>
+#include <sys/queue.h>
+
+
+#define DESTROY "destroy"
+#define BLKRESCAN "blkrescan"
+
+#define CMDS_NUM 2U
+#define CMD_NAME_MAX 32U
+#define CMD_ARG_MAX 320U
+
+typedef int (cmd_handler)(void *arg, void *command_para);
+struct handler_args {
+	void *channel_arg;
+	void *ctx_arg;
+};
+struct command_handler {
+	struct handler_args *arg;
+	cmd_handler *fn;
+};
+struct command_parameters {
+	int fd;
+	char option[CMD_ARG_MAX];
+};
+struct command {
+	const char name[CMD_NAME_MAX]; /**< command name */
+	struct command_parameters para;
+
+	/* command handler */
+	struct command_handler cmd_handler;
+};
+/**
+ * @brief register command handler, other module can use this interface to
+ * register multiple handler for one command.
+ *
+ * @param fn the command handler which will be registered
+ * @param arg the parameter which will be passed into hanlder
+ * @param cmd_name the command name
+ */
+int register_command_handler(cmd_handler *fn, struct handler_args *arg, const char *cmd_name);
+/**
+ * @brief find a command instance by name
+ *
+ * @param name the command name
+ * @return command instance
+ */
+struct command *find_command(const char *name);
+/**
+ * @brief dispatch the command and invoke registered handler.
+ *
+ * @param arg command instance
+ * @return the flag indicates the state of command handler execution
+ */
+int dispatch_command_handlers(void *arg);
+#endif
