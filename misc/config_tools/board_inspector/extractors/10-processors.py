projectacrn/acrn-hypervisor

commit b5b22bf98b6fa820c4117a1cfeb253c5714c16de

board_inspector: avoid hard-coded topo level names
While the terms to refer to CPU topology levels are originally borrowed
from MSR listings in SDMs (i.e. thread, core, die and package), we now
align the topology information with the CPUID topology enumeration
leaf (which does not have a "package" level) in the
implementation. However, some hard-coded level types are not updated
accordingly, leading to strangely organized nodes in the generated board
XML.

This patch refines the CPU extractor of the board inspector by removing
such hard code. Also the XPATH counting the total number of threads is
refined to tolerate the variance of reported intermediate levels.

Tracked-On: #6689
Signed-off-by: Junjie Mao <junjie.mao@intel.com>

misc/config_tools/board_inspector/extractors/10-processors.py
@@ -74,8 +74,8 @@ def extract_topology(processors_node):
         while True:
             leaf_topo = parse_cpuid(topo_leaf, subleaf, cpu_id)
             if leaf_topo.level_type == 0:
-                if last_node.tag != "package":
-                    n, _ = get_or_create_parent(processors_node, "die", "0")
+                if last_node.tag != level_types[-1]:
+                    n, _ = get_or_create_parent(processors_node, level_types[-1], "0x0")
                     n.append(last_node)
                     last_node = n
                 processors_node.append(last_node)

misc/config_tools/xforms/board_info.h.xsl
@@ -61,7 +61,7 @@
   </xsl:template>
 
   <xsl:template name="MAX_PCPU_NUM">
-    <xsl:value-of select="acrn:define('MAX_PCPU_NUM', count(//processors/die/core/thread), 'U')" />
+    <xsl:value-of select="acrn:define('MAX_PCPU_NUM', count(//processors//thread), 'U')" />
   </xsl:template>
 
   <xsl:template name="MAX_VMSIX_ON_MSI_PDEVS_NUM">

commit c7dde5fbf3f122286ea7aa375c60b541c0d8d2e0

config_tools: fix illegal access to the level_types dictionary
In 10-processors.py, level_types is an unordered dictionary from level type
encodings to human-readable terms, not a list. It is thus illegal to access the
highest level using the `[-1]` notation.

This patch fixes this by calculating the highest level in level_types and use
that level as the index instead.

Tracked-On: #6689
Fixes: b5b22bf98 ("board_inspector: avoid hard-coded topo level names")
Signed-off-by: Junjie Mao <junjie.mao@intel.com>

misc/config_tools/board_inspector/extractors/10-processors.py
@@ -74,8 +74,9 @@ def extract_topology(processors_node):
         while True:
             leaf_topo = parse_cpuid(topo_leaf, subleaf, cpu_id)
             if leaf_topo.level_type == 0:
-                if last_node.tag != level_types[-1]:
-                    n, _ = get_or_create_parent(processors_node, level_types[-1], "0x0")
+                highest_level = max(level_types.keys())
+                if last_node.tag != level_types[highest_level]:
+                    n, _ = get_or_create_parent(processors_node, level_types[highest_level], "0x0")
                     n.append(last_node)
                     last_node = n
                 processors_node.append(last_node)