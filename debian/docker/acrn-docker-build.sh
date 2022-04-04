112396ef88bd224f0fd65c1658d77d78fabde2bd
Helmut Buchsbaum <helmut.buchsbaum@opensource.tttech-industrial.com>
2022-04-04 12:15:05 +0200

    debian: Add docker build helpers
    To ease building ACRN with all its requirements, provide a distribution aware,
    self-contained docker build environment to build regular Debian packages.
    
    For Debian Buster(10) and Ubuntu Focal Fossa(20.04 LTS) some build tools are
    grabbed from their respective backports repositories. This provides a much
    easier alignment with the more resent distributions.
    
    There are three packages to be re-packaged to accommodate the build needs as
    well as the runtime needs of the ACRN packages:
    
     * python3-elementpath_2.4.0-1
     * python3-xmlschema_1.9.2-1
     * acpica-tools_20200925-1
    
    These packages are built during docker image build time and provided within
    a local APT repository for installation at build time, i.e. when running the
    respective docker container.
    
    The multistage Dockerfile separates the required pre-build stages and reduces
    the resulting image footprint of the final docker image. There are separate
    docker images for each distribution.
    
    To summarize the build steps the script acrn-docker-build.sh is provided. To
    control the vendor and the distribution for the build, just set the
    environment variables VENDOR and DISTRO before calling the script from the
    top directory of the ACRN hypervisor workspace, e.g.:
    
      VENDOR=debian DISTRO=bullseye debian/docker/acrn-docker-build.sh
    
    or
    
      VENDOR=ubuntu DISTRO=focal debian/docker/acrn-docker-build.sh
    
    Tracked-On: #6688
    Signed-off-by: Helmut Buchsbaum <helmut.buchsbaum@opensource.tttech-industrial.com>

---
+++ debian/docker/acrn-docker-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Helper script to build ACRN with docker
+# This also includes building packages required for ACRN build or runtime
+
+VENDOR=${VENDOR:-debian}
+DISTRO=${DISTRO:-stable}
+
+TOPDIR=$(git rev-parse --show-toplevel)
+DOCKER=$(which docker)
+
+if [ -z "${TOPDIR}" ]; then
+    echo "Run $0 from inside git repository!"
+    exit 1
+fi
+
+if [ -z "${DOCKER}" ]; then
+    echo "Cannot find docker binary, please install!"
+    exit 1
+fi
+
+pushd ${TOPDIR} >/dev/null
+
+if [ ! -f debian/docker/Dockerfile ]; then
+    echo "No Dockerfile available!"
+    exit 1
+fi
+
+set -e
+# create docker image for Debian package build
+${DOCKER} build \
+    -f debian/docker/Dockerfile \
+    --build-arg DISTRO=${DISTRO} \
+    --build-arg VENDOR=${VENDOR} \
+    -t acrn-pkg-builder:${DISTRO} debian/docker
+
+# build ACRN packages
+${DOCKER} run \
+    --rm \
+    -e UID=$(id -u) \
+    -e GID=$(id -g) \
+    -v $(pwd):/source acrn-pkg-builder:${DISTRO} -F --no-sign --git-export-dir=build/${DISTRO} "$@"
+
+popd >/dev/null
