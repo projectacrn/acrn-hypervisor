1563bccfff5e67a5cc613b02c9610f8eab1f5ac9
Weiyi Feng <weiyix.feng@intel.com>
2022-05-10 13:38:33 +0800

    config_tools: let windows build scripts auto exit when an error occurs
    let windows build scripts auto exit when an error occurs
    
    Tracked-On: #6691
    Signed-off-by: Weiyi Feng <weiyix.feng@intel.com>

--- misc/config_tools/build.cmd
+++ misc/config_tools/build.cmd
@@ -1,9 +1,9 @@
 @echo off
-python scenario_config/schema_slicer.py
-python scenario_config/jsonschema/converter.py
-xmllint --xinclude schema/datachecks.xsd > schema/allchecks.xsd
-python -m build
+python scenario_config/schema_slicer.py || exit /b
+python scenario_config/jsonschema/converter.py || exit /b
+xmllint --xinclude schema/datachecks.xsd > schema/allchecks.xsd || exit /b
+python -m build || exit /b
 rem pip install .\dist\acrn_config_tools-3.0-py3-none-any.whl --force-reinstall
 del .\configurator\packages\configurator\thirdLib\acrn_config_tools-3.0-py3-none-any.whl
-python .\configurator\packages\configurator\thirdLib\manager.py install
+python .\configurator\packages\configurator\thirdLib\manager.py install || exit /b
 echo build and install success
