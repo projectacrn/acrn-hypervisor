bb723df8254a6b368071c34be3ffde1a7ded1dd9
Liu Long <long.liu@linux.intel.com>
2022-05-11 09:55:16 +0800

    ACRN: DM: Expose the vhost kernel ioctrl API
    The patch expose the vhost kernel ioctrl API and remove the virtio_net
    operation functions to the virtio_net code module.
    
    Tracked-On: #7456
    Signed-off-by: Liu Long <long.liu@linux.intel.com>
    Reviewed-by: Conghui <conghui.chen@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

--- devicemodel/include/vhost.h
+++ devicemodel/include/vhost.h
@@ -132,19 +132,15 @@ int vhost_dev_start(struct vhost_dev *vdev);
 int vhost_dev_stop(struct vhost_dev *vdev);
 
 /**
- * @brief set backend fd of vhost net.
+ * @brief vhost kernel dev ioctrl function
  *
- * This interface is called to set the backend fd (for example tap fd)
- * to vhost.
+ * This interface is used to operation the vhost dev kernel.
  *
- * @param vdev Pointer to struct vhost_dev.
- * @param backend_fd fd of backend (for example tap fd).
+ * @param vdev Pointer to struct vhost_dev
+ * @param The request to vhost kernel
+ * @param The arguments of vhost kernel operation
  *
- * @return 0 on success and -1 on failure.
+ * @return 0 on success and -1 on failure
  */
-int vhost_net_set_backend(struct vhost_dev *vdev, int backend_fd);
-
-/**
- * @}
- */
+int vhost_kernel_ioctl(struct vhost_dev *vdev, unsigned long int request, void *arg);
 #endif
