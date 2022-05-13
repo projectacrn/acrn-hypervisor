dc76437b98c8fb4a23f02748c95928c11d5e5be7
Weiyi Feng <weiyix.feng@intel.com>
2022-05-13 15:40:05 +0800

    config_tools: fix Windows config_tools package build script
    fix error return code of config_tools python package build script in Windows environment
    
    Tracked-On: #6691
    Signed-off-by: Weiyi Feng <weiyix.feng@intel.com>

--- misc/config_tools/build.cmd
+++ misc/config_tools/build.cmd
@@ -1,11 +1,11 @@
 @echo off
-python scenario_config/schema_slicer.py || exit /b
-python scenario_config/jsonschema/converter.py || exit /b
-xmllint --xinclude schema/datachecks.xsd > schema/allchecks.xsd || exit /b
-python -m build || exit /b
+python scenario_config/schema_slicer.py || exit 1
+python scenario_config/jsonschema/converter.py || exit 1
+xmllint --xinclude schema/datachecks.xsd > schema/allchecks.xsd || exit 1
+python -m build || exit 1
 rem pip install .\dist\acrn_config_tools-3.0-py3-none-any.whl --force-reinstall
 del .\configurator\packages\configurator\thirdLib\acrn_config_tools-3.0-py3-none-any.whl
 cd configurator
-python .\packages\configurator\thirdLib\manager.py install || exit /b
-yarn
+python .\packages\configurator\thirdLib\manager.py install || exit 1
+yarn || exit 1
 echo build and install success
