e5233a9d8fded9d13eda68d1de8baf6bf18bbf21
Weiyi Feng <weiyix.feng@intel.com>
2022-05-26 06:11:10 +0800

    config_tools: remove footer version link
    remove footer version link
    
    Tracked-On: #6691
    Signed-off-by: Weiyi Feng <weiyix.feng@intel.com>

--- misc/config_tools/configurator/packages/configurator/src/components/common/Footer.vue
+++ misc/config_tools/configurator/packages/configurator/src/components/common/Footer.vue
@@ -2,15 +2,12 @@
   <div class="pt-3">
     <p class="text-center text-secondary">
       © Copyright Project ACRN™, a Series of LF Projects, LLC.
-      <text v-if="version" @click="home">- Version {{ version }}</text>
+      <text v-if="version">- Version {{ version }}</text>
     </p>
   </div>
 </template>
 
 <script>
-import {getVersion} from "@tauri-apps/api/app";
-
-
 export default {
   name: "Footer",
   data: () => {
@@ -19,12 +16,7 @@ export default {
     }
   },
   mounted() {
-	this.version = packageVersion
-  },
-  methods: {
-    home() {
-      this.$router.push('/')
-    },
+    this.version = packageVersion
   }
 }
 </script>
