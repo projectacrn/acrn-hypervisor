dafe7dfe99a447e6ba42e94882d47ef1009fc638
Weiyi Feng <weiyix.feng@intel.com>
2022-06-21 13:20:27 +0800

    config_tools: refine cat widget
    refine cat widget
    
    Tracked-On: #6691
    Signed-off-by: Weiyi Feng <weiyix.feng@intel.com>

--- misc/config_tools/configurator/packages/configurator/src/pages/Config/ConfigForm/CustomWidget/CAT/HexBlockRangeSelector.vue
+++ misc/config_tools/configurator/packages/configurator/src/pages/Config/ConfigForm/CustomWidget/CAT/HexBlockRangeSelector.vue
@@ -6,20 +6,14 @@
 
 <script>
 import Slider from '@vueform/slider'
+import configurator from '../../../../../lib/acrn'
 
-function count(source, target) {
-  return (source.match(new RegExp(target, 'g')) || []).length;
-}
 
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-let getHexValue = (value, max) => {
-  let newHexValue = '0'.repeat(value[0]) + '1'.repeat(value[1] - value[0]) + '0'.repeat(max - value[1])
-  newHexValue = (parseInt(newHexValue, 2).toString(16))
-  let zeroPadding = '0'.repeat(Number.parseInt('1'.repeat(max), 2).toString(16).length - newHexValue.length)
-  newHexValue = '0x' + zeroPadding + newHexValue;
-  return newHexValue;
-}
+
+
+
 
 export default {
   name: "HexBlockRangeSelector",
@@ -36,32 +30,7 @@ export default {
     },
     hexField: {
       get() {
-        let str_bin = Number.parseInt(this.modelValue).toString(2);
-        let block_length = str_bin.length;
-        let block_enabled_length = count(str_bin, "1");
-
-        let start = 0
-        let end = 0
-
-        if (block_length > this.max) {
-          if (block_enabled_length >= this.max) {
-            str_bin = "1".repeat(this.max);
-          } else {
-            str_bin = "0".repeat(this.max - block_enabled_length) + "1".repeat(block_enabled_length);
-          }
-        } else {
-          if (block_length < this.max) {
-            str_bin = "0".repeat(this.max - block_length) + str_bin;
-          }
-        }
-
-        start = str_bin.indexOf("1") !== -1 ? str_bin.indexOf("1") : 0;
-        end = start + count(str_bin, "1");
-
-        // noinspection UnnecessaryLocalVariableJS
-        let result = [start, end]
-        return result
-
+        return configurator.cat.hexToRange(this.modelValue, this.max)
       },
       set(value) {
         if (value[1] - value[0] === 0) {
@@ -71,7 +40,7 @@ export default {
         } else {
           this.lastValue = JSON.parse(JSON.stringify(value))
         }
-        this.$emit("update:modelValue", getHexValue(value, this.max))
+        this.$emit("update:modelValue", configurator.cat.rangeToHex(value, this.max))
       }
     }
   },
