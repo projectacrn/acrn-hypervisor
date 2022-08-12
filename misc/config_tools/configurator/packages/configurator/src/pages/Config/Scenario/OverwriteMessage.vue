e47d0e2b2a10647778efdbffc3620f1438f5e753
Chuang Ke <chuangx.ke@intel.com>
2022-08-12 17:13:24 +0800

    [config_tool] v2 show warning message when users attempt to create a new scenario
    fix warning message when users attempt to create a new scenario, or import an existing scenario, for an existing configuration
    v1-->v2: update text to align with the design prototype spec.
    
    Tracked-On: #7898
    Signed-off-by: Chuang-Ke chuangx.ke@intel.com
    Reviewed-by: Junjie Mao junjie.mao@intel.com

---
+++ misc/config_tools/configurator/packages/configurator/src/pages/Config/Scenario/OverwriteMessage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-modal id="my-modal" no-close-on-backdrop
+             title="Scenario XML Overwrite" v-model="showModal"
+             @cancel="cancel"
+             @hidden="cancel"
+             @abort="cancel"
+             @close="cancel"
+             @ok="overWrite"
+             @submit="overWrite"
+             ok-title="Overwrite">
+      <div>Are you sure you want to overwrite the scenario XML file in this working folder?</div>
+      <br>
+      <div>if you continue, the scenario XML file in the following working folder will be overwritten:
+        <br>{{ usingWorkingFolder }}
+      </div>
+      <br>
+      <div>(You must save your configuration to generate new launch scripts associated to your new scenario XML file.)
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OverwriteMessage',
+  props: {
+    usingWorkingFolder: String,
+    showModal: {
+      type: Boolean,
+      default: false
+    },
+  },
+  emits: {'overWrite': null},
+  data() {
+    return {
+      show: false,
+      variants: ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'],
+      headerBgVariant: 'dark',
+      headerTextVariant: 'light',
+      bodyBgVariant: 'light',
+      bodyTextVariant: 'dark',
+      footerBgVariant: 'warning',
+      footerTextVariant: 'dark'
+    }
+  },
+  methods: {
+    cancel() {
+      this.$emit('update:showModal', false)
+    },
+    overWrite() {
+      this.$emit('update:showModal', false)
+      this.$emit("overWrite")
+    },
+  }
+}
+</script>
