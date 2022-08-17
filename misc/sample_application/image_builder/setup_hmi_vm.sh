e6de2e5798fd6698fcbf32bc22a34b7d11597f30
Liu Long <long.liu@linux.intel.com>
2022-08-17 11:17:13 +0800

    SampleApplication: Update the image create scripts
    1. Delete the generated images if the create scripts failed.
    2. Enable root user login on GUI.
    3. Enable systemd service in case there are many error logs.
    
    Tracked-On: #7820
    Signed-off-by: Liu Long <long.liu@linux.intel.com>

--- misc/sample_application/image_builder/setup_hmi_vm.sh
+++ misc/sample_application/image_builder/setup_hmi_vm.sh
@@ -15,22 +15,38 @@ function update_package_info() {
     apt update -y && apt install python3 python3-pip \
         net-tools python3-matplotlib \
 	linux-modules-extra-$(uname -r) \
-	openssh-server -y
+	openssh-server \
+	isc-dhcp-server -y
     pip3 install flask 'numpy==1.18.5' pandas posix_ipc
 
 }
 
 function install_desktop() {
-    apt install ubuntu-gnome-desktop
+    apt install ubuntu-gnome-desktop -y
 }
 
 function change_root_password() {
     passwd root
 }
 
+function enable_root_login() {
+    sed -i -e '3 s/^/#/' /etc/pam.d/gdm-password
+    sed -i 's/\[daemon\]/& \n  AllowRoot=true /' /etc/gdm3/custom.conf
+}
+
 function add_normal_user() {
     useradd -s /bin/bash -d /home/acrn/ -m -G sudo acrn && \
         passwd acrn
+}
+
+function enable_services() {
+    services=(ssh.service isc-dhcp-server)
+    sudo ssh-keygen -A
+    for service in ${services[*]}
+    do
+        systemctl enable ${service}
+	systemctl unmask ${service}
+    done
 }
 
 # Change current working directory to the root to avoid "target is busy" errors
@@ -42,4 +58,5 @@ try_step "Unmounting /home" umount_directory /home
 try_step "Updating package information" update_package_info
 try_step "Installing GNOME desktop" install_desktop
 try_step "Changing the password of the root user" change_root_password
+try_step "Enable root user login" enable_root_login
 try_step "Adding the normal user acrn" add_normal_user
