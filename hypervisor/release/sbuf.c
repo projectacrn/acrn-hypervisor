f886c432c9801e93e9f39c6a47a906574d2c4fea
Conghui <conghui.chen@intel.com>
2022-10-19 14:48:10 +0800

    hv: fix compile issue in release mode
    fix compile issue for sbuf in release mode.
    
    Tracked-On: #8241
    Signed-off-by: Conghui <conghui.chen@intel.com>
    Acked-by: Fei Li <fei1.li@intel.com>

--- hypervisor/release/sbuf.c
+++ hypervisor/release/sbuf.c
@@ -4,4 +4,12 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <types.h>
+#include <errno.h>
+
+int32_t sbuf_share_setup(__unused uint16_t pcpu_id,
+		__unused uint32_t sbuf_id, __unused uint64_t *hva)
+{
+	return -EPERM;
+}
 void sbuf_reset(void) {}
