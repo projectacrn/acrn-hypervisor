a715a3222b55d7778b067b9d1c5a980063023944
Conghui <conghui.chen@intel.com>
2022-11-24 13:28:23 +0800

    asyncio: refine the setup ioctl
    Remove the common interface for sbuf setup, as it is not accept by
    kernel side. Instead, use dedicate setup function for asyncio to init
    its sbuf.
    
    Tracked-On: #8209
    Signed-off-by: Conghui <conghui.chen@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

--- devicemodel/core/vmmapi.c
+++ devicemodel/core/vmmapi.c
@@ -295,19 +295,14 @@ vm_destroy(struct vmctx *ctx)
 }
 
 int
-vm_setup_sbuf(struct vmctx *ctx, uint32_t sbuf_id, uint64_t base)
+vm_setup_asyncio(struct vmctx *ctx, uint64_t base)
 {
 	int error;
-	struct acrn_sbuf sbuf_param;
 
-	bzero(&sbuf_param, sizeof(sbuf_param));
-	sbuf_param.sbuf_id = sbuf_id;
-	sbuf_param.base = base;
-
-	error = ioctl(ctx->fd, ACRN_IOCTL_SETUP_SBUF, &sbuf_param);
+	error = ioctl(ctx->fd, ACRN_IOCTL_SETUP_ASYNCIO, base);
 
 	if (error) {
-		pr_err("ACRN_IOCTL_SBUF_PAGE ioctl() returned an error: %s\n", errormsg(errno));
+		pr_err("ACRN_IOCTL_SETUP_ASYNCIO ioctl() returned an error: %s\n", errormsg(errno));
 	}
 
 	return error;
