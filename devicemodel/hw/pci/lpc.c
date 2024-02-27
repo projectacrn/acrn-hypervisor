projectacrn/acrn-hypervisor

commit 852f10cc31bdca60dfb269b524dfeecd68d4bbe9

dm: lpc: only emulate COM ports specified in command line
Currently lpc emulates all the supported COM ports no matter it is
configured or not in command line. Change the behavior to only emulate
those specified in command line.

Tracked-On: #8537
Signed-off-by: Jiaqing Zhao <jiaqing.zhao@linux.intel.com>
Reviewed-by: Jian Jun Chen <jian.jun.chen@intel.com>

devicemodel/hw/pci/lpc.c
@@ -87,6 +87,7 @@ lpc_device_parse(const char *opts)
 	if (lpcdev != NULL) {
 		for (unit = 0; unit < LPC_UART_NUM; unit++) {
 			if (strcasecmp(lpcdev, lpc_uart_names[unit]) == 0) {
+				lpc_uart_vdev[unit].enabled = 1;
 				lpc_uart_vdev[unit].opts = str;
 				error = 0;
 				goto done;
@@ -201,6 +202,9 @@ lpc_init(struct vmctx *ctx)
 	for (unit = 0; unit < LPC_UART_NUM; unit++) {
 		lpc_uart = &lpc_uart_vdev[unit];
 		name = lpc_uart_names[unit];
+
+		if (lpc_uart->enabled == 0)
+			continue;
 
 		if (uart_legacy_alloc(unit,
 				      &lpc_uart->iobase,

commit a40ef447a36fc4baf7d3437e883ab8943abd9acd

dm: fix LPC UART no output issue after reboot
The meaning of lpc_uart_vdev::enabled was changed from runtime enablement
status of UARTs to user configuration in commit 852f10cc3. So it's
incorrect to clear it in lpc_deinit() which will cause UARTs disabled
after reboot.

Tracked-On: #8537
Fixes: 852f10cc3 ("dm: lpc: only emulate COM ports specified in command line")
Signed-off-by: Qiang Zhang <qiang4.zhang@intel.com>

devicemodel/hw/pci/lpc.c
@@ -65,7 +65,7 @@ static struct lpc_uart_vdev {
 	const char *opts;
 	int	iobase;
 	int	irq;
-	int	enabled;
+	int	enabled;	/* enabled/configured by user */
 } lpc_uart_vdev[LPC_UART_NUM];
 
 static const char *lpc_uart_names[LPC_UART_NUM] = { "COM1", "COM2", "COM3", "COM4" };
@@ -185,7 +185,6 @@ lpc_deinit(struct vmctx *ctx)
 		uart_release_backend(lpc_uart->uart, lpc_uart->opts);
 		uart_legacy_dealloc(unit);
 		lpc_uart->uart = NULL;
-		lpc_uart->enabled = 0;
 	}
 }
 
@@ -233,7 +232,6 @@ lpc_init(struct vmctx *ctx)
 		error = register_inout(&iop);
 		if (error)
 			goto init_failed;
-		lpc_uart->enabled = 1;
 	}
 
 	return 0;