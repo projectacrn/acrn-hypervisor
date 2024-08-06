172c56fe0aa5a17eb2e2ec8b02ab0966d7f9ce30
Haiwei Li <haiwei.li@intel.com>
2024-08-06 15:03:44 +0800

    doc: add module design for peripheral vrtc device
    GAI Tooling Notice: These contents may have been developed with support from one
    or more generative artificial intelligence solutions.
    
    This patch is to add doxygen style comments for some elements in vp-dm_vperipheral
    vrtc module.
    
    Tracked-On: #8665
    
    Signed-off-by: Haiwei Li <haiwei.li@intel.com>

--- hypervisor/include/dm/vrtc.h
+++ hypervisor/include/dm/vrtc.h
@@ -1,11 +1,30 @@
 /*
- * Copyright (C) 2022 Intel Corporation.
+ * Copyright (C) 2024 Intel Corporation.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #ifndef VRTC_H
 #define VRTC_H
+
+/**
+ * @defgroup vp-dm_vperipheral vp-dm.vperipheral
+ * @ingroup vp-dm
+ * @brief Implementation of virtual peripheral devices in hypervisor.
+ *
+ * This module implements the virtualization of all peripheral devices in hypervisor. The virtual device initial
+ * function is usually called by the VM initialization function and registers their port IO and memory IO access
+ * functions. So when a guest VM accesses its peripheral device by port IO or memory IO, it would cause VM exit and then
+ * call their registered functions.
+ * @{
+ */
+
+/**
+ * @file
+ * @brief Definitions for the virtual RTC device.
+ *
+ * This file defines types and data structure for the virtual RTC device.
+ */
 
 typedef int32_t time_t;
 
@@ -29,17 +48,34 @@ struct rtcdev {
 	uint8_t	century;
 };
 
+/**
+ * @brief Data structure to illustrate a virtual RTC device.
+ *
+ * This structure contains the information of a virtual RTC device.
+ *
+ * @consistency self.vm->vrtc == self
+ * @alignment N/A
+ *
+ * @remark N/A
+ */
 struct acrn_vrtc {
-	struct acrn_vm	*vm;
-	uint32_t	addr;           /* RTC register to read or write */
-
-	time_t		base_rtctime;	/* Base time calulated from physical rtc register. */
-	time_t		offset_rtctime;	/* RTC offset against base time. */
-	time_t		last_rtctime;	/* Last RTC time, to keep monotonicity. */
-
-	uint64_t	base_tsc;	/* Base tsc value */
-
-	struct rtcdev	rtcdev;		/* RTC register */
+	struct acrn_vm	*vm; /**< Pointer to the VM that owns the virtual RTC device. */
+	/**
+	 * @brief The RTC register to read or write.
+	 *
+	 * To access RTC registers, the guest writes the register index to the RTC address port and then reads/writes
+	 * the register value from/to the RTC data port. This field is used to store the register index.
+	 */
+	uint32_t	addr;
+	time_t		base_rtctime; /**< Base time calculated from physical RTC register. */
+	time_t		offset_rtctime; /**< RTC offset against base time. */
+	time_t		last_rtctime; /**< Last RTC time, to keep monotonicity. */
+	uint64_t	base_tsc; /**< Base TSC value. */
+	struct rtcdev	rtcdev; /**< Register layout of RTC. */
 };
 
 #endif /* VRTC_H */
+
+/**
+ * @}
+ */
