16c2ebda0f46b1050559e8ece850bfb05e14eb78
Haicheng Li <haicheng.li@linux.intel.com>
2025-09-06 11:41:58 +0800

    hv: multi-arch add RISC-V memory library
    RISC-V use common memory library.
    
    Tracked-On: #8794
    Signed-off-by: Haicheng Li <haicheng.li@linux.intel.com>
    Co-developed-by: Haoyu Tang <haoyu.tang@intel.com>
    Signed-off-by: Haoyu Tang <haoyu.tang@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

---
+++ hypervisor/include/arch/riscv/asm/lib/memory.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2025 Intel Corporation.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef RISCV_LIB_MEMORY_H
+#define RISCV_LIB_MEMORY_H
+/*
+ * RISCV use common memory library implementation, doesn't define __HAS_ARCH_MEMORY_LIB.
+ */
+#endif /* RISCV_LIB_MEMORY_H */
