a42cefa0845461cd2d84c64e4a2dc60d9025ca43
Haicheng Li <haicheng.li@intel.com>
2025-09-07 10:50:00 +0800

    hv: multi-arch add RISC-V bits library implementation
    Follow multi-arch design, implement the mandatory arch bit ops
    functions declared in common bits.h for risc-v support.
    
    Tracked-On: #8803
    Signed-off-by: Haicheng Li <haicheng.li@linux.intel.com>
    Signed-off-by: Haoyu Tang <haoyu.tang@intel.com>
    Reviewed-by: Yifan Liu  <yifan1.liu@intel.com>
    Acked-by: Wang, Yu1 <yu1.wang@intel.com>

--- hypervisor/include/arch/riscv/asm/lib/bits.h
+++ hypervisor/include/arch/riscv/asm/lib/bits.h
@@ -1,55 +1,133 @@
 /*
- * Copyright (C) 2025 Intel Corporation.
+ * Copyright (C) 2023-2025 Intel Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
+ * Haicheng Li <haicheng.li@intel.com>
  */
 
-#ifndef RISCV_BITS_H
-#define RISCV_BITS_H
-
+#ifndef RISCV_LIB_BITS_H
+#define RISCV_LIB_BITS_H
 #include <types.h>
-
-uint16_t ffs64(__unused uint64_t value)
+#include <asm/cpu.h>
+#define INVALID_BIT_INDEX  0xffffU
+static inline uint16_t arch_ffs64(uint64_t x)
 {
-	/**
-	 * Dummy implementation.
-	 * Official implementations are to be provided in the library patchset (by Haoyu).
-	 */
-	return 0U;
+    asm volatile ("ctz %0, %1" : "=r"(x) : "r"(x));
+    return x == BITS_PER_LONG ? INVALID_BIT_INDEX :(uint16_t)x;
 }
 
-bool bitmap_test(__unused uint16_t nr, __unused const volatile uint64_t *addr)
+static inline uint16_t arch_fls64(uint64_t x)
 {
-	/**
-	 * Dummy implementation.
-	 * Official implementations are to be provided in the library patchset (by Haoyu).
-	 */
-	return true;
+    asm volatile ("clz %0, %1" : "=r"(x) : "r"(x));
+    return  x == BITS_PER_LONG ? INVALID_BIT_INDEX :(BITS_PER_LONG - 1U - (uint16_t)x);
 }
 
-void bitmap_set_lock(__unused uint16_t nr_arg, __unused volatile uint64_t *addr)
+static inline int16_t arch_fls32(uint32_t x)
 {
-	/**
-	 * Dummy implementation.
-	 * Official implementations are to be provided in the library patchset (by Haoyu).
-	 */
+    asm volatile ("clzw %0, %1" : "=r" (x) : "r" (x));
+    return  x == 32 ? (int16_t)INVALID_BIT_INDEX :(int16_t)(31 - x);
 }
 
-void bitmap_clear_lock(__unused uint16_t nr_arg, __unused volatile uint64_t *addr)
+/*
+ * return !!((*addr) & (1UL<<nr));
+ * Note:Input parameter nr shall be less than 64. If nr>=64, it will
+ * be truncated.
+ */
+ static inline bool arch_bitmap_test(uint32_t nr, const volatile uint64_t *addr)
 {
-	/**
-	 * Dummy implementation.
-	 * Official implementations are to be provided in the library patchset (by Haoyu).
-	 */
+    uint64_t mask = 1UL << (nr & ((8U * sizeof(uint64_t)) - 1U));
+    return !!(*addr & mask);
 }
 
-void bitmap_clear_nolock(__unused uint16_t nr_arg, __unused volatile uint64_t *addr)
+static inline bool arch_bitmap32_test(uint32_t nr, const volatile uint32_t *addr)
 {
-	/**
-	 * Dummy implementation.
-	 * Official implementations are to be provided in the library patchset (by Haoyu).
-	 */
+    uint32_t mask = 1UL << (nr & ((8U * sizeof(uint32_t)) - 1U));
+    // TODO: Use RISC-V B extension instruction 'bext' to optimize this function
+    return !!(*addr & mask);
 }
 
-#endif /* RISCV_BITS_H */
+/*
+ * (*addr) |= (1UL<<nr);
+ * Note:Input parameter nr shall be less than 64.
+ * If nr>=64, it will be truncated.
+ */
+#define build_bitmap_set(name, op_len, op_type, lock)                   \
+static inline void name(uint32_t nr_arg, volatile op_type *addr)        \
+{                                                                       \
+    op_type mask = 1UL << (nr_arg & ((8U * sizeof(op_type)) - 1U));     \
+    asm volatile (                                                      \
+        "amoor." op_len "." lock " zero, %1, %0"                        \
+        : "+A" (*addr)                                                  \
+        : "r" (mask)                                                    \
+        : "memory"                                                      \
+    );                                                                  \
+}
+build_bitmap_set(arch_bitmap_set, "d", uint64_t, "aqrl")
+build_bitmap_set(arch_bitmap32_set, "w", uint32_t, "aqrl")
+
+/*
+ * (*addr) &= ~(1UL<<nr);
+ * Note:Input parameter nr shall be less than 64.
+ * If nr>=64, it will be truncated.
+ */
+#define build_bitmap_clear(name, op_len, op_type, lock)                 \
+static inline void name(uint32_t nr_arg, volatile op_type *addr)        \
+{                                                                       \
+    op_type mask = ~(1UL << (nr_arg & ((8U * sizeof(op_type)) - 1U)));  \
+    asm volatile (                                                      \
+        "amoand." op_len "." lock " zero, %1, %0"                       \
+        : "+A" (*addr)                                                  \
+        : "r" (mask)                                                    \
+        : "memory"                                                      \
+    );                                                                  \
+}
+build_bitmap_clear(arch_bitmap_clear, "d", uint64_t, "aqrl")
+build_bitmap_clear(arch_bitmap32_clear, "w", uint32_t, "aqrl")
+
+/*
+ * bool ret = (*addr) & (1UL<<nr);
+ * (*addr) |= (1UL<<nr);
+ * return ret;
+ * Note:Input parameter nr shall be less than 64. If nr>=64, it
+ * will be truncated.
+ */
+#define build_bitmap_testandset(name, op_len, op_type, lock)            \
+static inline bool name(uint32_t nr_arg, volatile op_type *addr)        \
+{                                                                       \
+    op_type mask = 1UL << (nr_arg & ((8U * sizeof(op_type)) - 1U));     \
+    op_type old;                                                        \
+    asm volatile (                                                      \
+        "amoor." op_len "." lock " %0, %2, %1"                          \
+        : "=r" (old), "+A" (*addr)                                      \
+        : "r" (mask)                                                    \
+        : "memory"                                                      \
+    );                                                                  \
+    return !!(old & mask);                                              \
+}
+build_bitmap_testandset(arch_bitmap_test_and_set, "d", uint64_t, "aqrl")
+build_bitmap_testandset(arch_bitmap32_test_and_set, "w", uint32_t, "aqrl")
+
+/*
+ * bool ret = (*addr) & (1UL<<nr);
+ * (*addr) &= ~(1UL<<nr);
+ * return ret;
+ * Note:Input parameter nr shall be less than 64. If nr>=64,
+ * it will be truncated.
+ */
+#define build_bitmap_testandclear(name, op_len, op_type, lock)          \
+static inline bool name(uint32_t nr_arg, volatile op_type *addr)        \
+{                                                                       \
+    op_type mask = 1UL << (nr_arg & ((8U * sizeof(op_type)) - 1U));     \
+    op_type old;                                                        \
+    asm volatile (                                                      \
+        "amoand." op_len "." lock " %0, %2, %1"                         \
+        : "=r" (old), "+A" (*addr)                                      \
+        : "r" (~mask)                                                   \
+        : "memory"                                                      \
+    );                                                                  \
+    return !!(old & mask);                                              \
+}
+build_bitmap_testandclear(arch_bitmap_test_and_clear, "d", uint64_t, "aqrl")
+build_bitmap_testandclear(arch_bitmap32_test_and_clear, "w", uint32_t, "aqrl")
+#endif /* RISCV_LIB_BITOPS_H */
